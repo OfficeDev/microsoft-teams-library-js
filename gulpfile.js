99b177fa73f5e778a57547e59ebdc36bc34eaa10
Daniel Zumwalt <daniel.zumwalt@microsoft.com>
2017-05-09 11:35:04 -0700

    adding gulp tasks to handle asset upload to CDN (blob store) (#45)

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,26 @@
 var del = require("del"),
     fs = require("fs"),
     gulp = require("gulp"),
+    gutil = require("gulp-util"),
     karma = require("karma").Server,
-    merge = require('merge2'),
+    merge = require("merge2"),
     tslint = require("gulp-tslint"),
     typescript = require("gulp-typescript"),
     rename = require("gulp-rename"),
     typings = require("gulp-typings"),
-    uglify = require("gulp-uglify");
+    uglify = require("gulp-uglify"),
+    deployCdn = require("gulp-deploy-azure-cdn"),
+    KeyVault = require("azure-keyvault"),
+    argv = require("yargs").argv,
+    AuthenticationContext = require("adal-node").AuthenticationContext;
 
 var buildDir = "./build/";
 var distDir = "./dist/";
+
+/// global options
+var options = {
+    connectionString: ""
+};
 
 gulp.task("typings", function ()
 {
@@ -91,4 +101,78 @@ gulp.task("clean", function() {
         distDir,
         "./typings/"
     ]);
+});
+
+/// tasks for uploading dist assets to CDN
+gulp.task("get-connectionstring-from-secret", function (done) {
+    var clientId = argv.clientId;
+    var clientSecret = argv.clientSecret;
+    var vaultUri = argv.vaultUri;
+    var secretName = argv.secretName;
+    var secretIdentifier = vaultUri + "/secrets/" + secretName + "/";
+
+    if (!clientId || !clientSecret || !vaultUri || !secretName) {
+        console.error("missing required inputs for 'get-connectionstring-form-secret'");
+        process.exit(1);
+    }
+
+    // Authenticator - retrieves the access token 
+    var authenticator = function (challenge, callback) {
+        var context = new AuthenticationContext(challenge.authorization);
+        return context.acquireTokenWithClientCredentials(challenge.resource, clientId, clientSecret, function (err, tokenResponse) {
+            if (err) throw err;
+            var authorizationValue = tokenResponse.tokenType + " " + tokenResponse.accessToken;
+            return callback(null, authorizationValue);
+        });
+    };
+
+    var credentials = new KeyVault.KeyVaultCredentials(authenticator);
+    var keyVaultClient = new KeyVault.KeyVaultClient(credentials);
+
+    keyVaultClient.getSecret(secretIdentifier, function (err, secretBundle) {
+        if (err) throw err;
+        options.connectionString = secretBundle.value;
+        done();
+    });
+});
+
+gulp.task("upload", ["get-connectionstring-from-secret", "dist", "test"], function () {
+
+    var buildVer = argv.version || "";
+    if (!buildVer) {
+        console.error("missing build version argument (--version)!");
+        process.exit(1);
+    }
+
+    const assetBundles = [
+        {
+            glob: distDir + "*.schema.json",
+            dest: `v${buildVer}/manifest`
+        },
+        {
+            glob: distDir + "*.js",
+            dest: `v${buildVer}/js`
+        },
+        {
+            glob: distDir + "*.d.ts",
+            dest: `v${buildVer}/types`
+        }
+    ];
+
+    var uploadTasks = assetBundles.map(function(assetBundle) {
+        return gulp.src(assetBundle.glob).pipe(deployCdn({
+            containerName: "sdk", // container name in blob
+            serviceOptions: [options.connectionString], // custom arguments to azure.createBlobService
+            folder: assetBundle.dest, // path within container
+            zip: true, // gzip files if they become smaller after zipping, content-encoding header will change if file is zipped
+            deleteExistingBlobs: false, // true means recursively deleting anything under folder
+            concurrentUploadThreads: 4, // number of concurrent uploads, choose best for your network condition
+            metadata: {
+                cacheControl: "public, max-age=31536000", // cache in browser for 1 year
+            },
+            testRun: argv.whatIf || false // test run - means no blobs will be actually deleted or uploaded, see log messages for details
+        }));
+    });
+
+    return merge(...uploadTasks).on("error", gutil.log);
 });
