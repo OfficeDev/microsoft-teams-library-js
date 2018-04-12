ad3f33362f86ba3ecae57d897e8e78e69886251b
Sachin Gupta <saching@microsoft.com>
2018-04-12 16:29:09 -0700

    rebase to master resolving conflicts

--- src/MicrosoftTeams.ts
+++ src/MicrosoftTeams.ts
@@ -850,13 +850,13 @@ namespace microsoftTeams {
         link.href,
         "_blank",
         "toolbar=no, location=yes, status=no, menubar=no, scrollbars=yes, top=" +
-          top +
-          ", left=" +
-          left +
-          ", width=" +
-          width +
-          ", height=" +
-          height
+        top +
+        ", left=" +
+        left +
+        ", width=" +
+        width +
+        ", height=" +
+        height
       );
       if (childWindow) {
         // Start monitoring the authentication window so that we can detect if it gets closed before the flow completes
