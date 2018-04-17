16db604166bc0739dba26d0f60915ac2d9659d66
Sachin Gupta <saching@microsoft.com>
2018-04-17 15:34:39 -0700

    reverting formatign change sin gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,22 @@
 "use strict";
 
-var del = require("del"),
-  fs = require("fs"),
-  gulp = require("gulp"),
-  gutil = require("gulp-util"),
-  umd = require('gulp-umd'),
-  karma = require("karma").Server,
-  merge = require("merge2"),
-  header = require('gulp-header'),
-  tslint = require("gulp-tslint"),
-  typescript = require("gulp-typescript"),
-  rename = require("gulp-rename"),
-  uglify = require("gulp-uglify"),
-  deployCdn = require("gulp-deploy-azure-cdn"),
-  KeyVault = require("azure-keyvault"),
-  argv = require("yargs")
-    .option('version', { type: 'string' }) // version may look like a number, so force it to be a string
-    .argv,
-  AuthenticationContext = require("adal-node").AuthenticationContext;
+var del = require("del");
+var fs = require("fs");
+var gulp = require("gulp");
+var gutil = require("gulp-util");
+var umd = require('gulp-umd');
+var karma = require("karma").Server;
+var merge = require("merge2");
+var tslint = require("gulp-tslint");
+var header = require('gulp-header');
+var typescript = require("gulp-typescript");
+var rename = require("gulp-rename");
+var uglify = require("gulp-uglify");
+var deployCdn = require("gulp-deploy-azure-cdn");
+var prettierPlugin = require("gulp-prettier-plugin");
+var KeyVault = require("azure-keyvault");
+var argv = require("yargs").option("version", { type: "string" }).argv; // version may look like a number, so force it to be a string
+var AuthenticationContext = require("adal-node").AuthenticationContext;
 
 var buildDir = "./build/";
 var distDir = "./dist/";
@@ -45,9 +44,17 @@ gulp.task("tslint", function () {
     }));
 });
 
+gulp.task("prettier", () =>
+  gulp
+    .src(["./src/**/*.ts", "./test/**/*.ts", "./gulpfile.js"])
+    .pipe(prettierPlugin(undefined, { filter: true }))
+    // passing a function that returns base will write the files in-place
+    .pipe(gulp.dest(file => file.base))
+);
+
 var tsProject = typescript.createProject("./tsconfig.json", {
   // Point to the specific typescript package we pull in, not a machine-installed one
-  typescript: require("typescript"),
+  typescript: require("typescript")
 });
 
 gulp.task("ts", ["tslint"], function () {
@@ -96,17 +103,13 @@ gulp.task("dist", ["ts", "doc"], function () {
     "./src/**/*.schema.json",
   ];
 
-  return gulp.src(distFiles)
-    .pipe(gulp.dest(distDir));
+  return gulp.src(distFiles).pipe(gulp.dest(distDir));
 });
 
-gulp.task("default", ["ts", "test", "doc", "dist"]);
+gulp.task("default", ["prettier", "ts", "test", "doc", "dist"]);
 
 gulp.task("clean", function () {
-  return del([
-    buildDir,
-    distDir
-  ]);
+  return del([buildDir, distDir]);
 });
 
 /// tasks for uploading dist assets to CDN
@@ -118,18 +121,26 @@ gulp.task("get-connectionstring-from-secret", function (done) {
   var secretIdentifier = vaultUri + "/secrets/" + secretName + "/";
 
   if (!clientId || !clientSecret || !vaultUri || !secretName) {
-    console.error("missing required inputs for 'get-connectionstring-form-secret'");
+    console.error(
+      "missing required inputs for 'get-connectionstring-form-secret'"
+    );
     process.exit(1);
   }
 
-  // Authenticator - retrieves the access token 
+  // Authenticator - retrieves the access token
   var authenticator = function (challenge, callback) {
     var context = new AuthenticationContext(challenge.authorization);
-    return context.acquireTokenWithClientCredentials(challenge.resource, clientId, clientSecret, function (err, tokenResponse) {
-      if (err) throw err;
-      var authorizationValue = tokenResponse.tokenType + " " + tokenResponse.accessToken;
-      return callback(null, authorizationValue);
-    });
+    return context.acquireTokenWithClientCredentials(
+      challenge.resource,
+      clientId,
+      clientSecret,
+      function (err, tokenResponse) {
+        if (err) throw err;
+        var authorizationValue =
+          tokenResponse.tokenType + " " + tokenResponse.accessToken;
+        return callback(null, authorizationValue);
+      }
+    );
   };
 
   var credentials = new KeyVault.KeyVaultCredentials(authenticator);
@@ -142,43 +153,46 @@ gulp.task("get-connectionstring-from-secret", function (done) {
   });
 });
 
-gulp.task("upload", ["get-connectionstring-from-secret", "dist", "test"], function () {
-
-  var buildVer = argv.version || "";
-  if (!buildVer) {
-    console.error("missing build version argument (--version)!");
-    process.exit(1);
-  }
-
-  const assetBundles = [
-    {
-      glob: distDir + "*.schema.json",
-      dest: `v${buildVer}/manifest`
-    },
-    {
-      glob: distDir + "*.js",
-      dest: `v${buildVer}/js`
-    },
-    {
-      glob: distDir + "*.d.ts",
-      dest: `v${buildVer}/types`
+gulp.task(
+  "upload",
+  ["get-connectionstring-from-secret", "dist", "test"],
+  function () {
+    var buildVer = argv.version || "";
+    if (!buildVer) {
+      console.error("missing build version argument (--version)!");
+      process.exit(1);
     }
-  ];
 
-  var uploadTasks = assetBundles.map(function (assetBundle) {
-    return gulp.src(assetBundle.glob).pipe(deployCdn({
-      containerName: "sdk", // container name in blob
-      serviceOptions: [options.connectionString], // custom arguments to azure.createBlobService
-      folder: assetBundle.dest, // path within container
-      zip: true, // gzip files if they become smaller after zipping, content-encoding header will change if file is zipped
-      deleteExistingBlobs: false, // true means recursively deleting anything under folder
-      concurrentUploadThreads: 4, // number of concurrent uploads, choose best for your network condition
-      metadata: {
-        cacheControl: "public, max-age=31536000", // cache in browser for 1 year
+    const assetBundles = [
+      {
+        glob: distDir + "*.schema.json",
+        dest: `v${buildVer}/manifest`
       },
-      testRun: argv.whatIf || false // test run - means no blobs will be actually deleted or uploaded, see log messages for details
-    }));
-  });
+      {
+        glob: distDir + "*.js",
+        dest: `v${buildVer}/js`
+      },
+      {
+        glob: distDir + "*.d.ts",
+        dest: `v${buildVer}/types`
+      }
+    ];
 
-  return merge(...uploadTasks).on("error", gutil.log);
-});
+    var uploadTasks = assetBundles.map(function (assetBundle) {
+      return gulp.src(assetBundle.glob).pipe(deployCdn({
+        containerName: "sdk", // container name in blob
+        serviceOptions: [options.connectionString], // custom arguments to azure.createBlobService
+        folder: assetBundle.dest, // path within container
+        zip: true, // gzip files if they become smaller after zipping, content-encoding header will change if file is zipped
+        deleteExistingBlobs: false, // true means recursively deleting anything under folder
+        concurrentUploadThreads: 4, // number of concurrent uploads, choose best for your network condition
+        metadata: {
+          cacheControl: "public, max-age=31536000", // cache in browser for 1 year
+        },
+        testRun: argv.whatIf || false // test run - means no blobs will be actually deleted or uploaded, see log messages for details
+      }));
+    });
+
+    return merge(...uploadTasks).on("error", gutil.log);
+  }
+);
