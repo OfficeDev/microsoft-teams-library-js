16812956d348cba400398382fd1277581dfd5dd2
Sachin Gupta <saching@microsoft.com>
2018-06-29 17:48:09 -0700

    fixing build error

--- src/MicrosoftTeams.ts
+++ src/MicrosoftTeams.ts
@@ -1,9 +1,9 @@
 declare interface String {
-  startsWith(search: string, pos: number): boolean;
+  startsWith(search: string, pos?: number): boolean;
 }
 
 if (!String.prototype.startsWith) {
-  String.prototype.startsWith = function (search: string, pos: number): boolean {
+  String.prototype.startsWith = function (search: string, pos?: number): boolean {
     return this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search;
   };
 }
