a329194dfcb03479b72384cc62e23423ac599bae
Yuri Dogandjiev <yudogan@microsoft.com>
2018-08-21 23:34:21 +0300

    Prepare to release v1.3.4

--- test/MicrosoftTeams.spec.ts
+++ test/MicrosoftTeams.spec.ts
@@ -129,7 +129,7 @@ describe("MicrosoftTeams", () => {
     expect(initMessage.id).toBe(0);
     expect(initMessage.func).toBe("initialize");
     expect(initMessage.args.length).toEqual(1);
-    expect(initMessage.args[0]).toEqual("1.3.3");
+    expect(initMessage.args[0]).toEqual("1.3.4");
   });
 
   it("should allow multiple initialize calls", () => {
