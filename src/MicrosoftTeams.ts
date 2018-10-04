ec885ddaa95a94a9b33e4fcbdd7bd9f6442c711d
Xin Gao <xig@microsoft.com>
2018-10-04 10:36:56 -0700

    Prepare to release verson 1.3.6

--- src/MicrosoftTeams.ts
+++ src/MicrosoftTeams.ts
@@ -44,7 +44,7 @@ interface Window {
 namespace microsoftTeams {
   "use strict";
 
-  const version = "1.3.5";
+  const version = "1.3.6";
 
   const validOrigins = [
     "https://teams.microsoft.com",
