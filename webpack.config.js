b9a3e1314f31f4969d109807afb55459d10eefc2
Sachin Gupta <hapysachin91@gmail.com>
2018-11-02 12:05:19 -0700

    Saching/web pack improvement (#148)
    * exporting as * and as well as object
    
    * fix build error
    
    * adding backward compatibility
    
    * fixing UT
    
    * removing unused comment
    
    * updating readme for build instractions

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const libraryName = 'microsoftTeams';
 
 module.exports = {
   entry: {
@@ -9,6 +10,7 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
+    library: libraryName,
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
