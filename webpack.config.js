9f18809f87014481f1af079decd5a1366a521fb2
Sachin Gupta <saching@microsoft.com>
2019-03-05 11:44:41 -0800

    addign uglijs inline false option

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const libraryName = 'microsoftTeams';
+const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const libraryName = 'microsoftTeams'
 
 module.exports = {
   entry: {
-    'MicrosoftTeams': './src/index.ts',
+    MicrosoftTeams: './src/index.ts',
     'MicrosoftTeams.min': './src/index.ts'
   },
   output: {
@@ -14,21 +14,32 @@ module.exports = {
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   resolve: {
     extensions: ['.tsx', '.ts', '.js']
   },
   module: {
-    rules: [{
-      test: /\.tsx?$/,
-      use: 'ts-loader',
-      exclude: /node_modules/
-    }]
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
   },
   optimization: {
     minimize: true,
-    minimizer: [new UglifyJsPlugin({
-      include: /\.min\.js$/
-    })]
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          /*
+            inlining is broken sometimes where inlined function uses the same variable name as inlining function.
+            See https://github.com/mishoo/UglifyJS2/issues/2842, https://github.com/mishoo/UglifyJS2/issues/2843
+         */
+          compress: { inline: false }
+        },
+        include: /\.min\.js$/
+      })
+    ]
   }
-};
+}
