07c537cfb08ef858e9fd290d8d7c1dab5da23cd7
Sachin Gupta <saching@microsoft.com>
2019-03-22 14:18:13 -0700

    adding promise based api

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export * from "./private/index";
 export * from "./public/index";
+import * as MicrosoftTeams from "./public/index.promise";
+export { MicrosoftTeams };
---
+++ src/public/index.promise.ts
@@ -0,0 +1,7 @@
+export * from "./authentication.Promise";
+export * from "./constants";
+export * from "./interfaces";
+export * from "./publicAPIs.Promise";
+export * from "./settings";
+export * from "./tasks";
+export * from "./appWindow";
