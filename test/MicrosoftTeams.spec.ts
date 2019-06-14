417ecafbfc981828d235f416d8dbdb4dc88dcca7
Sachin Gupta <saching@microsoft.com>
2019-06-13 22:58:51 -0700

    Prepare to release version 1.4.3

--- test/MicrosoftTeams.spec.ts
+++ test/MicrosoftTeams.spec.ts
@@ -119,7 +119,7 @@ describe("MicrosoftTeams", () => {
     expect(initMessage.id).toBe(0);
     expect(initMessage.func).toBe("initialize");
     expect(initMessage.args.length).toEqual(1);
-    expect(initMessage.args[0]).toEqual("1.4.2");
+    expect(initMessage.args[0]).toEqual("1.4.3");
   });
 
   it("should allow multiple initialize calls", () => {
