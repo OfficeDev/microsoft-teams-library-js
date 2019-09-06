b11799893f1112ca047f205c4a5f4c39990b4477
Rohith Jidagam <rojidaga@microsoft.com>
2019-09-06 13:39:01 -0700

    Testing Azure deploy

--- deploy-to-cdn.js
+++ deploy-to-cdn.js
@@ -1,13 +1,14 @@
+const { argv } = require('yargs');
 const { deploy } = require('deploy-azure-cdn');
-const fs = require('fs-jetpack');
-const { argv } = require("yargs")
 const KeyVault = require('azure-keyvault');
+const fs = require('fs-jetpack');
 const AuthenticationContext = require('adal-node').AuthenticationContext;
 
 const url = argv.staticsUri;
 const clientId = argv.clientId;
 const clientSecret = argv.clientSecret;
 const vaultUri = argv.vaultUri;
+const secretName = argv.vaultSecretName;
 
 function getConnectionString() {
   var authenticator = function(challenge, callback) {
@@ -24,10 +25,7 @@ function getConnectionString() {
 
   var credentials = new KeyVault.KeyVaultCredentials(authenticator);
   var keyVaultClient = new KeyVault.KeyVaultClient(credentials);
-  var secretName = 'VersionedBuildContainerUrl';
-  var secretIdentifier = vaultUri + '/secrets/' + secretName + '/';
-  console.log(secretIdentifier)
-  keyVaultClient.getSecret(vaultUri, secretName, '').then(res => console.log(res));
+  return keyVaultClient.getSecret(vaultUri, secretName, '').then(res => res.value);
 }
 
 (async () => {
@@ -39,24 +37,26 @@ function getConnectionString() {
   const files = await fs.listAsync('./dist');
   const logger = console.log;
 
-  console.log(getConnectionString())
+  getConnectionString().then(connectionString => {
 
-  // const opts = {
-  //   serviceOptions: [getConnectionString()], // custom arguments to azure.createBlobService
-  //   containerName: 'sdk', // container name in blob
-  //   containerOptions: { publicAccessLevel: 'blob' }, // container options
-  //   folder: 'v' + version + '/js', // path within container
-  //   deleteExistingBlobs: true, // true means recursively deleting anything under folder
-  //   concurrentUploadThreads: 2, // number of concurrent uploads, choose best for your network condition
-  //   zip: true, // gzip files if they become smaller after zipping, content-encoding header will change if file is zipped
-  //   metadata: { cacheControl: 'public, max-age=31556926' }, // metadata for each uploaded file
-  //   testRun: false, // test run - means no blobs will be actually deleted or uploaded, see log messages for details
-  // };
+    console.log(connectionString)
+    const opts = {
+      serviceOptions: [connectionString], // custom arguments to azure.createBlobService
+      containerName: 'sdk', // container name in blob
+      containerOptions: { publicAccessLevel: 'blob' }, // container options
+      folder: 'v' + version + '/js', // path within container
+      deleteExistingBlobs: true, // true means recursively deleting anything under folder
+      concurrentUploadThreads: 2, // number of concurrent uploads, choose best for your network condition
+      zip: true, // gzip files if they become smaller after zipping, content-encoding header will change if file is zipped
+      metadata: { cacheControl: 'public, max-age=31556926' }, // metadata for each uploaded file
+      testRun: false, // test run - means no blobs will be actually deleted or uploaded, see log messages for details
+    };
 
-  // deploy(opts, files, logger, function(err) {
-  //   if (err) {
-  //     console.log('Error deploying', err);
-  //   }
-  //   console.log('Deployment Successful.');
-  // });
+    deploy(opts, files, logger, function(err) {
+      if (err) {
+        console.log('Error deploying', err);
+      }
+      console.log('Deployment Successful.');
+    });
+  });
 })();
