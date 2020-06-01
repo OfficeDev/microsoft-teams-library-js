61e9cbe9d58a617c312921f9b88fc8fb39ba9f53
Anand V Nath <anandnat@microsoft.com>
2020-06-01 13:58:45 +0530

    Moved getImages API out of devices namespace. Renamed it to captureImage

--- src/public/index.ts
+++ src/public/index.ts
@@ -35,4 +35,4 @@ export {
 export { settings } from './settings';
 export { tasks } from './tasks';
 export { ChildAppWindow, IAppWindow, ParentAppWindow } from './appWindow';
-export { device } from './device';
+export { captureImage, ErrorCode, File, FileFormat } from './media';
