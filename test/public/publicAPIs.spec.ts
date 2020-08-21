1982da69ab04d8806ed4838de233ca176cb5091d
Stephen Houston <sthousto@microsoft.com>
2020-08-21 15:59:49 -0700

    version number update

--- test/public/publicAPIs.spec.ts
+++ test/public/publicAPIs.spec.ts
@@ -934,7 +934,7 @@ describe('MicrosoftTeams-publicAPIs', () => {
     expect(initMessage.id).toBe(0);
     expect(initMessage.func).toBe('initialize');
     expect(initMessage.args.length).toEqual(1);
-    expect(initMessage.args[0]).toEqual('1.7.0');
+    expect(initMessage.args[0]).toEqual('1.8.0-beta.2');
     let message = utils.findMessageByFunc('setFrameContext');
     expect(message).not.toBeNull();
     expect(message.args.length).toBe(1);
