3c92c57ee324c4a840151088b387b3a289103a69
Shengming <shex@microsoft.com>
2020-09-28 20:59:22 -0700

    add meeting namespace and required APIs to enable send command to meeting room

---
+++ src/private/meeting.ts
@@ -0,0 +1,164 @@
+import { ensureInitialized, sendMessageRequestToParent } from '../internal/internalAPIs';
+import { GlobalVars } from '../internal/globalVars';
+
+export namespace meeting {
+  /**
+   * @private
+   * Hide from docs
+   * Data structure to represent a meeting room.
+   */
+  export interface MeetingRoomInfo {
+    /**
+        endpoint id of the meeting room.
+        */
+    endpointId: string;
+    /**
+        device name of the meeting room.
+        */
+    deviceName: string;
+    /**
+        client type of the meeting room.
+        */
+    clientType: string;
+    /**
+        client version of the meeting room.
+        */
+    clientVersion: string;
+  }
+  /**
+   * @private
+   * Hide from docs
+   * Data structure to represent capabilities of a meeting room.
+   */
+  export interface MeetingRoomCapability {
+    /**
+     * media control capabilities, value can be "toggleMute", "toggleCamera", "toggleCaptions", "volume".
+     */
+    mediaControls: string[];
+    /**
+     * main stage layout control capabilities, value can be "showVideoGallery", "showContent", "showVideoGalleryAndContent", "showLargeGallery", "showTogether".
+     */
+    stageLayoutControls: string[];
+    /**
+     * meeting control capabilities, value can be "leaveMeeting".
+     */
+    meetingControls: string[];
+  }
+
+  /**
+   * @private
+   * Hide from docs
+   * Data structure to represent states of a meeting room.
+   */
+  export interface MeetingRoomState {
+    /**
+     * current mute state, true: mute, false: unmute.
+     */
+    toggleMute: boolean;
+    /**
+     * current camera state, true: camera on, false: camera off.
+     */
+    toggleCamera: boolean;
+    /**
+     * current captions state, true: captions on, false: captions off.
+     */
+    toggleCaptions: boolean;
+    /**
+     * current main stage layout state, value can be one of "Gallery", "Content + gallery", "Content", "Large gallery" and "Together mode".
+     */
+    stageLayout: string;
+    /**
+     * current leaveMeeting state, true: leave, false: no-op.
+     */
+    leaveMeeting: boolean;
+  }
+
+  let meetingRoomCapabilitiesUpdateHandler: (capabilities: MeetingRoomCapability) => void;
+  GlobalVars.handlers['meeting.meetingRoomCapabilitiesUpdate'] = handleMeetingRoomCapabilitiesUpdate;
+  let meetingRoomStatesUpdateHandler: (states: MeetingRoomState) => void;
+  GlobalVars.handlers['meeting.meetingRoomStatesUpdate'] = handleMeetingRoomStatesUpdate;
+
+  /**
+   * @private
+   * Hide from docs
+   * Fetch the meeting room info that paired with current client.
+   * @param callback Callback to invoke when the meeting room info is fetched.
+   */
+  export function getPairedMeetingRoomInfo(callback: (meetingRoomInfo: MeetingRoomInfo) => void): void {
+    ensureInitialized();
+    const messageId = sendMessageRequestToParent('meeting.getPairedMeetingRoomInfo');
+    GlobalVars.callbacks[messageId] = callback;
+  }
+
+  /**
+   * @private
+   * Hide from docs
+   * Send a command to paired meeting room.
+   * @param commandName The command name.
+   * @param callback Callback to invoke when the command response returns.
+   */
+  export function sendCommandToPairedMeetingRoom(
+    commandName: string,
+    callback: (errorCode: number, message?: string) => void,
+  ): void {
+    if (!commandName || commandName.length == 0) {
+      throw new Error('[meeting.sendCommandToPairedMeetingRoom] Command name cannot be null or empty');
+    }
+    if (!callback) {
+      throw new Error('[meeting.sendCommandToPairedMeetingRoom] Callback cannot be null');
+    }
+    ensureInitialized();
+    const messageId = sendMessageRequestToParent('meeting.sendCommandToPairedMeetingRoom', [commandName]);
+    GlobalVars.callbacks[messageId] = callback;
+  }
+
+  /**
+   * @private
+   * Hide from docs
+   * Registers a handler for meeting room capabilities update.
+   * Only one handler can be registered at a time. A subsequent registration replaces an existing registration.
+   * @param handler The handler to invoke when the capabilities of meeting room update.
+   */
+  export function registerMeetingRoomCapabilitiesUpdateHandler(
+    handler: (capabilities: MeetingRoomCapability) => void,
+  ): void {
+    if (!handler) {
+      throw new Error('[meeting.registerMeetingRoomCapabilitiesUpdateHandler] Handler cannot be null');
+    }
+    ensureInitialized();
+    meetingRoomCapabilitiesUpdateHandler = handler;
+    handler && sendMessageRequestToParent('registerHandler', ['meeting.meetingRoomCapabilitiesUpdate']);
+  }
+
+  /**
+   * @private
+   * Hide from docs
+   * Registers a handler for meeting room states update.
+   * Only one handler can be registered at a time. A subsequent registration replaces an existing registration.
+   * @param handler The handler to invoke when the states of meeting room update.
+   */
+  export function registerMeetingRoomStatesUpdateHandler(handler: (states: MeetingRoomState) => void): void {
+    if (!handler) {
+      throw new Error('[meeting.registerMeetingRoomStatesUpdateHandler] Handler cannot be null');
+    }
+    ensureInitialized();
+    meetingRoomStatesUpdateHandler = handler;
+    handler && sendMessageRequestToParent('registerHandler', ['meeting.meetingRoomStatesUpdate']);
+  }
+
+  function handleMeetingRoomCapabilitiesUpdate(capabilities: MeetingRoomCapability): void {
+    if (meetingRoomCapabilitiesUpdateHandler != null) {
+      ensureInitialized();
+      meetingRoomCapabilitiesUpdateHandler(capabilities);
+      sendMessageRequestToParent('meeting.handleMeetingRoomCapabilitiesUpdate', [capabilities]);
+    }
+  }
+
+  function handleMeetingRoomStatesUpdate(states: MeetingRoomState): void {
+    if (meetingRoomStatesUpdateHandler != null) {
+      ensureInitialized();
+      meetingRoomStatesUpdateHandler(states);
+      sendMessageRequestToParent('meeting.handleMeetingRoomStatesUpdate', [states]);
+    }
+  }
+}
---
+++ test/private/meeting.spec.ts
@@ -0,0 +1,267 @@
+import { Utils } from '../utils';
+import { FramelessPostMocks } from '../framelessPostMocks';
+import { meeting } from '../../src/private/meeting'
+import { _initialize, _uninitialize } from '../../src/public/publicAPIs';
+import { DOMMessageEvent } from '../../src/internal/interfaces';
+
+describe('meetings', () => {
+  const mobilePlatformMock = new FramelessPostMocks();
+  const desktopPlatformMock = new Utils()
+  const meetingRoomInfo: meeting.MeetingRoomInfo = {endpointId: "123-456", deviceName: "conference room 001",clientType: "norden", clientVersion: "v-2020-09-28"};
+  const meetingRoomCapability: meeting.MeetingRoomCapability = {mediaControls: ["toggleMute", "toggleCamera"], stageLayoutControls: ["showVideoGallery", "showContent"], meetingControls: []};
+  const meetingRoomState: meeting.MeetingRoomState = {toggleMute: true, toggleCamera: false, toggleCaptions: false, stageLayout: "Gallery", leaveMeeting: false};
+
+  beforeEach(() => {
+    mobilePlatformMock.messages = [];
+    desktopPlatformMock.messages = [];
+
+    // Set a mock window for testing
+    _initialize(mobilePlatformMock.mockWindow);
+  });
+
+  afterEach(() => {
+    // Reset the object since it's a singleton
+    if (_uninitialize) {
+      _uninitialize();
+    }
+  });
+
+  let emptyCallback = () => {};
+
+  describe('getPairedMeetingRoomInfo', () => {
+    it('should not allow calls before initialization', () => {
+      expect(() =>
+        meeting.getPairedMeetingRoomInfo((meetingRoomInfo: meeting.MeetingRoomInfo) => {}),
+      ).toThrowError('The library has not yet been initialized');
+    });
+
+    it('should successfully get meeting room info on mobile', () => {
+      mobilePlatformMock.initializeWithContext('content');
+
+      let handlerInvoked = false;
+      let returnedMeetingRoomInfo: meeting.MeetingRoomInfo = null;
+      meeting.getPairedMeetingRoomInfo((meetingRoomInfo: meeting.MeetingRoomInfo) => {
+        handlerInvoked = true;
+        returnedMeetingRoomInfo = meetingRoomInfo;
+      });
+
+      let message = mobilePlatformMock.findMessageByFunc('meeting.getPairedMeetingRoomInfo');
+      expect(message).not.toBeNull();
+      expect(message.args.length).toBe(0);
+
+      let callbackId = message.id;
+      mobilePlatformMock.respondToMessage({
+        data: {
+          id: callbackId,
+          args: [meetingRoomInfo]
+        }
+      } as DOMMessageEvent)
+
+      expect(handlerInvoked).toBeTruthy();
+      expect(returnedMeetingRoomInfo.endpointId).toBe(meetingRoomInfo.endpointId);
+      expect(returnedMeetingRoomInfo.deviceName).toBe(meetingRoomInfo.deviceName);
+      expect(returnedMeetingRoomInfo.clientType).toBe(meetingRoomInfo.clientType);
+      expect(returnedMeetingRoomInfo.clientVersion).toBe(meetingRoomInfo.clientVersion);
+    });
+
+    it('should allow getPairedMeetingRoomInfo calls on desktop', () => {
+      desktopPlatformMock.initializeWithContext('content');
+
+      meeting.getPairedMeetingRoomInfo(emptyCallback);
+
+      let message = desktopPlatformMock.findMessageByFunc('meeting.getPairedMeetingRoomInfo');
+      expect(message).not.toBeNull();
+      expect(message.args.length).toBe(0);
+    });
+  });
+
+  describe('sendCommandToPairedMeetingRoom', () => {
+    it('should not allow calls before initialization', () => {
+      expect(() => meeting.sendCommandToPairedMeetingRoom("mute", emptyCallback)).toThrowError(
+        'The library has not yet been initialized'
+      )
+    });
+
+    it('should not allow calls with null command name', () => {
+      mobilePlatformMock.initializeWithContext('content');
+      expect(() => meeting.sendCommandToPairedMeetingRoom(null, emptyCallback)).toThrowError(
+        '[meeting.sendCommandToPairedMeetingRoom] Command name cannot be null or empty'
+      )
+    });
+
+    it('should not allow calls with empty command name', () => {
+      mobilePlatformMock.initializeWithContext('content');
+      expect(() => meeting.sendCommandToPairedMeetingRoom("", emptyCallback)).toThrowError(
+        '[meeting.sendCommandToPairedMeetingRoom] Command name cannot be null or empty'
+      )
+    });
+
+    it('should not allow calls with null callback', () => {
+      mobilePlatformMock.initializeWithContext('content');
+      expect(() => meeting.sendCommandToPairedMeetingRoom("mute", null)).toThrowError(
+        '[meeting.sendCommandToPairedMeetingRoom] Callback cannot be null'
+      )
+    });
+
+    it('should successfully send commands on mobile', () => {
+      mobilePlatformMock.initializeWithContext('content');
+
+      let handlerInvoked = false;
+      let error, msg;
+      meeting.sendCommandToPairedMeetingRoom("mute", (errorCode: number, message?: string) => {
+        handlerInvoked = true;
+        error = errorCode;
+        msg = message;
+      });
+
+      let message = mobilePlatformMock.findMessageByFunc('meeting.sendCommandToPairedMeetingRoom');
+      expect(message).not.toBeNull();
+      expect(message.args.length).toBe(1);
+      expect(message.args[0]).toBe('mute');
+
+      let callbackId = message.id;
+      mobilePlatformMock.respondToMessage({
+        data: {
+          id: callbackId,
+          args: [1, "command failed"]
+        }
+      } as DOMMessageEvent)
+
+      expect(handlerInvoked).toBeTruthy();
+      expect(error).toBe(1);
+      expect(msg).toBe("command failed");
+    });
+
+    it('should successfully send commands on desktop', () => {
+      desktopPlatformMock.initializeWithContext('content');
+
+      meeting.sendCommandToPairedMeetingRoom("mute", emptyCallback);
+
+      let message = desktopPlatformMock.findMessageByFunc('meeting.sendCommandToPairedMeetingRoom');
+      expect(message).not.toBeNull();
+      expect(message.args.length).toBe(1);
+      expect(message.args[0]).toBe('mute');
+    });
+  });
+
+  describe('registerMeetingRoomCapabilitiesUpdateHandler', () => {
+    it('should not allow calls with null callback ', () => {
+      expect(() => meeting.registerMeetingRoomCapabilitiesUpdateHandler(null)).toThrowError(
+        '[meeting.registerMeetingRoomCapabilitiesUpdateHandler] Handler cannot be null'
+      )
+    });
+
+    it('should not allow calls before initialization ', () => {
+      expect(() => meeting.registerMeetingRoomCapabilitiesUpdateHandler(emptyCallback)).toThrowError(
+        'The library has not yet been initialized'
+      )
+    });
+
+    it('should successful register capabilities update handler on mobile', () => {
+      mobilePlatformMock.initializeWithContext('content');
+
+      let handlerInvoked = false;
+      let returnedCapabilities;
+      meeting.registerMeetingRoomCapabilitiesUpdateHandler((capabilities: meeting.MeetingRoomCapability) => {
+        handlerInvoked = true;
+        returnedCapabilities = capabilities;
+      });
+
+      let messageForRegister = mobilePlatformMock.findMessageByFunc('registerHandler');
+      expect(messageForRegister).not.toBeNull();
+      expect(messageForRegister.args.length).toBe(1);
+      expect(messageForRegister.args[0]).toBe('meeting.meetingRoomCapabilitiesUpdate');
+
+      mobilePlatformMock.respondToMessage({
+        data: {
+          func: "meeting.meetingRoomCapabilitiesUpdate",
+          args: [meetingRoomCapability]
+        }
+      } as DOMMessageEvent)
+
+    
+      expect(handlerInvoked).toBeTruthy();
+      expect(returnedCapabilities.mediaControls).toBe(meetingRoomCapability.mediaControls);
+      expect(returnedCapabilities.stageLayoutControls).toBe(meetingRoomCapability.stageLayoutControls);
+      expect(returnedCapabilities.meetingControls).toBe(meetingRoomCapability.meetingControls);
+
+      let messageForHandlerCallback = mobilePlatformMock.findMessageByFunc('meeting.handleMeetingRoomCapabilitiesUpdate');
+      expect(messageForHandlerCallback).not.toBeNull();
+      expect(messageForHandlerCallback.args.length).toBe(1);
+      expect(messageForHandlerCallback.args[0]).toEqual(meetingRoomCapability);
+    });
+
+    it('should successful register capabilities update handler on desktop', () => {
+      desktopPlatformMock.initializeWithContext('content');
+
+
+      meeting.registerMeetingRoomCapabilitiesUpdateHandler(emptyCallback);
+
+      let messageForRegister = desktopPlatformMock.findMessageByFunc('registerHandler');
+      expect(messageForRegister).not.toBeNull();
+      expect(messageForRegister.args.length).toBe(1);
+      expect(messageForRegister.args[0]).toBe('meeting.meetingRoomCapabilitiesUpdate');
+    });
+  });
+
+  describe('registerMeetingRoomStatesUpdateHandler', () => {
+    it('should not allow calls with null callback ', () => {
+      expect(() => meeting.registerMeetingRoomStatesUpdateHandler(null)).toThrowError(
+        '[meeting.registerMeetingRoomStatesUpdateHandler] Handler cannot be null'
+      )
+    });
+
+    it('should not allow calls before initialization ', () => {
+      expect(() => meeting.registerMeetingRoomStatesUpdateHandler(emptyCallback)).toThrowError(
+        'The library has not yet been initialized'
+      )
+    });
+
+    it('should successful register states update handler on mobile', () => {
+      mobilePlatformMock.initializeWithContext('content');
+
+      let handlerInvoked = false;
+      let returnedStates;
+      meeting.registerMeetingRoomStatesUpdateHandler((states: meeting.MeetingRoomState) => {
+        handlerInvoked = true;
+        returnedStates = states;
+      });
+
+      let messageForRegister = mobilePlatformMock.findMessageByFunc('registerHandler');
+      expect(messageForRegister).not.toBeNull();
+      expect(messageForRegister.args.length).toBe(1);
+      expect(messageForRegister.args[0]).toBe('meeting.meetingRoomStatesUpdate');
+
+      mobilePlatformMock.respondToMessage({
+        data: {
+          func: "meeting.meetingRoomStatesUpdate",
+          args: [meetingRoomState]
+        }
+      } as DOMMessageEvent)
+
+    
+      expect(handlerInvoked).toBeTruthy();
+      expect(returnedStates.toggleMute).toBe(meetingRoomState.toggleMute);
+      expect(returnedStates.toggleCamera).toBe(meetingRoomState.toggleCamera);
+      expect(returnedStates.toggleCaptions).toBe(meetingRoomState.toggleCaptions);
+      expect(returnedStates.stageLayout).toBe(meetingRoomState.stageLayout);
+      expect(returnedStates.leaveMeeting).toBe(meetingRoomState.leaveMeeting);
+
+      let messageForHandlerCallback = mobilePlatformMock.findMessageByFunc('meeting.handleMeetingRoomStatesUpdate');
+      expect(messageForHandlerCallback).not.toBeNull();
+      expect(messageForHandlerCallback.args.length).toBe(1);
+      expect(messageForHandlerCallback.args[0]).toEqual(meetingRoomState);
+    });
+
+    it('should successful register states update handler on desktop', () => {
+      desktopPlatformMock.initializeWithContext('content');
+
+      meeting.registerMeetingRoomStatesUpdateHandler(emptyCallback);
+
+      let messageForRegister = desktopPlatformMock.findMessageByFunc('registerHandler');
+      expect(messageForRegister).not.toBeNull();
+      expect(messageForRegister.args.length).toBe(1);
+      expect(messageForRegister.args[0]).toBe('meeting.meetingRoomStatesUpdate');
+    });
+  });
+});
