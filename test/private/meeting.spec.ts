335563d335f99a9c38735267ebb6db20ed78bec5
Ronak Bakshi <robakshi@microsoft.com>
2020-12-10 17:29:44 -0800

    Update meetingAudio namespace to meeting

---
+++ test/private/meeting.spec.ts
@@ -0,0 +1,102 @@
+<<<<<<< HEAD:test/public/meeting.spec.ts
+import { meeting } from "../../src/public/meeting";
+=======
+import { meeting } from "../../src/private/meeting";
+>>>>>>> 15ab5bd... Update meetingAudio namespace to meeting:test/private/meeting.spec.ts
+import { SdkError, ErrorCode } from "../../src/public/interfaces";
+import { DOMMessageEvent } from "../../src/internal/interfaces";
+import { FramelessPostMocks } from "../framelessPostMocks";
+import { _initialize, _uninitialize } from "../../src/public/publicAPIs";
+
+describe("meeting", () => {
+  const desktopPlatformMock = new FramelessPostMocks();
+
+  beforeEach(() => {
+    desktopPlatformMock.messages = [];
+    _initialize(desktopPlatformMock.mockWindow);
+  });
+
+  afterEach(() => {
+    // Reset the object since it's a singleton
+    if (_uninitialize) {
+      _uninitialize();
+    }
+  });
+
+  describe("toggleIncomingClientAudio", () => {
+    it('should not allow toggle incoming client audio calls with null callback', () => {
+      expect(() => meeting.toggleIncomingClientAudio(null)).toThrowError(
+        '[toggle incoming client audio] Callback cannot be null',
+      );
+    });
+    it("should not allow calls before initialization", () => {
+      expect(() =>
+        meeting.toggleIncomingClientAudio(() => {
+          return;
+        })
+      ).toThrowError("The library has not yet been initialized");
+    });
+
+    it("should successfully toggle the incoming client audio", () => {
+      desktopPlatformMock.initializeWithContext("content");
+
+      let callbackCalled = false;
+      let returnedSdkError: SdkError | null;
+      let returnedResult: boolean | null;
+<<<<<<< HEAD:test/public/meeting.spec.ts
+      meeting.toggleIncomingClientAudio((error: SdkError, result: boolean) => {
+=======
+      meeting.toggleIncomingClientAudio((responseObject: meeting.IToggleClientAudio) => {
+>>>>>>> 15ab5bd... Update meetingAudio namespace to meeting:test/private/meeting.spec.ts
+        callbackCalled = true;
+        returnedResult = result;
+        returnedSdkError = error;
+      });
+
+      let toggleIncomingClientAudioMessage = desktopPlatformMock.findMessageByFunc("toggleIncomingClientAudio");
+      expect(toggleIncomingClientAudioMessage).not.toBeNull();
+      let callbackId = toggleIncomingClientAudioMessage.id;
+      // desktopPlatformMock.respondToMessage(toggleIncomingClientAudioMessage, {});
+      desktopPlatformMock.respondToMessage({
+        data: {
+          id: callbackId,
+          args: [null, true],
+        }
+      } as DOMMessageEvent);
+      expect(callbackCalled).toBe(true);
+      expect(returnedSdkError).toBeNull();
+      expect(returnedResult).toBe(true);
+    });
+
+    it("should return error code 500", () => {
+      desktopPlatformMock.initializeWithContext("content");
+
+      let callbackCalled = false;
+      let returnedSdkError: SdkError | null;
+      let returnedResult: boolean | null;
+<<<<<<< HEAD:test/public/meeting.spec.ts
+      meeting.toggleIncomingClientAudio((error: SdkError, result: boolean) => {
+=======
+      meeting.toggleIncomingClientAudio((responseObject: meeting.IToggleClientAudio) => {
+>>>>>>> 15ab5bd... Update meetingAudio namespace to meeting:test/private/meeting.spec.ts
+        callbackCalled = true;
+        returnedResult = result;
+        returnedSdkError = error;
+      });
+
+      let toggleIncomingClientAudioMessage = desktopPlatformMock.findMessageByFunc("toggleIncomingClientAudio");
+      expect(toggleIncomingClientAudioMessage).not.toBeNull();
+      let callbackId = toggleIncomingClientAudioMessage.id;
+      desktopPlatformMock.respondToMessage({
+        data: {
+          id: callbackId,
+          args: [{ errorCode: ErrorCode.INTERNAL_ERROR }, null]
+        }
+      } as DOMMessageEvent);
+      expect(callbackCalled).toBe(true);
+      expect(returnedSdkError).not.toBeNull();
+      expect(returnedSdkError).toEqual({ errorCode: ErrorCode.INTERNAL_ERROR });
+      expect(returnedResult).toBe(null);
+    });
+  });
+});
