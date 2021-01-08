8472b4e06dff6c8543d7f6532fc67bf0bd19e235
anthonychui <anthonychui@microsoft.com>
2021-01-08 12:59:33 -0800

    Add isPartialResponse to message when passing from parent to child frame

--- src/internal/interfaces.ts
+++ src/internal/interfaces.ts
@@ -31,12 +31,13 @@ export interface ExtendedWindow extends Window {
 export interface MessageRequest {
   id?: number;
   func: string;
-  args?: any[]; // tslint:disable-line:no-any The args here are a passthrough to postMessage where we do allow any[]
+  args?: any[]; // tslint:disable-line:no-any The args here are a passthrough to postMessage where wes do allow any[]
 }
 
 export interface MessageResponse {
   id: number;
   args?: any[]; // tslint:disable-line:no-any The args here are a passthrough from OnMessage where we do receive any[]
+  isPartialResponse?: boolean;
 }
 
 /**
