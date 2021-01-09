6f035008c8e85a0ebfe408045a4b1a49941ea08a
anthonychui <anthonychui@microsoft.com>
2021-01-08 16:41:28 -0800

    Fix typo

--- src/internal/interfaces.ts
+++ src/internal/interfaces.ts
@@ -31,7 +31,7 @@ export interface ExtendedWindow extends Window {
 export interface MessageRequest {
   id?: number;
   func: string;
-  args?: any[]; // tslint:disable-line:no-any The args here are a passthrough to postMessage where wes do allow any[]
+  args?: any[]; // tslint:disable-line:no-any The args here are a passthrough to postMessage where we do allow any[]
 }
 
 export interface MessageResponse {
