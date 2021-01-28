0c212619cef1e368b10a512a67d779a67c5c62cd
Maggie <mgong90@gmail.com>
2021-01-28 15:11:26 -0800

    [Task-2055] Implement deepLink and auth in MOS Test App (#32)

--- examples/teams-js-test-app/src/App.tsx
+++ examples/teams-js-test-app/src/App.tsx
@@ -10,6 +10,10 @@ const App = () => {
   const [context, setContext] = React.useState("");
   const [auth, setAuth] = React.useState("");
   const [executeDeepLink, setExecuteDeepLink] = React.useState("");
+  const [shareDeepLink, setShareDeepLink] = React.useState("");
+  const [authenticationNotifyFailure, setAuthenticationNotifyFailure] = React.useState("");
+  const [authenticationNotifySucess, setAuthenticationNotifySucess] = React.useState("");
+  const [authenticationAuthenticate, setAuthenticationAuthenticate] = React.useState("");
 
   const returnContext = () => {
     let textResult = "No Context";
@@ -45,6 +49,38 @@ const App = () => {
     };
     core.executeDeepLink(deepLink, onComplete);
   };
+  
+  const returnShareDeepLink = (deepLinkParams: any) => {
+    deepLinkParams = JSON.parse(deepLinkParams);
+    core.shareDeepLink(deepLinkParams);
+    // TODO: return a feedback for users 
+  };
+
+  const returnAuthenticationNotifyFailure = (reason?: string, callbackUrl?: string) => {
+    authentication.notifyFailure(reason, callbackUrl);
+     // TODO: return a feedback for users 
+  };
+
+  const returnAuthenticationNotifySucess = (result?: string, callbackUrl?: string) => {
+    authentication.notifySuccess(result, callbackUrl);
+     // TODO: return a feedback for users 
+  };
+
+  const returnAuthenticationAuthenticate = (authenticateParameters: any) => {
+    let textResult = "";
+    authenticateParameters = JSON.parse(authenticateParameters);
+    try {
+      authenticateParameters.successCallback = (token: string) => {
+        setAuth("Success: " + token);
+      };
+      authenticateParameters.failureCallback = (reason: string) => {
+        setAuth("Failure: " + reason);
+      };
+    } catch(e) {
+       setAuthenticationAuthenticate(textResult);
+    }
+    authentication.authenticate(authenticateParameters);
+  };
 
   return (
     <>
@@ -68,6 +104,34 @@ const App = () => {
         hasInput={true}
         title="Execute Deep Link"
         name="executeDeepLink"
+      />
+      <BoxAndButton
+        handleClick={returnShareDeepLink}
+        output={shareDeepLink}
+        hasInput={true}
+        title="Share Deep Link"
+        name="ShareDeepLink"
+      />
+      <BoxAndButton
+        handleClick={returnAuthenticationNotifyFailure}
+        output={authenticationNotifyFailure}
+        hasInput={true}
+        title="authentication.notifyFailure"
+        name="authentication.notifyFailure"
+      />
+      <BoxAndButton
+        handleClick={returnAuthenticationNotifySucess}
+        output={authenticationNotifySucess}
+        hasInput={true}
+        title="authentication.notifySucess"
+        name="authentication.notifySucess"
+      />
+      <BoxAndButton
+        handleClick={returnAuthenticationAuthenticate}
+        output={authenticationAuthenticate}
+        hasInput={true}
+        title="authentication.authenticate"
+        name="authentication.authenticate"
       />
     </>
   );
