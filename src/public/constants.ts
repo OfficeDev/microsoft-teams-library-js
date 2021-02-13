2874242f119f18c26bc270867b0409c2c203f79b
kpyapali <39844422+kpyapali@users.noreply.github.com>
2021-02-12 18:49:40 -0800

    Add surfaceHub2 platform to HostClientType
    Add surfaceHub2 platform to HostClientType.
    Use this value for SurfaceHub 2s specific scenarios/workflows.

--- src/public/constants.ts
+++ src/public/constants.ts
@@ -4,6 +4,7 @@ export enum HostClientType {
   android = 'android',
   ios = 'ios',
   rigel = 'rigel',
+  surfaceHub2 = 'surfaceHub2',
 }
 
 // Ensure these declarations stay in sync with the framework.
