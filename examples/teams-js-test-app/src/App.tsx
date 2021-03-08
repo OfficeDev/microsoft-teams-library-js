4a7a1c8c1626e48269da2ad04718fe7997ccd194
Marcin Juraszek <mail@marcinjuraszek.com>
2021-03-08 15:47:09 -0800

    Implement App Initialization flow in Test App (#83)
    Implement App Initialization flow in Test App

--- examples/teams-js-test-app/src/App.tsx
+++ examples/teams-js-test-app/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react';
 import './App.css';
 import { core, appInitialization } from '@microsoft/teamsjs-app-sdk';
+import AppInitializationAPIs from './components/AppInitialization';
 import AuthenticationAPIs from './components/AuthenticationAPIs';
 import CalendarAPIs from './components/CalendarAPIs';
 import ConversationsAPIs from './components/ConversationsAPIs';
@@ -16,13 +17,24 @@ import MailAPIs from './components/MailAPIs';
 import NotificationAPIs from './components/privateApis/NotificationAPIs';
 
 core.initialize();
-appInitialization.notifyAppLoaded();
+
+// for AppInitialization tests we need a way to stop the Test App from sending these
+// we do it by adding appInitializationTest=true to query string
+const urlParams = new URLSearchParams(window.location.search);
+if (urlParams.has('appInitializationTest') && urlParams.get('appInitializationTest')) {
+  console.info('Not calling appInitialization because part of App Initialization Test run');
+} else {
+  appInitialization.notifyAppLoaded();
+  appInitialization.notifySuccess();
+}
+
 export const noHubSdkMsg = ' was called, but there was no response from the Hub SDK.';
 
 const App = (): ReactElement => {
   return (
     <>
       <AuthenticationAPIs />
+      <AppInitializationAPIs />
       <CalendarAPIs />
       <MailAPIs />
       <ConversationsAPIs />
