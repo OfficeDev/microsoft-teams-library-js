0fcf00c563e1bfb68eec9b00088adafe80b9127b
Vivek Kaarthek <vivra@microsoft.com>
2021-03-22 16:02:13 +0530

    Add documentation

--- src/private/files.ts
+++ src/private/files.ts
@@ -9,6 +9,12 @@ import { FileOpenPreference, SdkError } from '../public';
  * Hide from docs
  */
 export namespace files {
+  /**
+   * @private
+   * Hide from docs
+   * 
+   * Cloud storage providers registered with Microsoft Teams
+   */
   export enum CloudStorageProviderCode {
     Dropbox = 'DROPBOX',
     Box = 'BOX',
@@ -17,12 +23,24 @@ export namespace files {
     Egnyte = 'EGNYTE',
   }
 
+  /**
+   * @private
+   * Hide from docs
+   * 
+   * Cloud storage provider integration type
+   */
   export enum CloudStorageProviderType {
     Sharepoint = 0,
     WopiIntegration = 1,
     Google = 2,
   }
 
+  /**
+   * @private
+   * Hide from docs
+   * 
+   * Cloud storage folder interface
+   */
   export interface CloudStorageFolder {
     id: string;
     title: string;
@@ -36,6 +54,14 @@ export namespace files {
     accessType?: string;
   }
 
+  /**
+   * @private
+   * Hide from docs
+   * 
+   * Gets a list of cloud storage folders added to the channel
+   * @param channelId ID of the channel whose cloud storage folders should be retrieved
+   * @param callback Callback that will be triggered post folders load
+   */
   export function getCloudStorageFolders(
     channelId: string,
     callback: (error: SdkError, folders: CloudStorageFolder[]) => void,
@@ -51,6 +77,14 @@ export namespace files {
     sendMessageToParent('files.getCloudStorageFolders', [channelId], callback);
   }
 
+  /**
+   * @private
+   * Hide from docs
+   * 
+   * Initiates the add cloud storage folder flow
+   * @param channelId ID of the channel to add cloud storage folder
+   * @param callback Callback that will be triggered post add folder flow is compelete
+   */
   export function addCloudStorageFolder(
     channelId: string,
     callback: (error: SdkError, isFolderAdded: boolean, folders: CloudStorageFolder[]) => void,
@@ -66,6 +100,15 @@ export namespace files {
     sendMessageToParent('files.addCloudStorageFolder', [channelId], callback);
   }
 
+  /**
+   * @private
+   * Hide from docs
+   * 
+   * Deletes a cloud storage folder from channel
+   * @param channelId ID of the channel where folder is to be deleted
+   * @param folderToDelete cloud storage folder to be deleted
+   * @param callback Callback that will be triggered post delete
+   */
   export function deleteCloudStorageFolder(
     channelId: string,
     folderToDelete: CloudStorageFolder,
