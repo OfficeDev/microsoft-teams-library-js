d7c1e042b6990637063adb55d83e150aad605314
Jason Chong <jasonchongg@gmail.com>
2021-04-19 13:58:31 -0700

    fulltrust capabilities (#107)
    * fulltrust capabilities
    
    * addressed comments
    
    * address comments
    
    * append res back
    
    * renamed handler fn
    
    Co-authored-by: Jason Chong <jasonchong@microsoft.com>

--- examples/teams-js-test-app/src/components/PrivateAPIs.tsx
+++ examples/teams-js-test-app/src/components/PrivateAPIs.tsx
@@ -3,9 +3,7 @@ import {
   ChatMembersInformation,
   FilePreviewParameters,
   getChatMembers,
-  getUserJoinedTeams,
   openFilePreview,
-  UserJoinedTeamsInformation,
 } from '@microsoft/teamsjs-app-sdk';
 import BoxAndButton from './BoxAndButton';
 import { noHubSdkMsg } from '../App';
@@ -13,7 +11,6 @@ import { noHubSdkMsg } from '../App';
 const PrivateAPIs = (): ReactElement => {
   const [openFilePreviewRes, setOpenFilePreviewRes] = React.useState('');
   const [getChatMembersRes, setGetChatMembersRes] = React.useState('');
-  const [getUserJoinedTeamsRes, setGetUserJoinedTeamsRes] = React.useState('');
 
   const returnOpenFilePreview = (filePreviewParamsInput: string): void => {
     let filePreviewParams: FilePreviewParameters = JSON.parse(filePreviewParamsInput);
@@ -27,15 +24,6 @@ const PrivateAPIs = (): ReactElement => {
       setGetChatMembersRes(JSON.stringify(chatMembersInformation));
     };
     getChatMembers(onComplete);
-  };
-
-  const returnGetUserJoinedTeams = (teamInstanceParamsInput: string): void => {
-    let teamInstanceParams = JSON.parse(teamInstanceParamsInput);
-    setGetUserJoinedTeamsRes('getUserJoinedTeams()' + noHubSdkMsg);
-    const onComplete = (userJoinedTeamsInfo: UserJoinedTeamsInformation): void => {
-      setGetUserJoinedTeamsRes(JSON.stringify(userJoinedTeamsInfo));
-    };
-    getUserJoinedTeams(onComplete, teamInstanceParams);
   };
 
   return (
@@ -53,13 +41,6 @@ const PrivateAPIs = (): ReactElement => {
         hasInput={false}
         title="Get Chat Members"
         name="getChatMembers"
-      />
-      <BoxAndButton
-        handleClickWithInput={returnGetUserJoinedTeams}
-        output={getUserJoinedTeamsRes}
-        hasInput={true}
-        title="Get User Joined Teams"
-        name="getUserJoinedTeams"
       />
     </>
   );
