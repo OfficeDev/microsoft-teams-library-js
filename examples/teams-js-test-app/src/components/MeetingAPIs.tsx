b7e4178a156d3dc380ad6a3858d573d71fc061f5
Angela Eun <69540388+aeunms@users.noreply.github.com>
2021-04-26 12:42:38 -0700

    Add some meeting APIs to test app (#111)
    * add more meeting APIs to test app
    
    * introduce constants into meetingAPIs

--- examples/teams-js-test-app/src/components/MeetingAPIs.tsx
+++ examples/teams-js-test-app/src/components/MeetingAPIs.tsx
@@ -8,7 +8,12 @@ const MeetingAPIs = (): ReactElement => {
   const [toggleIncomingClientAudioRes, setToggleIncomingClientAudioRes] = React.useState('');
   const [getMeetingDetailsRes, setGetMeetingDetailsRes] = React.useState('');
   const [getAuthenticationTokenRes, setGetAuthenticationTokenRes] = React.useState('');
+  const [getLiveStreamStateRes, setGetLiveStreamStateRes] = React.useState('');
+  const [requestStartLiveStreamingRes, setRequestStartLiveStreamingRes] = React.useState('');
+  const [requestStopLiveStreamingRes, setRequestStopLiveStreamingRes] = React.useState('');
+  const [registerLiveStreamChangedHandlerRes, setRegisterLiveStreamChangedHandlerRes] = React.useState('');
   const [checkMeetingCapabilityRes, setCheckMeetingCapabilityRes] = React.useState('');
+  const NULL = 'null';
 
   const getIncomingClientAudioState = (): void => {
     setGetIncomingClientAudioStateRes('getIncomingClientAudioState()' + noHubSdkMsg);
@@ -62,6 +67,77 @@ const MeetingAPIs = (): ReactElement => {
     });
   };
 
+  const getLiveStreamState = (): void => {
+    setGetLiveStreamStateRes('meeting.getLiveStreamState()' + noHubSdkMsg);
+    const callback = (error: SdkError | null, liveStreamState: meeting.LiveStreamState | null): void => {
+      if (error) {
+        setGetLiveStreamStateRes(JSON.stringify(error));
+      } else {
+        liveStreamState
+          ? setGetLiveStreamStateRes(liveStreamState.isStreaming.toString())
+          : setGetLiveStreamStateRes(NULL);
+      }
+    };
+    meeting.getLiveStreamState(callback);
+  };
+
+  const requestStartLiveStreaming = (input: string): void => {
+    let streamInput;
+    const STREAM_URL = 'streamUrl';
+    const STREAM_KEY = 'streamKey';
+
+    try {
+      streamInput = JSON.parse(input);
+    } catch (error) {
+      setRequestStartLiveStreamingRes(
+        `Please JSON format your input. Your input should be JSON formatted containing at least a ${STREAM_URL} and an optional ${STREAM_KEY}. For example, {"${STREAM_URL}": "https://bing.com"}`,
+      );
+      return;
+    }
+
+    if (streamInput.hasOwnProperty(STREAM_URL)) {
+      setRequestStartLiveStreamingRes('meeting.requestStartLiveStreaming()' + noHubSdkMsg);
+      const callback = (error: SdkError | null, liveStreamState: meeting.LiveStreamState | null): void => {
+        if (error) {
+          setRequestStartLiveStreamingRes(JSON.stringify(error));
+        } else {
+          liveStreamState
+            ? setRequestStartLiveStreamingRes(liveStreamState.isStreaming.toString())
+            : setRequestStartLiveStreamingRes(NULL);
+        }
+      };
+      streamInput.hasOwnProperty(STREAM_KEY)
+        ? meeting.requestStartLiveStreaming(callback, streamInput.get(STREAM_URL), streamInput.get(STREAM_KEY))
+        : meeting.requestStartLiveStreaming(callback, streamInput.get(STREAM_URL));
+    } else {
+      setRequestStartLiveStreamingRes(
+        `Please include a ${STREAM_URL}. Your input should be JSON formatted containing at least a ${STREAM_URL} and an optional ${STREAM_KEY}. For example, {"${STREAM_URL}": "https://bing.com"}`,
+      );
+    }
+  };
+
+  const requestStopLiveStreaming = (): void => {
+    setRequestStopLiveStreamingRes('meeting.requestStopLiveStreaming' + noHubSdkMsg);
+    const callback = (error: SdkError | null, liveStreamState: meeting.LiveStreamState | null): void => {
+      if (error) {
+        setRequestStopLiveStreamingRes(JSON.stringify(error));
+      } else {
+        liveStreamState
+          ? setRequestStopLiveStreamingRes(liveStreamState.isStreaming.toString())
+          : setRequestStopLiveStreamingRes(NULL);
+      }
+    };
+    meeting.requestStopLiveStreaming(callback);
+  };
+
+  const registerLiveStreamChangedHandler = (): void => {
+    setRegisterLiveStreamChangedHandlerRes('meeting.registerLiveStreamChangedHandler' + noHubSdkMsg);
+    const handler = (liveStreamState: meeting.LiveStreamState): void => {
+      setRegisterLiveStreamChangedHandlerRes('Live StreamState changed to ' + liveStreamState.isStreaming.toString());
+    };
+    meeting.registerLiveStreamChangedHandler(handler);
+  };
+
   const meetingCapabilityCheck = (): void => {
     if (meeting.isSupported()) {
       setCheckMeetingCapabilityRes('Meeting module is supported');
@@ -99,6 +175,34 @@ const MeetingAPIs = (): ReactElement => {
         hasInput={false}
         title="Get Auth Token For Anonymous User"
         name="getAuthTokenForAnonymousUser"
+      />
+      <BoxAndButton
+        handleClick={getLiveStreamState}
+        output={getLiveStreamStateRes}
+        hasInput={false}
+        title="Get LiveStream State"
+        name="getLiveStreamState"
+      />
+      <BoxAndButton
+        handleClickWithInput={requestStartLiveStreaming}
+        output={requestStartLiveStreamingRes}
+        hasInput={true}
+        title="Request Start LiveStreaming"
+        name="requestStartLiveStreaming"
+      />
+      <BoxAndButton
+        handleClickWithInput={requestStopLiveStreaming}
+        output={requestStopLiveStreamingRes}
+        hasInput={false}
+        title="Request Stop LiveStreaming"
+        name="requestStopLiveStreaming"
+      />
+      <BoxAndButton
+        handleClickWithInput={registerLiveStreamChangedHandler}
+        output={registerLiveStreamChangedHandlerRes}
+        hasInput={false}
+        title="Register LiveStream Changed Handler"
+        name="registerLiveStreamChangedHandler"
       />
       <BoxAndButton
         handleClick={meetingCapabilityCheck}
