fbe7f23b8e14f4168e2a1760adda9a2dbd982505
Angela Eun <69540388+aeunms@users.noreply.github.com>
2021-06-14 15:24:18 -0700

    set up eslintrc system for entire repo (#144)
    makes app sdk follow its original eslintrc settings in its own eslintrc file and every other project follows one eslintrc file in the repo root.

--- teams-js-app-sdk/.eslintrc.js
+++ teams-js-app-sdk/.eslintrc.js
@@ -1,16 +1,15 @@
 module.exports = {
+  root: true,
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    'plugin:react/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
   },
-  plugins: ['@typescript-eslint', 'react-hooks'],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     '@typescript-eslint/no-use-before-define': 'off',
@@ -23,15 +22,5 @@ module.exports = {
         allowExpressions: true,
       },
     ],
-    'curly': 'error',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'react/prop-types': 'off',
-  },
-  settings: {
-    react: {
-      'pragma': 'React',
-      'version': 'detect',
-    }
   },
 };
