516cd2440c1a5d74416dea961977b02de1a29714
Christian Harper-Cyr <charpercyr@gmail.com>
2021-06-16 14:39:23 -0700

    Readd tests that were deleted by the merge

--- teams-js-app-sdk/test/private/teams.spec.ts
+++ teams-js-app-sdk/test/private/teams.spec.ts
@@ -1,9 +1,9 @@
 import { core } from '../../src/public';
 import { Utils } from '../utils';
-import { teams } from '../../src/private/teams'
+import { teams } from '../../src/private/teams';
+import { TeamInstanceParameters } from '../../src/private/interfaces';
 
-
-describe('teams', () => {
+describe('teamsjsAppSDK-privateAPIs', () => {
   // Use to send a mock message from the app.
   const utils = new Utils();
   const emptyCallback = () => {};
@@ -24,6 +24,107 @@ describe('teams', () => {
     if (core._uninitialize) {
       core._uninitialize();
     }
+  });
+  it('should exist in the global namespace', () => {
+    expect(core).toBeDefined();
+  });
+  describe('getUserJoinedTeams', () => {
+    it('should not allow calls before initialization', () => {
+      expect(() =>
+        teams.fullTrust.getUserJoinedTeams(() => {
+          return;
+        }),
+      ).toThrowError('The library has not yet been initialized');
+    });
+
+    it('should allow a valid optional parameter set to true', () => {
+      utils.initializeWithContext('content');
+
+      let callbackCalled: boolean = false;
+      teams.fullTrust.getUserJoinedTeams(
+        () => {
+          callbackCalled = true;
+        },
+        { favoriteTeamsOnly: true } as TeamInstanceParameters,
+      );
+
+      let getUserJoinedTeamsMessage = utils.findMessageByFunc('getUserJoinedTeams');
+      expect(getUserJoinedTeamsMessage).not.toBeNull();
+      utils.respondToMessage(getUserJoinedTeamsMessage, {});
+      expect(callbackCalled).toBe(true);
+    });
+
+    it('should allow a valid optional parameter set to false', () => {
+      utils.initializeWithContext('content');
+
+      let callbackCalled: boolean = false;
+      teams.fullTrust.getUserJoinedTeams(
+        () => {
+          callbackCalled = true;
+        },
+        { favoriteTeamsOnly: false } as TeamInstanceParameters,
+      );
+
+      let getUserJoinedTeamsMessage = utils.findMessageByFunc('getUserJoinedTeams');
+      expect(getUserJoinedTeamsMessage).not.toBeNull();
+      utils.respondToMessage(getUserJoinedTeamsMessage, {});
+      expect(callbackCalled).toBe(true);
+    });
+
+    it('should allow a missing optional parameter', () => {
+      utils.initializeWithContext('content');
+
+      let callbackCalled: boolean = false;
+      teams.fullTrust.getUserJoinedTeams(() => {
+        callbackCalled = true;
+      });
+
+      let getUserJoinedTeamsMessage = utils.findMessageByFunc('getUserJoinedTeams');
+      expect(getUserJoinedTeamsMessage).not.toBeNull();
+      utils.respondToMessage(getUserJoinedTeamsMessage, {});
+      expect(callbackCalled).toBe(true);
+    });
+
+    it('should allow a missing and valid optional parameter', () => {
+      utils.initializeWithContext('content');
+
+      let callbackCalled: boolean = false;
+      teams.fullTrust.getUserJoinedTeams(
+        () => {
+          callbackCalled = true;
+        },
+        {} as TeamInstanceParameters,
+      );
+
+      let getUserJoinedTeamsMessage = utils.findMessageByFunc('getUserJoinedTeams');
+      expect(getUserJoinedTeamsMessage).not.toBeNull();
+      utils.respondToMessage(getUserJoinedTeamsMessage, {});
+      expect(callbackCalled).toBe(true);
+    });
+  });
+
+  describe('getConfigSetting', () => {
+    it('should not allow calls before initialization', () => {
+      expect(() =>
+        teams.fullTrust.getConfigSetting(() => {
+          return;
+        }, 'key'),
+      ).toThrowError('The library has not yet been initialized');
+    });
+
+    it('should allow a valid parameter', () => {
+      utils.initializeWithContext('content');
+
+      let callbackCalled: boolean = false;
+      teams.fullTrust.getConfigSetting(() => {
+        callbackCalled = true;
+      }, 'key');
+
+      let getConfigSettingMessage = utils.findMessageByFunc('getConfigSetting');
+      expect(getConfigSettingMessage).not.toBeNull();
+      utils.respondToMessage(getConfigSettingMessage, {});
+      expect(callbackCalled).toBe(true);
+    });
   });
   describe('getTeamChannels', () => {
     it('should not allow calls before initialization', () => {
