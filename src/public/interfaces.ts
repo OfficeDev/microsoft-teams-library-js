4cf28203bb4aefb516c4411e4e86536657496e81
Debajyoti Chowdhury <57507664+debajyoti251088@users.noreply.github.com>
2021-07-27 11:19:14 -0700

    Add the app launch id to the getContext call. (#619)
    Co-authored-by: dechowdh <dechowdh@MININT-Q5LAEPA>
    Co-authored-by: Sachin Gupta <hapysachin91@gmail.com>

--- src/public/interfaces.ts
+++ src/public/interfaces.ts
@@ -376,6 +376,11 @@ export interface Context {
   appSessionId?: string;
 
   /**
+   * ID for the current visible app which is different for across cached sessions. Used for correlating telemetry data``
+   */
+  appLaunchId?: string;
+
+  /**
    * Represents whether calling is allowed for the current logged in User
    */
   isCallingAllowed?: boolean;
