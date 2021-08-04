049f7116f0ec56c909f8df873ad90337054ee4b6
Corey Shaw <coshaw@microsoft.com>
2021-08-04 15:21:49 -0700

    Update path for dev release script, add "dev" qualifier to version (#234)

--- tools/cli/prepNextRelease.js
+++ tools/cli/prepNextRelease.js
@@ -1,13 +1,13 @@
-const fs = require("fs");
+const fs = require('fs');
 
-let packageJsonPath = "../../teamsjs-app-sdk/package.json";
+let packageJsonPath = '../../packages/teamsjs-app-sdk/package.json';
 const EXIT_CODE_FATAL_ERROR = 5;
 
 function getPackageJson() {
   if (fs.existsSync(packageJsonPath)) {
-    return JSON.parse(fs.readFileSync(packageJsonPath, { encoding: "utf8" }));
+    return JSON.parse(fs.readFileSync(packageJsonPath, { encoding: 'utf8' }));
   }
-  console.log("FATAL ERROR: package.json path could not be found in the file system.");
+  console.log('FATAL ERROR: package.json path could not be found in the file system.');
   process.exitCode = EXIT_CODE_FATAL_ERROR;
   return;
 }
@@ -18,7 +18,7 @@ function savePackageJson(packageJson) {
 
 function getFileVersion(packageJson) {
   if (!packageJson.version) {
-    console.log("FATAL ERROR: a version was not found in the package.json.");
+    console.log('FATAL ERROR: a version was not found in the package.json.');
     process.exitCode = EXIT_CODE_FATAL_ERROR;
     return;
   } else {
@@ -29,23 +29,23 @@ function getFileVersion(packageJson) {
 let packageJson = getPackageJson();
 
 // SHA of the commit the package was generated from
-let hash = process.env["BUILD_SOURCEVERSION"];
+let hash = process.env['BUILD_SOURCEVERSION'];
 
 // pre-release version part, either from the hash or random if has not available
 let suffixLength = 10;
-let versionSuffix = hash.substr(0, suffixLength);
+let versionSuffix = 'dev.' + hash.substr(0, suffixLength);
 
-console.log("version suffix: " + versionSuffix);
+console.log('version suffix: ' + versionSuffix);
 
 // get package version from package.json
 let version = getFileVersion(packageJson);
 
-console.log("package.json version: " + version);
+console.log('package.json version: ' + version);
 
 // append the suffix to form a new version
-let nextVersion = version + "-" + versionSuffix;
+let nextVersion = version + '-' + versionSuffix;
 
-console.log("@next version: " + nextVersion);
+console.log('@next version: ' + nextVersion);
 
 // update package.json with the new version
 packageJson.version = nextVersion;
