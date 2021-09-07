b5948e20c51f5fd66c4d4e8be4ce588b07696513
Lakhveer Kaur <lakhveerkaur@microsoft.com>
2021-09-07 16:24:35 -0700

    Updating typescript

--- src/public/publicAPIs.ts
+++ src/public/publicAPIs.ts
@@ -59,37 +59,6 @@ export function initialize(callback?: () => void, validMessageOrigins?: string[]
     authentication.initialize();
     settings.initialize();
     initializePrivateApis();
-
-    // Undocumented function used to clear state between unit tests
-    this._uninitialize = () => {
-      if (GlobalVars.frameContext) {
-        registerOnThemeChangeHandler(null);
-        registerFullScreenHandler(null);
-        registerBackButtonHandler(null);
-        registerBeforeUnloadHandler(null);
-        registerFocusEnterHandler(null);
-        registerOnLoadHandler(null);
-        logs.registerGetLogHandler(null);
-      }
-
-      if (GlobalVars.frameContext === FrameContexts.settings) {
-        settings.registerOnSaveHandler(null);
-      }
-
-      if (GlobalVars.frameContext === FrameContexts.remove) {
-        settings.registerOnRemoveHandler(null);
-      }
-
-      GlobalVars.initializeCalled = false;
-      GlobalVars.initializeCompleted = false;
-      GlobalVars.initializeCallbacks = [];
-      GlobalVars.additionalValidOrigins = [];
-      GlobalVars.frameContext = null;
-      GlobalVars.hostClientType = null;
-      GlobalVars.isFramelessWindow = false;
-
-      uninitializeCommunication();
-    };
   }
 
   // Handle additional valid message origins if specified
@@ -124,6 +93,37 @@ export function _initialize(hostWindow: any): void {
  */
 export function _uninitialize(): void {
   //used to clear state between unit tests
+
+  if (!GlobalVars.initializeCalled) {
+    return;
+  }
+  if (GlobalVars.frameContext) {
+    registerOnThemeChangeHandler(null);
+    registerFullScreenHandler(null);
+    registerBackButtonHandler(null);
+    registerBeforeUnloadHandler(null);
+    registerFocusEnterHandler(null);
+    registerOnLoadHandler(null);
+    logs.registerGetLogHandler(null);
+  }
+
+  if (GlobalVars.frameContext === FrameContexts.settings) {
+    settings.registerOnSaveHandler(null);
+  }
+
+  if (GlobalVars.frameContext === FrameContexts.remove) {
+    settings.registerOnRemoveHandler(null);
+  }
+
+  GlobalVars.initializeCalled = false;
+  GlobalVars.initializeCompleted = false;
+  GlobalVars.initializeCallbacks = [];
+  GlobalVars.additionalValidOrigins = [];
+  GlobalVars.frameContext = null;
+  GlobalVars.hostClientType = null;
+  GlobalVars.isFramelessWindow = false;
+
+  uninitializeCommunication();
 }
 
 /**
