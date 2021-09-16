5aa7ccb6d6de5e7e68ff05652ef4d79225e454f3
Luis Gonzalez <luisgon@microsoft.com>
2021-09-16 14:26:23 -0700

    Update mail.ts
    Updating mail.ts to match mail.ts from the Hub sdk.
    
    We are updating the compose mail API to support reply / reply all / forward, we are using a discriminated union to restrict the parameters per scenario.
    
    Hub PR: https://office.visualstudio.com/ISS/_git/teams-js-hub-sdk/pullrequest/991131

--- packages/teams-js-app-sdk/src/public/mail.ts
+++ packages/teams-js-app-sdk/src/public/mail.ts
@@ -12,14 +12,17 @@ export namespace mail {
       resolve(sendAndHandleError('mail.openMailItem', openMailItemParams));
     });
   }
-  export function composeMail(composeMailParams: ComposeMailParams): Promise<void> {
+
+  export function composeMail(composeScenarioParams: ComposeScenarioParams): Promise<void> {
     return new Promise<void>(resolve => {
       ensureInitialized(FrameContexts.content);
       if (!isSupported()) throw 'Not Supported';
 
-      resolve(sendAndHandleError('mail.composeMail', composeMailParams));
+      resolve(sendAndHandleError('mail.composeMail', composeScenarioParams));
     });
   }
+
+
   export function isSupported(): boolean {
     return runtime.supports.mail ? true : false;
   }
@@ -28,11 +31,48 @@ export namespace mail {
     itemId: string;
   }
 
-  export interface ComposeMailParams {
-    toRecipients?: string[];
-    ccRecipients?: string[];
-    bccRecipients?: string[];
-    subject?: string;
-    message?: string;
-  }
+/**
+* Properties shared between compose scenarios
+*/
+interface ComposeProps {
+  toRecipients?: string[];
+  ccRecipients?: string[];
+  bccRecipients?: string[];
+  subject?: string;
+  message?: string;
+}
+
+export enum ComposeMailType {
+  New = 'new',
+  Reply = 'reply',
+  ReplyAll = 'replyAll',
+  Forward = 'forward',
+}
+
+/**
+ * Base of a discriminated union between compose scenarios.
+ */
+ interface ComposeMailBase<T extends ComposeMailType> {
+  type: T;
+}
+/**
+ * Interfaces for each type.
+ */
+export interface ComposeNewParams extends ComposeMailBase<ComposeMailType.New> {
+  toRecipients?: string[];
+  ccRecipients?: string[];
+  bccRecipients?: string[];
+  subject?: string;
+  message?: string;
+}
+export interface ComposeReplyOrForwardParams<T extends ComposeMailType> extends ComposeMailBase<T> {
+  itemid: string;
+}
+
+export type ComposeScenarioParams =
+  | ComposeNewParams
+  | ComposeReplyOrForwardParams<ComposeMailType.Reply>
+  | ComposeReplyOrForwardParams<ComposeMailType.ReplyAll>
+  | ComposeReplyOrForwardParams<ComposeMailType.Forward>;
+
 }
