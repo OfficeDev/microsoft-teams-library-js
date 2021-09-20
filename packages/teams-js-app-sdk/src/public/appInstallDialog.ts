3f734a35fd0277d696fdd5aece49a334af04fd63
Christian Harper-Cyr <charpercyr@gmail.com>
2021-09-20 15:10:02 -0700

    Update require FrameContexts

--- packages/teams-js-app-sdk/src/public/appInstallDialog.ts
+++ packages/teams-js-app-sdk/src/public/appInstallDialog.ts
@@ -10,7 +10,14 @@ export namespace appInstallDialog {
 
   export function openAppInstallDialog(openAPPInstallDialogParams: OpenAppInstallDialogParams): Promise<void> {
     return new Promise(resolve => {
-      ensureInitialized(FrameContexts.content);
+      ensureInitialized(
+        FrameContexts.content,
+        FrameContexts.sidePanel,
+        FrameContexts.settings,
+        FrameContexts.task,
+        FrameContexts.stage,
+        FrameContexts.meetingStage,
+      );
       if (!isSupported()) throw 'Not supported';
       sendMessageToParent('appInstallDialog.openAppInstallDialog', [openAPPInstallDialogParams]);
       resolve();
