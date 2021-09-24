f8fab7280ae43bbc52e64f2cf55d9b8d86237c32
Lakhveer Kaur <lakhveerkaur@microsoft.com>
2021-09-24 16:08:27 -0700

    TsDoc style comments in app.ts file

--- packages/teams-js-app-sdk/src/public/app.ts
+++ packages/teams-js-app-sdk/src/public/app.ts
@@ -12,7 +12,7 @@ import {
 } from '../internal/communication';
 import { defaultSDKVersionForCompatCheck, version } from '../internal/constants';
 import { GlobalVars } from '../internal/globalVars';
-import * as Handlers from '../internal/handlers'; // Conflict with some names
+import * as Handlers from '../internal/handlers';
 import { ensureInitialized, processAdditionalValidOrigins } from '../internal/internalAPIs';
 import { compareSDKVersions, transformContext } from '../internal/utils';
 import { logs } from '../private/logs';
@@ -80,16 +80,23 @@ export namespace app {
   }
 
   /**
-   * Initializes the library. This must be called before any other SDK calls
+   * Initializes the library. 
+   * 
+   * @privateRemarks
+   * This must be called before any other SDK calls
    * but after the frame is loaded successfully.
-   * @param validMessageOrigins Optionally specify a list of cross frame message origins. They must have
-   * https: protocol otherwise they will be ignored. Example: https://www.example.com
+   * 
+   * @param validMessageOrigins - Optionally specify a list of cross frame message origins. They must have
+   * https: protocol otherwise they will be ignored. Example: https:www.example.com
    * @returns Promise that will be fulfilled when initialization has completed
    */
   export function initialize(validMessageOrigins?: string[]): Promise<void> {
     return new Promise<void>(resolve => {
-      // Independent components might not know whether the SDK is initialized so might call it to be safe.
-      // Just no-op if that happens to make it easier to use.
+      /**
+       * @privateRemarks
+       * Independent components might not know whether the SDK is initialized so might call it to be safe.
+       * Just no-op if that happens to make it easier to use.
+       */
       if (!GlobalVars.initializeCalled) {
         GlobalVars.initializeCalled = true;
 
@@ -99,27 +106,32 @@ export namespace app {
             GlobalVars.frameContext = context;
             GlobalVars.hostClientType = clientType;
             GlobalVars.clientSupportedSDKVersion = clientSupportedSDKVersion;
-
-            // Temporary workaround while the Hub is updated with the new argument order.
-            // For now, we might receive any of these possibilities:
-            // - `runtimeConfig` in `runtimeConfig` and `clientSupportedSDKVersion` in `clientSupportedSDKVersion`.
-            // - `runtimeConfig` in `clientSupportedSDKVersion` and `clientSupportedSDKVersion` in `runtimeConfig`.
-            // - `clientSupportedSDKVersion` in `runtimeConfig` and no `clientSupportedSDKVersion`.
-            // This code supports any of these possibilities
-
-            // Until Teams adopts the hub SDK, the Teams AppHost won't provide this runtime config
-            // so we assume that if we don't have it, we must be running in Teams.
-            // After Teams switches to the hub SDK, we can remove this default code.
+             /**
+              * @privateRemarks
+              * Temporary workaround while the Hub is updated with the new argument order.
+              * For now, we might receive any of these possibilities:
+              * - `runtimeConfig` in `runtimeConfig` and `clientSupportedSDKVersion` in `clientSupportedSDKVersion`.
+              * - `runtimeConfig` in `clientSupportedSDKVersion` and `clientSupportedSDKVersion` in `runtimeConfig`.
+              * - `clientSupportedSDKVersion` in `runtimeConfig` and no `clientSupportedSDKVersion`.
+              * This code supports any of these possibilities
+              * 
+              * Until Teams adopts the hub SDK, the Teams AppHost won't provide this runtime config
+              * so we assume that if we don't have it, we must be running in Teams.
+              * After Teams switches to the hub SDK, we can remove this default code.
+              */
             try {
               const givenRuntimeConfig: IRuntime = JSON.parse(runtimeConfig);
               runtimeConfig && applyRuntimeConfig(givenRuntimeConfig);
             } catch (e) {
               if (e instanceof SyntaxError) {
                 try {
-                  // if the given runtime config was actually meant to be a SDK version, store it as such.
-                  // TODO: This is a temporary workaround to allow Teams to store clientSupportedSDKVersion even when
-                  // it doesn't provide the runtimeConfig. After Teams switches to the hub SDK, we should
-                  // remove this feature.
+                  /**
+                   * @privateRemarks
+                   * if the given runtime config was actually meant to be a SDK version, store it as such.
+                   * TODO: This is a temporary workaround to allow Teams to store clientSupportedSDKVersion even when
+                   * it doesn't provide the runtimeConfig. After Teams switches to the hub SDK, we should
+                   * remove this feature.
+                   */
                   if (!isNaN(compareSDKVersions(runtimeConfig, defaultSDKVersionForCompatCheck))) {
                     GlobalVars.clientSupportedSDKVersion = runtimeConfig;
                   }
@@ -133,7 +145,10 @@ export namespace app {
                   }
                 }
               } else {
-                // If it's any error that's not a JSON parsing error, we want the program to fail.
+                /** 
+                 * @privateRemarks
+                 * If it's any error that's not a JSON parsing error, we want the program to fail. 
+                 */
                 throw e;
               }
             }
@@ -147,7 +162,10 @@ export namespace app {
         initializePrivateApis();
       }
 
-      // Handle additional valid message origins if specified
+      /** 
+       * @privateRemarks
+       * Handle additional valid message origins if specified 
+       */
       if (Array.isArray(validMessageOrigins)) {
         processAdditionalValidOrigins(validMessageOrigins);
       }
@@ -257,7 +275,10 @@ export namespace app {
 
   /**
    * Registers a handler for theme changes.
+   * 
+   * @privateRemarks
    * Only one handler can be registered at a time. A subsequent registration replaces an existing registration.
+   * 
    * @param handler - The handler to invoke when the user changes their theme.
    */
   export function registerOnThemeChangeHandler(handler: (theme: string) => void): void {
@@ -269,6 +290,7 @@ export namespace app {
 export namespace core {
   /**
    * Shares a deep link that a user can use to navigate back to a specific state in this page.
+   * 
    * @param deepLinkParameters - ID and label for the link and fallback URL.
    */
   export function shareDeepLink(deepLinkParameters: DeepLinkParameters): void {
@@ -283,6 +305,7 @@ export namespace core {
 
   /**
    * execute deep link API.
+   * 
    * @param deepLink - deep link.
    * @returns Promise that will be fulfilled when the operation has completed
    */
