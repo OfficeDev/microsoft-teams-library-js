9448fa1132c43917cb21c4b644bf4f1deed3d3f0
Xi Huang <huaxi@microsoft.com>
2021-10-21 23:53:49 -0700

    added cloud storage module

---
+++ src/private/cloudStorage.ts
@@ -0,0 +1,127 @@
+import { sendMessageToParent } from '../internal/communication';
+import { ensureInitialized } from '../internal/internalAPIs';
+import { SdkError } from '../public';
+
+/**
+ * Namespace to interact with the `cloudStorage` specific part of the SDK.
+ *
+ * @private
+ * Hide from docs
+ */
+export namespace cloudStorage {
+
+  export type ExternalFilesProviderCode = "DROPBOX" | "BOX" | "SHAREFILE" | "GOOGLEDRIVE" | "EGNYTE";
+
+  export enum FilesNavigationServiceType {
+    Recent,
+    Aggregate,
+    Personal,
+    Teams,
+    Channels,
+    Downloads,
+    PersonalWopi,
+    PersonalGoogle,
+    CustomSpo,
+    SharedWithMe,
+    Chats
+  }
+
+  export enum FilesProviderType {
+    Sharepoint = 0,
+    WOPI,
+    Google,
+    OneDrive,
+    Recent,
+    Aggregate,
+    FileSystem, // Used for Downloaded files on Desktop
+    Search, // Used by P2P files with OSearch
+    AllFiles, // Used by P2P files with AllFiles API
+    SharedWithMe
+  }
+
+  interface IWopiThumbnail {
+    size: number;
+    url: string;
+  }
+
+  interface IWopiService {
+    name: string;
+    description: string;
+    thumbnails: IWopiThumbnail[];
+  }
+
+  export interface ICommonExternalDto extends ICoreExternalItemlDto {
+    id: string;
+    lastModifiedTime: string;
+    size: number;
+    objectUrl: string; // TODO: (TASK 123357) DELETE THIS
+    accessToken?: string;
+  }
+
+  /**
+   * Contains the minimal properties for an item
+   */
+  export interface ICoreExternalItemlDto {
+    title: string;
+    isSubdirectory: boolean;
+    type: string;
+  }
+
+  export interface IExternalProvider extends IWopiService {
+    navigationType: FilesNavigationServiceType;
+    providerType: FilesProviderType;
+    providerCode: ExternalFilesProviderCode;
+  }
+
+  /**
+   * @private
+   * Allow 1st party apps to call this function to get the external 
+   * third party cloud storage accounts that the tenant supports.
+   */
+  export function getExternalProviders(callback: (error: SdkError, providers: IExternalProvider[]) => void ): void {
+    ensureInitialized();
+
+    if (!callback) {
+      throw new Error('[cloudStorage.getExternalProviders] Callback cannot be null');
+    }
+
+    sendMessageToParent('cloudStorage.getExternalProviders', callback);
+  }
+
+  /**
+   * @private
+   * Allow 1st party apps to call this function to move files
+   * among SharePoint and third party cloud storages.
+   */
+  export function copyMoveFiles(
+    selectedFiles: ICommonExternalDto[],
+    providerCode: ExternalFilesProviderCode,
+    destinationFolder: ICommonExternalDto,
+    destinationProviderCode: ExternalFilesProviderCode,
+    isMove: boolean = false,
+    callback: (error: SdkError) => void ): void {
+      ensureInitialized();
+
+      if (!selectedFiles || selectedFiles.length == 0) {
+        throw new Error('[cloudStorage.copyMoveFiles] selectedFiles cannot be null or empty');
+      }
+
+      if (!providerCode) {
+        throw new Error('[cloudStorage.copyMoveFiles] providerCode cannot be null or empty');
+      }
+
+      if (!destinationFolder) {
+        throw new Error('[cloudStorage.copyMoveFiles] destinationFolder cannot be null or empty');
+      }
+
+      if (!destinationProviderCode) {
+        throw new Error('[cloudStorage.copyMoveFiles] destinationProviderCode cannot be null or empty');
+      }
+  
+      if (!callback) {
+        throw new Error('[cloudStorage.copyMoveFiles] callback cannot be null');
+      }
+  
+      sendMessageToParent('cloudStorage.copyMoveFiles', callback);
+  }
+}
---
+++ test/private/cloudStorage.spec.ts
@@ -0,0 +1,102 @@
+import {
+  cloudStorage
+} from '../../src/private/cloudStorage';
+import { Utils } from '../utils';
+import { _initialize, _uninitialize } from '../../src/public/publicAPIs';
+
+describe('cloudStorage', () => {
+  const utils = new Utils();
+  const emptyCallback = () => {};
+
+  beforeEach(() => {
+    utils.processMessage = null;
+    utils.messages = [];
+    utils.childMessages = [];
+    utils.childWindow.closed = false;
+    utils.mockWindow.parent = utils.parentWindow;
+
+    // Set a mock window for testing
+    _initialize(utils.mockWindow);
+  });
+
+  afterEach(() => {
+    // Reset the object since it's a singleton
+    if (_uninitialize) {
+      _uninitialize();
+    }
+  });
+
+  describe('getExternalProviders', () => {
+
+    it('should trigger callback correctly', () => {
+      utils.initializeWithContext('content');
+      const mockExternalProviders: cloudStorage.IExternalProvider[] = [
+        {
+          name: 'google',
+          description: 'google storage',
+          thumbnails: [{
+            size: 32,
+            url: 'string'
+          }],
+          navigationType: cloudStorage.FilesNavigationServiceType.PersonalGoogle,
+          providerType: cloudStorage.FilesProviderType.Google,
+          providerCode: "GOOGLEDRIVE"
+        },
+      ];
+
+      const callback = jest.fn((err, providers) => {
+        expect(err).toBeFalsy();
+        expect(providers).toEqual(mockExternalProviders);
+      });
+
+      cloudStorage.getExternalProviders(callback);
+
+      const getExternalProviders = utils.findMessageByFunc('cloudStorage.getExternalProviders');
+      expect(getExternalProviders).not.toBeNull();
+      utils.respondToMessage(getExternalProviders, false, mockExternalProviders);
+      expect(callback).toHaveBeenCalled();
+    });
+  });
+
+  describe('copyMoveFiles', () => {
+    const mockSelectedFiles: cloudStorage.ICommonExternalDto[] = [
+      {
+        id: '123',
+        lastModifiedTime: '2021-10-22T06:29:37.051Z',
+        size: 32,
+        objectUrl: 'abc.com',
+        title: 'file',
+        isSubdirectory: false,
+        type: 'type'
+      }
+    ];
+
+    const mockDestinationFolder: cloudStorage.ICommonExternalDto = {
+        id: '123',
+        lastModifiedTime: '2021-10-22T06:29:37.051Z',
+        size: 32,
+        objectUrl: 'abc.com',
+        title: 'file',
+        isSubdirectory: false,
+        type: 'type'
+    };
+
+    const mockProviderCode = "DROPBOX";
+
+    it('should not allow calls before initialization', () => {
+      expect(() => cloudStorage.copyMoveFiles(mockSelectedFiles, mockProviderCode, mockDestinationFolder, mockProviderCode, false, emptyCallback)).toThrowError('The library has not yet been initialized');
+    });
+
+    it('should trigger callback correctly', () => {
+      utils.initializeWithContext('content');
+
+      const callback = jest.fn((err) => {
+        expect(err).toBeFalsy();
+      });
+
+      cloudStorage.copyMoveFiles(mockSelectedFiles, mockProviderCode, mockDestinationFolder, mockProviderCode, false, callback);
+      expect(callback).toHaveBeenCalled();
+    });
+  });
+
+});
