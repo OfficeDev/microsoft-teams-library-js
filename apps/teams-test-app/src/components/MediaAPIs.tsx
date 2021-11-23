a3266916c7acaab12ddd2ae04df67250e4db0c63
Marcin Juraszek <marcinj@microsoft.com>
2021-11-22 16:57:06 -0800

    Move Media APIs to the new components

--- apps/teams-test-app/src/components/MediaAPIs.tsx
+++ apps/teams-test-app/src/components/MediaAPIs.tsx
@@ -3,41 +3,39 @@ import React, { ReactElement } from 'react';
 
 import { noHostSdkMsg } from '../App';
 import BoxAndButton from './BoxAndButton';
+import { ApiWithoutInput, ApiWithTextInput } from './utils';
 
-const MediaAPIs = (): ReactElement => {
-  const [captureImageRes, setCaptureImageRes] = React.useState('');
-  const [selectMediaRes, setSelectMediaRes] = React.useState('');
-  const [getMediaRes, setGetMediaRes] = React.useState('');
-  const [viewImagesWithIdRes, setViewImagesWithIdRes] = React.useState('');
-  const [viewImagesWithUrlsRes, setViewImagesWithUrlsRes] = React.useState('');
-  const [scanBarCodeRes, setScanBarCodeRes] = React.useState('');
-  const [checkMediaCapabilityRes, setCheckMediaCapabilityRes] = React.useState('');
+const CaptureImage = (): React.ReactElement =>
+  ApiWithoutInput({
+    name: 'CaptureImage',
+    title: 'Capture Image',
+    onClick: async () => {
+      const result = await media.captureImage();
+      const file: media.File = result[0];
+      let content = '';
+      let len = 20;
+      if (file.content) {
+        len = Math.min(len, file.content.length);
+        content = file.content.substr(0, len);
+      }
+      const output =
+        'format: ' + file.format + ', size: ' + file.size + ', mimeType: ' + file.mimeType + ', content: ' + content;
+      return output;
+    },
+  });
 
-  const captureImage = (): void => {
-    setCaptureImageRes('media.captureImage()' + noHostSdkMsg);
-    media
-      .captureImage()
-      .then(files => {
-        const file: media.File = files[0];
-        let content = '';
-        let len = 20;
-        if (file.content) {
-          len = Math.min(len, file.content.length);
-          content = file.content.substr(0, len);
+const SelectMedia = (): React.ReactElement =>
+  ApiWithTextInput<media.MediaInputs>({
+    name: 'selectMedia',
+    title: 'Select Media',
+    onClick: {
+      validateInput: input => {
+        if (!input.mediaType || !input.maxMediaCount) {
+          throw new Error('mediaType and maxMediaCount are required');
         }
-        const output =
-          'format: ' + file.format + ', size: ' + file.size + ', mimeType: ' + file.mimeType + ', content: ' + content;
-        setCaptureImageRes(output);
-      })
-      .catch(error => setCaptureImageRes(error.errorCode.toString + ' ' + error.message));
-  };
-
-  const selectMedia = (mediaInputs: string): void => {
-    const mediaInputsParams: media.MediaInputs = JSON.parse(mediaInputs);
-    setSelectMediaRes('media.selectMedia()' + noHostSdkMsg);
-    media
-      .selectMedia(mediaInputsParams)
-      .then(medias => {
+      },
+      submit: async input => {
+        const medias = await media.selectMedia(input);
         let message = '';
         for (let i = 0; i < medias.length; i++) {
           const media: media.Media = medias[i];
@@ -59,40 +57,49 @@ const MediaAPIs = (): ReactElement => {
             ', preview: ' +
             preview +
             '],';
-          setSelectMediaRes(message);
         }
-      })
-      .catch(error => setSelectMediaRes(error.errorCode.toString + ' ' + error.message));
-  };
+        return message;
+      },
+    },
+  });
 
-  const getMedia = (mediaInputs: string): void => {
-    const mediaInputsParams: media.MediaInputs = JSON.parse(mediaInputs);
-    setGetMediaRes('media.getMedia()' + noHostSdkMsg);
-    media
-      .selectMedia(mediaInputsParams)
-      .then(medias => {
-        const media: media.Media = medias[0] as media.Media;
-        return media.getMedia();
-      })
-      .then(blob => {
+const GetMedia = (): React.ReactElement =>
+  ApiWithTextInput<media.MediaInputs>({
+    name: 'getMedia',
+    title: 'Get Media',
+    onClick: {
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      validateInput: () => {},
+      submit: async (input, setResult) => {
+        const medias = await media.selectMedia(input);
+        const mediaItem: media.Media = medias[0] as media.Media;
+        const blob = await mediaItem.getMedia();
         const reader = new FileReader();
         reader.readAsDataURL(blob);
         reader.onloadend = () => {
           if (reader.result) {
             // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-            setGetMediaRes('Received Blob (length: ' + (reader.result as any).length + ')');
+            setResult('Received Blob (length: ' + (reader.result as any).length + ')');
           }
         };
-      })
-      .catch(error => setGetMediaRes(error.errorCode.toString + ' ' + error.message));
-  };
+        return 'media.getMedia()' + noHostSdkMsg;
+      },
+    },
+  });
 
-  const viewImagesWithId = (mediaInputs: string): void => {
-    const mediaInputsParams: media.MediaInputs = JSON.parse(mediaInputs);
-    setViewImagesWithIdRes('media.viewImagesWithId()' + noHostSdkMsg);
-    media
-      .selectMedia(mediaInputsParams)
-      .then(medias => {
+const ViewImagesWithId = (): React.ReactElement =>
+  ApiWithTextInput<media.MediaInputs>({
+    name: 'viewImagesWithId',
+    title: 'View Images With Id',
+    onClick: {
+      validateInput: input => {
+        if (!input.mediaType || !input.maxMediaCount) {
+          throw new Error('mediaType and maxMediaCount are required');
+        }
+      },
+      submit: async input => {
+        const medias = await media.selectMedia(input);
+
         const urlList: media.ImageUri[] = [];
         for (let i = 0; i < medias.length; i++) {
           const media = medias[i];
@@ -101,12 +108,52 @@ const MediaAPIs = (): ReactElement => {
             type: 1, //ImageUriType.ID
           } as media.ImageUri);
         }
-        return media.viewImages(urlList);
-      })
-      .then(() => setViewImagesWithIdRes('Success'))
-      .catch(err => setViewImagesWithIdRes(err.errorCode.toString + ' ' + err.message));
-  };
+        await media.viewImages(urlList);
+        return 'Success';
+      },
+    },
+  });
 
+const ScanBarCode = (): ReactElement =>
+  ApiWithTextInput<media.BarCodeConfig>({
+    name: 'mediaScanBarCode',
+    title: 'Media Scan Bar Code',
+    onClick: async input => {
+      const result = await media.scanBarCode(input);
+      return 'result: ' + result;
+    },
+  });
+
+const ViewImagesWithUrls = (): React.ReactElement =>
+  ApiWithTextInput<string[]>({
+    name: 'viewImagesWithUrls2',
+    title: 'View Images With Urls',
+    onClick: {
+      validateInput: input => {
+        if (!input || !Array.isArray(input) || input.length === 0 || input.find(x => typeof x !== 'string')) {
+          throw new Error('input has to be an array of strings with at least one element');
+        }
+      },
+      submit: async input => {
+        const urlList: media.ImageUri[] = input.map(x => ({ value: x, type: 2 }));
+        await media.viewImages(urlList);
+        return 'media.viewImagesWithUrls() executed';
+      },
+    },
+  });
+
+const CheckMediaCapability = (): React.ReactElement =>
+  ApiWithoutInput({
+    name: 'checkMediaCapability',
+    title: 'Check Media Call',
+    onClick: async () => `Media module ${media.isSupported() ? 'is' : 'is not'} supported`,
+  });
+
+const MediaAPIs = (): ReactElement => {
+  // TODO: Remove once E2E scenario tests are updated to use the new version
+  const [viewImagesWithUrlsRes, setViewImagesWithUrlsRes] = React.useState('');
+
+  // TODO: Remove once E2E scenario tests are updated to use the new version
   const viewImagesWithUrls = (imageUrlsInput: string): void => {
     setViewImagesWithUrlsRes('media.viewImagesWithUrls()' + noHostSdkMsg);
     const imageUrls: string[] = imageUrlsInput.split(', ');
@@ -123,55 +170,15 @@ const MediaAPIs = (): ReactElement => {
       .then(() => setViewImagesWithUrlsRes('media.viewImagesWithUrls() executed'))
       .catch(err => setViewImagesWithUrlsRes(err.errorCode.toString + ' ' + err.message));
   };
-
-  const scanBarCode = (scanBarCodeConfigInput: string): void => {
-    const scanBarCodeConfig: media.BarCodeConfig = JSON.parse(scanBarCodeConfigInput);
-    setScanBarCodeRes('media.scanBarCode()' + noHostSdkMsg);
-    media
-      .scanBarCode(scanBarCodeConfig)
-      .then(result => setScanBarCodeRes('result: ' + result))
-      .catch(err => setScanBarCodeRes(err.errorCode.toString + ' ' + err.message));
-  };
-
-  const mediaCapabilityCheck = (): void => {
-    if (media.isSupported()) {
-      setCheckMediaCapabilityRes('Media module is supported');
-    } else {
-      setCheckMediaCapabilityRes('Media module is not supported');
-    }
-  };
-
   return (
     <>
       <h1>media</h1>
-      <BoxAndButton
-        handleClick={captureImage}
-        output={captureImageRes}
-        hasInput={false}
-        title="Capture Image"
-        name="CaptureImage"
-      />
-      <BoxAndButton
-        handleClickWithInput={selectMedia}
-        output={selectMediaRes}
-        hasInput={true}
-        title="Select Media"
-        name="selectMedia"
-      />
-      <BoxAndButton
-        handleClickWithInput={getMedia}
-        output={getMediaRes}
-        hasInput={true}
-        title="Get Media"
-        name="getMedia"
-      />
-      <BoxAndButton
-        handleClickWithInput={viewImagesWithId}
-        output={viewImagesWithIdRes}
-        hasInput={true}
-        title="View Images With Id"
-        name="viewImagesWithId"
-      />
+      <CaptureImage />
+      <SelectMedia />
+      <GetMedia />
+      <ViewImagesWithId />
+      <ViewImagesWithUrls />
+      {/* TODO: Remove once E2E scenario tests are updated to use the new version */}
       <BoxAndButton
         handleClickWithInput={viewImagesWithUrls}
         output={viewImagesWithUrlsRes}
@@ -179,20 +186,8 @@ const MediaAPIs = (): ReactElement => {
         title="View Images With Urls"
         name="viewImagesWithUrls"
       />
-      <BoxAndButton
-        handleClickWithInput={scanBarCode}
-        output={scanBarCodeRes}
-        hasInput={true}
-        title="Media Scan Bar Code"
-        name="mediaScanBarCode"
-      />
-      <BoxAndButton
-        handleClick={mediaCapabilityCheck}
-        output={checkMediaCapabilityRes}
-        hasInput={false}
-        title="Check Media Capability"
-        name="checkMediaCapability"
-      />
+      <ScanBarCode />
+      <CheckMediaCapability />
     </>
   );
 };
