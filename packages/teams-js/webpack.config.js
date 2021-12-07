b0f3344ccd7098b139d6cc48c985b3fa8c747d14
Murthy Sistla <murthys@microsoft.com>
2021-12-07 15:13:37 -0800

    updating the hash in readme file

--- packages/teams-js/webpack.config.js
+++ packages/teams-js/webpack.config.js
@@ -6,7 +6,7 @@ const { SubresourceIntegrityPlugin } = require('webpack-subresource-integrity');
 const { readFileSync } = require('fs');
 const { join } = require('path');
 const WebpackAssetsManifest = require('webpack-assets-manifest');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+var replace = require('replace');
 const libraryName = 'microsoftTeams';
 const expect = require('expect');
 const path = require('path');
@@ -64,12 +64,11 @@ module.exports = {
       removeSource: true,
       outputAsModuleFolder: true,
     }),
-    new HtmlWebpackPlugin(),
 
     // https://www.npmjs.com/package/webpack-subresource-integrity
     new SubresourceIntegrityPlugin({ enabled: true }),
 
-    // Webpackmanifest produces the json with asset and its corresponding hash values(Example: https://github.com/waysact/webpack-subresource-integrity/blob/main/examples/webpack-assets-manifest/webpack.config.js)
+    // Webpackmanifest produces the json file containing asset(JS file) and its corresponding hash values(Example: https://github.com/waysact/webpack-subresource-integrity/blob/main/examples/webpack-assets-manifest/webpack.config.js)
     new WebpackAssetsManifest({
       integrity: true,
       integrityHashes: ['sha384'],
@@ -81,6 +80,16 @@ module.exports = {
           const manifest = JSON.parse(readFileSync(join(__dirname, 'dist/MicrosftTeams-manifest.json'), 'utf-8'));
           // If for some reason hash was not generated for the assets, this test will fail in build.
           expect(manifest['MicrosoftTeams.min.js'].integrity).toMatch(/sha384-.*/);
+
+          // Updating the old hash value with the new one in all places.
+          let currentHashValue = 'sha384-RhShpWwRxSnc+keX0WZxCsf8olaKOOw416Jky+StW6qhctsNR3GMwav/hMB1Snas';
+          replace({
+            regex: currentHashValue,
+            replacement: manifest['MicrosoftTeams.min.js'].integrity,
+            paths: ['.'],
+            recursive: true,
+            silent: true,
+          });
         });
       },
     },
