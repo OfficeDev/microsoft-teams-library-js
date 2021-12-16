6c2b24c773b3a6e86015b8c5667e3f7a835a8f22
jadahiya-MSFT <jdahiya8719@outlook.com>
2021-12-16 12:18:21 -0700

    Cleaned up logic

--- apps/teams-test-app/src/components/utils/getTestBackCompat.ts
+++ apps/teams-test-app/src/components/utils/getTestBackCompat.ts
@@ -1,8 +1,5 @@
 const urlParams = new URLSearchParams(window.location.search);
 
 export const getTestBackCompat = (): boolean => {
-  if (urlParams.get('testCallback') === 'true') {
-    return true;
-  }
-  return false;
+  return urlParams.get('testCallback') === 'true';
 };
