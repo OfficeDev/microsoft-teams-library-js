de09c6bf6fae98e5837ba65827a5d4e7baeef9c8
Jessica-Koch <jekoch@microsoft.com>
2021-12-16 13:49:22 -0800

    add an expectedFailure test component

--- apps/teams-test-app/src/components/AppInitialization.tsx
+++ apps/teams-test-app/src/components/AppInitialization.tsx
@@ -45,12 +45,35 @@ const NotifyFailure = (): React.ReactElement =>
     },
   });
 
+const NotifyExpectedFailure = (): React.ReactElement =>
+  ApiWithTextInput<app.ExpectedFailureReason>({
+    name: 'appInitializationExpectedFailure2',
+    title: 'appInitialization.expectedFailure',
+    onClick: {
+      validateInput: input => {
+        if (!input) {
+          // this API actually allow for the input not to be provided
+          return;
+        }
+        const acceptableValues = Object.values(app.ExpectedFailureReason);
+        if (!acceptableValues.includes(input)) {
+          throw new Error(`input must be one of: ${JSON.stringify(acceptableValues)}`);
+        }
+      },
+      submit: async input => {
+        app.notifyExpectedFailure({ reason: input || app.ExpectedFailureReason.Other });
+        return 'called';
+      },
+    },
+  });
+
 const AppInitializationAPIs = (): ReactElement => (
   <>
     <h1>appInitialization</h1>
     <NotifyLoaded />
     <NotifySuccess />
     <NotifyFailure />
+    <NotifyExpectedFailure />
   </>
 );
 
