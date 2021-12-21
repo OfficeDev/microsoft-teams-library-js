a6904c39199652ed7044fcaa93f6de566c8a2244
ramourt <87208590+ramourt@users.noreply.github.com>
2021-12-21 13:14:47 -0800

    Update sdf readme with sdf cdn url on release

--- packages/teams-js/prepNewReadme.js
+++ packages/teams-js/prepNewReadme.js
@@ -3,6 +3,12 @@ const path = require('path');
 
 const relativePathToManifestJson = './dist/MicrosoftTeams-manifest.json';
 const relativePathToReadmeTemplate = './Readme.md';
+const version = require('./package.json').version;
+
+const createUrl = () => {
+  const isDev = version.includes('dev');
+  return `https://res${isDev ? '-sdf' : ''}.cdn.office.net/teams-js/${version}/js/MicrosoftTeams.min.js`;
+}
 
 const getIntegrityHash = () => {
   const absolutePathToManifestJson = path.resolve(__dirname, relativePathToManifestJson);
@@ -18,19 +24,23 @@ const getIntegrityHash = () => {
   return integrityHash;
 };
 
-const updateReadmeWithIntegrityHash = (integrityHash) => {
+const updateReadme = (integrityHash) => {
   const absolutePathToReadmeTemplate = path.resolve(__dirname, relativePathToReadmeTemplate);
   if(!fs.existsSync(absolutePathToReadmeTemplate)) {
     throw(`ERROR: Readme-template.md at ${absolutePathToReadmeTemplate} was not found.`);
   }
   const readme = fs.readFileSync(absolutePathToReadmeTemplate, 'utf8');
-  const result = readme.replace(/integrity=\".*?\"/, `integrity="${integrityHash}"`);
+  const readmeWithIntegrity = readme.replace(/integrity=\".*?\"/, `integrity="${integrityHash}"`);
+  const result = readmeWithIntegrity.replace(
+    /src=\"https:\/\/res.*?\/js\/MicrosoftTeams.min.js\"/,
+    `src="${createUrl()}"`);
+
   fs.writeFileSync(absolutePathToReadmeTemplate, result);
 };
 
 (() => {
   console.log('readme-generator is running');
   const integrityHash = getIntegrityHash();
-  updateReadmeWithIntegrityHash(integrityHash);
+  updateReadme(integrityHash);
   console.log('readme-generator completed successfully!');
 })();
