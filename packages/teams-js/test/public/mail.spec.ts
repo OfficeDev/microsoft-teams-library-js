ca3198dc4c9adfcafdcae6f4918d8e6c42ee6d83
Trevor Harris <trharris@microsoft.com>
2022-01-13 11:48:38 -0800

    mail.composeMail unit tests
    Added unit tests for composeMail. Very similar to openMailItem

--- packages/teams-js/test/public/mail.spec.ts
+++ packages/teams-js/test/public/mail.spec.ts
@@ -168,6 +168,149 @@ describe('mail', () => {
     });
   });
 
+  describe('composeMail', () => {
+    const composeMailParams: mail.ComposeMailParams = {
+      type: mail.ComposeMailType.New,
+    };
+
+    it('should not allow calls before initialization', async () => {
+      await mail
+        .composeMail(composeMailParams)
+        .catch(e => expect(e).toMatchObject(new Error('The library has not yet been initialized')));
+    });
+
+    it('should not allow calls from settings context', async () => {
+      await utils.initializeWithContext('settings');
+
+      await mail
+        .composeMail(composeMailParams)
+        .catch(e =>
+          expect(e).toMatchObject(
+            new Error('This call is only allowed in following contexts: ["content"]. Current context: "settings".'),
+          ),
+        );
+    });
+
+    it('should not allow calls from authentication context', async () => {
+      await utils.initializeWithContext('authentication');
+
+      await mail
+        .composeMail(composeMailParams)
+        .catch(e =>
+          expect(e).toMatchObject(
+            new Error(
+              'This call is only allowed in following contexts: ["content"]. Current context: "authentication".',
+            ),
+          ),
+        );
+    });
+
+    it('should not allow calls from remove context', async () => {
+      await utils.initializeWithContext('remove');
+
+      await mail
+        .composeMail(composeMailParams)
+        .catch(e =>
+          expect(e).toMatchObject(
+            new Error('This call is only allowed in following contexts: ["content"]. Current context: "remove".'),
+          ),
+        );
+    });
+
+    it('should not allow calls from task context', async () => {
+      await utils.initializeWithContext('task');
+
+      await mail
+        .composeMail(composeMailParams)
+        .catch(e =>
+          expect(e).toMatchObject(
+            new Error('This call is only allowed in following contexts: ["content"]. Current context: "task".'),
+          ),
+        );
+    });
+
+    it('should not allow calls from sidePanel context', async () => {
+      await utils.initializeWithContext('sidePanel');
+
+      await mail
+        .composeMail(composeMailParams)
+        .catch(e =>
+          expect(e).toMatchObject(
+            new Error('This call is only allowed in following contexts: ["content"]. Current context: "sidePanel".'),
+          ),
+        );
+    });
+
+    it('should not allow calls from stage context', async () => {
+      await utils.initializeWithContext('stage');
+
+      await mail
+        .composeMail(composeMailParams)
+        .catch(e =>
+          expect(e).toMatchObject(
+            new Error('This call is only allowed in following contexts: ["content"]. Current context: "stage".'),
+          ),
+        );
+    });
+
+    it('should not allow calls from meetingStage context', async () => {
+      await utils.initializeWithContext('meetingStage');
+
+      await mail
+        .composeMail(composeMailParams)
+        .catch(e =>
+          expect(e).toMatchObject(
+            new Error('This call is only allowed in following contexts: ["content"]. Current context: "meetingStage".'),
+          ),
+        );
+    });
+
+    it('should not allow calls if runtime does not support mail', async () => {
+      await utils.initializeWithContext('content');
+      utils.setRuntimeConfig({ apiVersion: 1, supports: {} });
+
+      await mail.composeMail(composeMailParams).catch(e => expect(e).toBe('Not Supported'));
+    });
+
+    it('should successfully throw if the openMailItem message sends and fails', async () => {
+      await utils.initializeWithContext('content');
+      utils.setRuntimeConfig({ apiVersion: 1, supports: { mail: {} } });
+
+      const composeMailPromise = mail.composeMail(composeMailParams);
+
+      const composeMail = utils.findMessageByFunc('mail.composeMail');
+
+      const data = {
+        success: false,
+        error: 'Something went wrong...',
+      };
+
+      utils.respondToMessage(composeMail, data);
+      await composeMailPromise.catch(e => expect(e).toMatchObject(new Error('Something went wrong...')));
+    });
+
+    it('should successfully send the openMailItem message', async () => {
+      await utils.initializeWithContext('content');
+      utils.setRuntimeConfig({ apiVersion: 1, supports: { mail: {} } });
+
+      const promise = mail.composeMail(composeMailParams);
+
+      const composeMailMessage = utils.findMessageByFunc('mail.composeMail');
+
+      const data = {
+        success: true,
+        error: 'Something went wrong...',
+      };
+
+      utils.respondToMessage(composeMailMessage, data);
+      await promise;
+
+      expect(composeMailMessage).not.toBeNull();
+      expect(composeMailMessage.args.length).toEqual(1);
+      expect(composeMailMessage.args[0]).toStrictEqual(composeMailParams);
+    });
+  });
+
   describe('isSupported', () => {
     it('should return false if the runtime says mail is not supported', () => {
       utils.setRuntimeConfig({ apiVersion: 1, supports: {} });
