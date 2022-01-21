05db1f3b6ddeabdf93e6debd927185ce6b78c4ba
Stephen Houston <sthousto@microsoft.com>
2022-01-21 08:42:34 -0800

    updated SharePointSiteInfo to with mySite properties and renamed existing teamSite properties

--- packages/teams-js/src/public/app.ts
+++ packages/teams-js/src/public/app.ts
@@ -336,22 +336,32 @@ export namespace app {
     /**
      * The root SharePoint site associated with the team.
      */
-    url?: string;
+    teamSiteUrl?: string;
 
     /**
      * The domain of the root SharePoint site associated with the team.
      */
-    domain?: string;
+    teamSiteDomain?: string;
 
     /**
      * The relative path to the SharePoint site associated with the team.
      */
-    path?: string;
+    teamSitePath?: string;
 
     /**
      * Teamsite ID, aka sharepoint site id.
      */
-    id?: string;
+    teamSiteId?: string;
+
+    /**
+     * The SharePoint my site domain associated with the user.
+     */
+    mySiteDomain?: string;
+
+    /**
+     * The SharePoint relative path to the current users mysite
+     */
+    mySitePath?: string;
   }
 
   /**
