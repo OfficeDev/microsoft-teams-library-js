69ff8ae4a7039f8b1b2e83234578f8881e841eb7
Angela Eun <aeun@microsoft.com>
2022-02-03 13:44:30 -0800

    refine logging beachball

--- beachball.config.js
+++ beachball.config.js
@@ -3,6 +3,8 @@ const exec = require('child_process').exec;
 const postbump = (packagePath, packageName, packageVersion) => {
   if (packageName !== '@microsoft/teams-js') {
     console.log('postbump executed');
+    console.log(`packagePath: ${packagePath}`);
+    console.log(`packageName: ${packageName}`);
     exec('cd packagePath && rm CHANGELOG.*');
   }
 };
