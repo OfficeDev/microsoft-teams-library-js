95c57f1ac53070e2d1ab2806144097821708acb9
Angela Eun <aeun@microsoft.com>
2022-02-03 15:04:59 -0800

    Update beachball config with most todo done

--- beachball.config.js
+++ beachball.config.js
@@ -1,21 +1,23 @@
-const exec = require('child_process').exec;
+const fs = require('fs');
 
 const postbump = (packagePath, packageName, packageVersion) => {
-  if (packageName !== '@microsoft/teams-js') {
-    console.log('postbump executed');
-    console.log(`packagePath: ${packagePath}`);
-    console.log(`packageName: ${packageName}`);
-    exec('cd packagePath && rm CHANGELOG.*');
-  }
+  fs.rm(`${packagePath}\\CHANGELOG.json`, err => {
+    if (err) {
+      console.log(err.message);
+      return;
+    }
+    return;
+  });
 };
 
 module.exports = {
   branch: 'origin/2.0-preview',
+  bumpDeps: false,
   generateChangelog: true,
   hooks: { postbump },
-  // TODO: ignore tests, other packages, etc. Also, move the changelog file :)
-  ignorePatterns: [],
-  package: '@microsoft/teams-js',
+  // TODO: move the changelog file
+  ignorePatterns: ['.*ignore', '*.config.js', 'test/**'],
   publish: false,
   push: false,
+  scope: ['packages/teams-js'],
 };
