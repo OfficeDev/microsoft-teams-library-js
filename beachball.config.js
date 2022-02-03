d93f172804d965c2c27f28621ee99595e80a3ad6
Angela Eun <aeun@microsoft.com>
2022-02-03 09:20:42 -0800

    Add TODO

--- beachball.config.js
+++ beachball.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   branch: 'origin/2.0-preview',
   generateChangelog: true,
+  // TODO: ignore tests, other packages, etc. Also, move the changelog file :)
+  ignorePatterns: [],
   package: '@microsoft/teams-js',
   publish: false,
   push: false,
