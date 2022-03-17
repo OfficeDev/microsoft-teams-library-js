dffdcd0120dbb07da6d718a82299afe29b29400e
Lakhveer Kaur <lakhveerkaur@microsoft.com>
2022-03-17 15:32:49 -0700

    Adding dialog.bot to runtime and teamsRuntimeConfig

--- packages/teams-js/src/public/runtime.ts
+++ packages/teams-js/src/public/runtime.ts
@@ -50,7 +50,9 @@ export let runtime: IRuntime = {
     calendar: undefined,
     call: undefined,
     chat: undefined,
-    dialog: undefined,
+    dialog: {
+      bot: undefined,
+    },
     location: undefined,
     logs: undefined,
     mail: undefined,
@@ -87,7 +89,9 @@ export const teamsRuntimeConfig: IRuntime = {
     bot: {},
     call: {},
     chat: {},
-    dialog: {},
+    dialog: {
+      bot: {},
+    },
     files: {},
     location: {},
     logs: {},
