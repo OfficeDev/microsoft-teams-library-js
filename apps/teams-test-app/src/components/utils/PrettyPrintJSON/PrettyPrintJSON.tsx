9c67a6f0508a530ab72d2d83fec643b450a5b2d4
Jessica Koch <jekoch@microsoft.com>
2022-03-28 10:56:31 -0700

    add pretty print json component

--- apps/teams-test-app/src/components/utils/PrettyPrintJSON/PrettyPrintJSON.tsx
+++ apps/teams-test-app/src/components/utils/PrettyPrintJSON/PrettyPrintJSON.tsx
@@ -3,13 +3,14 @@ import './PrettyPrintJSON.css';
 export type PrettyPrintJSONProps<T> = {
   data: T;
   label: string;
+  name: string;
 };
 
 export const PrettyPrintJSON = <T,>(props: PrettyPrintJSONProps<T>): JSX.Element => {
-  const { data, label } = props;
+  const { data, label, name } = props;
 
   return (
-    <div className="prettyPrintWrapper">
+    <div id={`text_${name}`} className="prettyPrintWrapper">
       <div className="header">{label}</div>
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
