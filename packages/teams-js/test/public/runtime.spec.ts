bfff2ebcff102f3499989fec80184d71b3d9c719
shrshindeMSFT <98348000+shrshindeMSFT@users.noreply.github.com>
2022-04-05 12:37:31 -0700

    Adding unit test cases

--- packages/teams-js/test/public/runtime.spec.ts
+++ packages/teams-js/test/public/runtime.spec.ts
@@ -19,39 +19,31 @@ describe('runtime', () => {
     }
   });
 
-  // create test groups for each capability
   describe('generateBackCompatRuntimeConfig', () => {
-    // below test will be changed so instead of checking manually each capability, we make it
-    // so each capability added to versionConstants is automatically checked thorugh this test case.
-
-    // for each version number in versionConstants,
-    // for each hostclient type possible,
-    // check that if the hostclient type is in the ICapabilityReq and version number ...
-    it('Back compat host client supporting up to 1.9.0 should support location capability and not any later capabilities', () => {
-      return utils.initializeWithContext('content', 'android').then(() => {
-        // removing the brackets allows us to check for nested capabilities like legacy.fullTrust.joinedTeams.
-        const generatedRuntimeConfigSupportedCapabilities = JSON.stringify(
-          generateBackCompatRuntimeConfig('1.9.0').supports,
-        ).replace(/[{}]/g, '');
-        console.log(`generatedRuntimeConfigSupportedCapabilities: ${generatedRuntimeConfigSupportedCapabilities}`);
-        Object.keys(versionConstants).map(version => {
-          versionConstants[version].forEach(capabilityReqs => {
-            const capability = JSON.stringify(capabilityReqs.capability).replace(/[{}]/g, '');
-            console.log(`capability: ${capability}`);
-            expect(compareSDKVersions('1.9.0', version) >= 0).toBe(
-              generatedRuntimeConfigSupportedCapabilities.includes(capability),
-            );
+    Object.entries(versionConstants).forEach(([version, capabilities]) => {
+      capabilities.forEach(supportedCapability => {
+        const capability = JSON.stringify(supportedCapability.capability).replace(/[{}]/g, '');
+        supportedCapability.hostClientTypes.forEach(clientType => {
+          it(`Back compat host client type ${clientType} supporting up to ${version} should support ${capability.replace(
+            /:/g,
+            ' ',
+          )} capability and not any later capabilities`, async () => {
+            await utils.initializeWithContext('content', clientType);
+            const generatedRuntimeConfigSupportedCapabilities = JSON.stringify(
+              generateBackCompatRuntimeConfig(version).supports,
+            ).replace(/[{}]/g, '');
+            expect(generatedRuntimeConfigSupportedCapabilities.includes(capability)).toBe(true);
           });
         });
       });
     });
 
-    // versions < 1.9.0 should not contain location capability
-    // versions >= 1.9.0 should support location
-    // should work with any hostClientType for location
-
-    // Basically every capability needs to check those above
-    // should work with any hostClientType for people
-    // should only allow android hostClientType for joinedTeams
+    it('Back compat should return false when not proper version is supported', async () => {
+      await utils.initializeWithContext('content', 'ios');
+      const generatedRuntimeConfigSupportedCapabilities = JSON.stringify(
+        generateBackCompatRuntimeConfig('1.4.5').supports,
+      ).replace(/[{}]/g, '');
+      expect(generatedRuntimeConfigSupportedCapabilities.includes('location')).toBe(false);
+    });
   });
 });
