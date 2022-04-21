d67eb2893b16a11a438bb90b62364d4e9a10bb52
Jin Yu <yujin1@microsoft.com>
2022-04-21 17:41:22 +0800

    Change return type to promise

--- packages/teams-js/src/public/displayCapture.ts
+++ packages/teams-js/src/public/displayCapture.ts
@@ -6,38 +6,33 @@ import { FrameContexts } from './constants';
 import { ErrorCode, SdkError } from './interfaces';
 
 export namespace displayCapture {
-  /**
-   * @param callback Callback to invoke when the stream of selected display is fetched
-   */
-  export function getDisplayMedia(callback: (error: SdkError, displayStream: MediaStream) => void): void {
-    if (!callback) {
-      throw new Error('[displayCapture.getDisplayMedia] Callback cannot be null');
-    }
-    ensureInitialized(FrameContexts.content, FrameContexts.task);
+  export function getDisplayMedia(): Promise<MediaStream> {
+    return new Promise<MediaStream>((resolve, reject) => {
+      ensureInitialized(FrameContexts.content, FrameContexts.task);
 
-    if (!isCurrentSDKVersionAtLeast(displayCaptureAPIRequiredVersion)) {
-      const oldPlatformError: SdkError = { errorCode: ErrorCode.OLD_PLATFORM };
-      callback(oldPlatformError, undefined);
-      return;
-    }
+      if (!isCurrentSDKVersionAtLeast(displayCaptureAPIRequiredVersion)) {
+        const oldPlatformError: SdkError = { errorCode: ErrorCode.OLD_PLATFORM };
+        reject(oldPlatformError);
+      }
 
-    registerHandler('displayCapture.displayIdPicked', (displayId: string) => {
-      const mediaDevices = navigator.mediaDevices as any;
-      mediaDevices
-        .getUserMedia({
-          audio: false,
-          video: {
-            mandatory: {
-              chromeMediaSource: 'desktop',
-              chromeMediaSourceId: displayId,
+      registerHandler('displayCapture.displayIdPicked', (displayId: string) => {
+        const mediaDevices = navigator.mediaDevices as any;
+        mediaDevices
+          .getUserMedia({
+            audio: false,
+            video: {
+              mandatory: {
+                chromeMediaSource: 'desktop',
+                chromeMediaSourceId: displayId,
+              },
             },
-          },
-        })
-        .then((mediaStream: MediaStream) => {
-          callback(undefined, mediaStream);
-        });
-    });
+          })
+          .then((mediaStream: MediaStream) => {
+            resolve(mediaStream);
+          });
+      });
 
-    sendMessageToParent('displayCapture.showDisplayPicker');
+      sendMessageToParent('displayCapture.showDisplayPicker');
+    });
   }
 }
