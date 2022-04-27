c256f5d4a746aa21aa1be36fab0a77ed761c21ef
shrshindeMSFT <98348000+shrshindeMSFT@users.noreply.github.com>
2022-04-27 12:12:30 -0700

    shrshinde/isSupported-menus-check (#1126)
    * Adding isSupported check to menus capability
    
    * Change files

---
+++ packages/teams-js/test/public/menus.spec.ts
@@ -0,0 +1,260 @@
+import { app, FrameContexts, menus } from '../../src/public';
+import { errorNotSupportedOnPlatform } from '../../src/public/constants';
+import { _minRuntimeConfigToUninitialize } from '../../src/public/runtime';
+import { FramelessPostMocks } from '../framelessPostMocks';
+import { Utils } from '../utils';
+
+describe('Testing menus capability', () => {
+  describe('FRAMED - Testing menus capability', () => {
+    const framedMock = new Utils();
+
+    beforeEach(() => {
+      framedMock.processMessage = null;
+      framedMock.messages = [];
+      framedMock.childMessages = [];
+      framedMock.childWindow.closed = false;
+    });
+
+    afterEach(() => {
+      // Reset the object since it's a singleton
+      if (app._uninitialize) {
+        framedMock.setRuntimeConfig(_minRuntimeConfigToUninitialize);
+        app._uninitialize();
+      }
+    });
+
+    describe('Testing menus.setUpViews function', () => {
+      const viewConfiguration: menus.ViewConfiguration = {
+        id: 'some ID',
+        title: 'some Title',
+      };
+
+      it('should not allow calls before initialization', () => {
+        expect(() => menus.setUpViews([viewConfiguration], () => true)).toThrowError(
+          'The library has not yet been initialized',
+        );
+      });
+
+      Object.values(FrameContexts).forEach(frameContext => {
+        it(`menus.setUpViews should throw error when menus is not supported  when set to true in ${frameContext} context`, async () => {
+          await framedMock.initializeWithContext(frameContext);
+          framedMock.setRuntimeConfig({ apiVersion: 1, supports: {} });
+          expect.assertions(1);
+          try {
+            menus.setUpViews([viewConfiguration], () => true);
+          } catch (e) {
+            expect(e).toEqual(errorNotSupportedOnPlatform);
+          }
+        });
+
+        it(`menus.setUpViews should initiate the post message to Parent when set to true in ${frameContext} context `, async () => {
+          await framedMock.initializeWithContext(frameContext);
+          menus.setUpViews([viewConfiguration], () => true);
+          const message = framedMock.findMessageByFunc('setUpViews');
+          expect(message).not.toBeUndefined();
+          expect(message.args[0]).toStrictEqual([viewConfiguration]);
+        });
+      });
+    });
+
+    describe('Testing menus.setNavBarMenu function', () => {
+      const menuItem: menus.MenuItem = new menus.MenuItem();
+
+      it('should not allow calls before initialization', () => {
+        expect(() => menus.setNavBarMenu([menuItem], () => true)).toThrowError(
+          'The library has not yet been initialized',
+        );
+      });
+
+      Object.values(FrameContexts).forEach(frameContext => {
+        it(`menus.setNavBarMenu should throw error when menus is not supported  when set to true in ${frameContext} context`, async () => {
+          await framedMock.initializeWithContext(frameContext);
+          framedMock.setRuntimeConfig({ apiVersion: 1, supports: {} });
+          expect.assertions(1);
+          try {
+            menus.setNavBarMenu([menuItem], () => true);
+          } catch (e) {
+            expect(e).toEqual(errorNotSupportedOnPlatform);
+          }
+        });
+
+        it(`menus.setNavBarMenu should initiate the post message to Parent when set to true in ${frameContext} context `, async () => {
+          await framedMock.initializeWithContext(frameContext);
+          menus.setNavBarMenu([menuItem], () => true);
+          const message = framedMock.findMessageByFunc('setNavBarMenu');
+          expect(message).not.toBeUndefined();
+          expect(message.args[0]).toStrictEqual([menuItem]);
+        });
+      });
+    });
+
+    describe('Testing menus.showActionMenu function', () => {
+      const actionMenuParams: menus.ActionMenuParameters = {
+        title: 'Some Title',
+        items: [new menus.MenuItem()],
+      };
+
+      it('should not allow calls before initialization', () => {
+        expect(() => menus.showActionMenu(actionMenuParams, () => true)).toThrowError(
+          'The library has not yet been initialized',
+        );
+      });
+
+      Object.values(FrameContexts).forEach(frameContext => {
+        it(`menus.showActionMenu should throw error when menus is not supported  when set to true in ${frameContext} context`, async () => {
+          await framedMock.initializeWithContext(frameContext);
+          framedMock.setRuntimeConfig({ apiVersion: 1, supports: {} });
+          expect.assertions(1);
+          try {
+            menus.showActionMenu(actionMenuParams, () => true);
+          } catch (e) {
+            expect(e).toEqual(errorNotSupportedOnPlatform);
+          }
+        });
+
+        it(`menus.showActionMenu should initiate the post message to Parent when set to true in ${frameContext} context `, async () => {
+          await framedMock.initializeWithContext(frameContext);
+          menus.showActionMenu(actionMenuParams, () => true);
+          const message = framedMock.findMessageByFunc('showActionMenu');
+          expect(message).not.toBeUndefined();
+          expect(message.args[0]).toStrictEqual(actionMenuParams);
+        });
+      });
+    });
+  });
+
+  describe('FRAMELESS - Testing menus capability', () => {
+    const framedMock = new Utils();
+    const framelessMock = new FramelessPostMocks();
+
+    beforeEach(() => {
+      framedMock.processMessage = null;
+      framedMock.messages = [];
+      framelessMock.messages = [];
+      framedMock.childMessages = [];
+      framedMock.childWindow.closed = false;
+    });
+
+    afterEach(() => {
+      // Reset the object since it's a singleton
+      if (app._uninitialize) {
+        framedMock.setRuntimeConfig(_minRuntimeConfigToUninitialize);
+        app._uninitialize();
+      }
+    });
+
+    describe('Testing menus.setUpViews function', () => {
+      const viewConfiguration: menus.ViewConfiguration = {
+        id: 'some ID',
+        title: 'some Title',
+      };
+
+      it('should not allow calls before initialization', () => {
+        expect(() => menus.setUpViews([viewConfiguration], () => true)).toThrowError(
+          'The library has not yet been initialized',
+        );
+      });
+
+      Object.values(FrameContexts).forEach(frameContext => {
+        it(`menus.setUpViews should throw error when menus is not supported  when set to true in ${frameContext} context`, async () => {
+          await framelessMock.initializeWithContext(frameContext);
+          framedMock.setRuntimeConfig({ apiVersion: 1, supports: {} });
+          expect.assertions(4);
+          try {
+            menus.setUpViews([viewConfiguration], () => true);
+          } catch (e) {
+            expect(e).toEqual(errorNotSupportedOnPlatform);
+          }
+        });
+
+        it(`menus.setUpViews should initiate the post message to Parent when set to true in ${frameContext} context `, async () => {
+          await framelessMock.initializeWithContext(frameContext);
+          menus.setUpViews([viewConfiguration], () => true);
+          const message = framelessMock.findMessageByFunc('setUpViews');
+          expect(message).not.toBeUndefined();
+          expect(message.args[0]).toStrictEqual([viewConfiguration]);
+        });
+      });
+    });
+
+    describe('Testing menus.setNavBarMenu function', () => {
+      const menuItem: menus.MenuItem = new menus.MenuItem();
+      const expectedOutput: {
+        enabled: boolean;
+        selected: boolean;
+      } = { enabled: true, selected: false };
+
+      it('should not allow calls before initialization', () => {
+        expect(() => menus.setNavBarMenu([menuItem], () => true)).toThrowError(
+          'The library has not yet been initialized',
+        );
+      });
+
+      Object.values(FrameContexts).forEach(frameContext => {
+        it(`menus.setNavBarMenu should throw error when menus is not supported  when set to true in ${frameContext} context`, async () => {
+          await framelessMock.initializeWithContext(frameContext);
+          framedMock.setRuntimeConfig({ apiVersion: 1, supports: {} });
+          expect.assertions(4);
+          try {
+            menus.setNavBarMenu([menuItem], () => true);
+          } catch (e) {
+            expect(e).toEqual(errorNotSupportedOnPlatform);
+          }
+        });
+
+        it(`menus.setNavBarMenu should initiate the post message to Parent when set to true in ${frameContext} context `, async () => {
+          await framelessMock.initializeWithContext(frameContext);
+          menus.setNavBarMenu([menuItem], () => false);
+          const message = framelessMock.findMessageByFunc('setNavBarMenu');
+          expect(message).not.toBeUndefined();
+          expect(message.args[0]).toStrictEqual([expectedOutput]);
+        });
+      });
+    });
+
+    describe('Testing menus.showActionMenu function', () => {
+      const actionMenuParams: menus.ActionMenuParameters = {
+        title: 'Some Title',
+        items: [new menus.MenuItem()],
+      };
+
+      const expectedOutput: {
+        title: string;
+        items: {
+          enabled: boolean;
+          selected: boolean;
+        }[];
+      } = {
+        title: 'Some Title',
+        items: [{ enabled: true, selected: false }],
+      };
+
+      it('should not allow calls before initialization', () => {
+        expect(() => menus.showActionMenu(actionMenuParams, () => true)).toThrowError(
+          'The library has not yet been initialized',
+        );
+      });
+
+      Object.values(FrameContexts).forEach(frameContext => {
+        it(`menus.showActionMenu should throw error when menus is not supported  when set to true in ${frameContext} context`, async () => {
+          await framelessMock.initializeWithContext(frameContext);
+          framedMock.setRuntimeConfig({ apiVersion: 1, supports: {} });
+          expect.assertions(4);
+          try {
+            menus.showActionMenu(actionMenuParams, () => true);
+          } catch (e) {
+            expect(e).toEqual(errorNotSupportedOnPlatform);
+          }
+        });
+
+        it(`menus.showActionMenu should initiate the post message to Parent when set to true in ${frameContext} context `, async () => {
+          await framelessMock.initializeWithContext(frameContext);
+          menus.showActionMenu(actionMenuParams, () => true);
+          const message = framelessMock.findMessageByFunc('showActionMenu');
+          expect(message).not.toBeUndefined();
+          expect(message.args[0]).toStrictEqual(expectedOutput);
+        });
+      });
+    });
+  });
+});
