fef7aa39510e38b059e79a68cab35732f2ff48c0
jadahiya-MSFT <jdahiya8719@outlook.com>
2022-06-22 16:17:10 -0700

    Adding in powershell script for ADO pipeline

---
+++ packages/teams-js/view-published-packages-urls.ps1
@@ -0,0 +1,6 @@
+$version = Get-ChildItem -Path $(System.DefaultWorkingDirectory)/_OfficeDev.microsoft-teams-library-js/CDNFeed -Directory -Name
+Write-Host "Releasing version $version"
+Write-Host "CDN: https://res-sdf.cdn.office.net/teams-js/$version/js/MicrosoftTeams.min.js "
+Write-Host "NPM: https://www.npmjs.com/package/@microsoft/teams-js/v/$version"
+   
+displayName: 'View Published Package URLs'
