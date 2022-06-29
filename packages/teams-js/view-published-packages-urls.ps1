689944c807d2c67d99643490a8e7dd8944eff34b
Jay Dahiya <jdahiya8719@outlook.com>
2022-06-29 15:38:53 -0700

    Updating to take command argument

--- packages/teams-js/view-published-packages-urls.ps1
+++ packages/teams-js/view-published-packages-urls.ps1
@@ -1,4 +1,5 @@
-$version = Get-ChildItem -Path ../CDNFeed -Directory -Name
+$rootDir = $args[0]
+$version = Get-ChildItem -Path $rootDir/CDNFeed -Directory -Name
 Write-Host "Releasing version $version"
 Write-Host "CDN: https://res-sdf.cdn.office.net/teams-js/$version/js/MicrosoftTeams.min.js "
 Write-Host "NPM: https://www.npmjs.com/package/@microsoft/teams-js/v/$version"
