68ed24bae24f527f60259c2a2cfcd9b9bed3778f
jadahiya-MSFT <jdahiya8719@outlook.com>
2022-07-08 14:36:41 -0700

    Reverted view-published-packages-urls

--- packages/teams-js/scripts/view-published-packages-urls.ps1
+++ packages/teams-js/scripts/view-published-packages-urls.ps1
@@ -1,5 +1,5 @@
 $rootDir = $args[0]
-$version = Get-ChildItem -Path $Env:(System.DefaultWorkingDirectory)/_OfficeDev.microsoft-teams-library-js/CDNFeed -Directory -Name
+$version = Get-ChildItem -Path $rootDir/_OfficeDev.microsoft-teams-library-js/CDNFeed -Directory -Name
 Write-Host "Releasing version $version"
 Write-Host "CDN: https://res-sdf.cdn.office.net/teams-js/$version/js/MicrosoftTeams.min.js "
 Write-Host "NPM: https://www.npmjs.com/package/@microsoft/teams-js/v/$version"
