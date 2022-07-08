cbfcc955eac3233107a3051c21fed4367d0d911f
jadahiya-MSFT <jdahiya8719@outlook.com>
2022-07-08 15:18:38 -0700

    Modified powershell script to no longer need an input parameter, added in create-npmrc script to modify the existing npmrc with the correct NPMTOKEN value

--- packages/teams-js/scripts/create-npmrc.ps1
+++ packages/teams-js/scripts/create-npmrc.ps1
@@ -1,5 +1,1 @@
-$rootDir = $args[0]
-$version = Get-ChildItem -Path $Env:SYSTEM_DEFAULTWORKINGDIRECTORY/_OfficeDev.microsoft-teams-library-js/CDNFeed -Directory -Name
-Write-Host "Releasing version $version"
-Write-Host "CDN: https://res-sdf.cdn.office.net/teams-js/$version/js/MicrosoftTeams.min.js "
-Write-Host "NPM: https://www.npmjs.com/package/@microsoft/teams-js/v/$version"
+Set-Content -Path $Env:SYSTEM_DEFAULTWORKINGDIRECTORY/_OfficeDev.microsoft-teams-library-js/NPMFeed/.npmrc -Value "//registry.npmjs.org/:_authToken=$NPMTOKEN"
--- packages/teams-js/scripts/view-published-packages-urls.ps1
+++ packages/teams-js/scripts/view-published-packages-urls.ps1
@@ -1,5 +1,4 @@
-$rootDir = $args[0]
-$version = Get-ChildItem -Path $rootDir/_OfficeDev.microsoft-teams-library-js/CDNFeed -Directory -Name
+$version = Get-ChildItem -Path $Env:SYSTEM_DEFAULTWORKINGDIRECTORY//_OfficeDev.microsoft-teams-library-js/CDNFeed -Directory -Name
 Write-Host "Releasing version $version"
 Write-Host "CDN: https://res-sdf.cdn.office.net/teams-js/$version/js/MicrosoftTeams.min.js "
 Write-Host "NPM: https://www.npmjs.com/package/@microsoft/teams-js/v/$version"
