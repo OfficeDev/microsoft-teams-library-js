ad1a5cae075516ec905f44ea354d12216af5f68d
lnnl <lnnl.temp@hotmail.com>
2022-07-18 10:11:09 -0700

    remove copy-webpack-plugin

--- apps/teams-test-app/webpack.config.js
+++ apps/teams-test-app/webpack.config.js
@@ -4,15 +4,11 @@ const path = require('path');
 const commonConfig = require('./webpack.common.js');
 const { merge } = require('webpack-merge');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(commonConfig, {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'indexBundle.js',
   },
-  plugins: [
-    new HtmlWebPackPlugin({ template: './index_bundle.html', filename: 'index.html' }),
-    new CopyWebpackPlugin({ patterns: [{ from: './src/assets' }] }),
-  ],
+  plugins: [new HtmlWebPackPlugin({ template: './index_bundle.html', filename: 'index.html' })],
 });
