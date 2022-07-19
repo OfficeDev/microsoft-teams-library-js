462e03dedfa609e4ff095b95475154f60ac6c6e6
Ananya Barthakur <t-abarthakur@microsoft.com>
2022-07-19 10:57:52 -0700

    Adding calendar and pages capability + code clean up

--- apps/sample-app/src/components/Calendar.tsx
+++ apps/sample-app/src/components/Calendar.tsx
@@ -1,21 +1,57 @@
 import './styles.css';
 
 import { Button } from '@fluentui/react-components';
-import { calendar } from '@microsoft/teams-js';
+import { app, calendar } from '@microsoft/teams-js';
 import React from 'react';
 
-export const Calendar: React.FunctionComponent = () => {
-  const handleCalendar = async (): Promise<void> => {
-    // create calendar paramaters
+import { MessageListItem } from './Emails';
+
+const teamsDeepLinkHost = 'teams.microsoft.com';
+const teamsDeepLinkProtocol = 'https';
+const teamsDeepLinkAttendeesUrlParameterName = 'attendees';
+const teamsDeepLinkUrlPathForCalendar = '/l/meeting/new';
+
+export const handleNewMail = async (): Promise<void> => {
+  if (!calendar.isSupported()) {
+    const context = await app.getContext();
+    if (context?.app?.host?.name === 'Teams') {
+      const calendarParams: calendar.ComposeMeetingParams = {
+        attendees: ['emailAddress@microsoft.com'],
+      };
+      const attendeeSearchParameter =
+        calendarParams.attendees === undefined
+          ? ''
+          : `${teamsDeepLinkAttendeesUrlParameterName}=` +
+            calendarParams.attendees.map(attendee => encodeURIComponent(attendee)).join(',');
+
+      const deepLinkUrl = `${teamsDeepLinkProtocol}://${teamsDeepLinkHost}${teamsDeepLinkUrlPathForCalendar}?${attendeeSearchParameter}`;
+      app.openLink(deepLinkUrl);
+    } else {
+      alert('compose new meeting is not supported');
+    }
+  } else {
     const calendarParams: calendar.ComposeMeetingParams = {
-      attendees: ['t-abarthakur@microsoft.com'],
+      attendees: ['emailAdd@microsoft.com'],
     };
-    // send to 'compose meeting'
     await calendar.composeMeeting(calendarParams);
-  };
+  }
+};
+
+export const handleOpenMail = async (e: MessageListItem): Promise<void> => {
+  if (!calendar.isSupported()) {
+    alert('open mail item is not supported');
+  } else {
+    const openMailParams: calendar.OpenCalendarItemParams = {
+      itemId: e.key || '',
+    };
+    const result = await calendar.openCalendarItem(openMailParams);
+    return alert(result);
+  }
+};
+export const CalendarCapability: React.FunctionComponent = () => {
   return (
     <div>
-      <Button onClick={() => handleCalendar}>Add Meeting</Button>
+      <Button onClick={() => handleNewMail()}>Add Meeting</Button>
     </div>
   );
 };
