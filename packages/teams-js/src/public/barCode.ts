a57062a601d3bf3d6b7b2215de52f8b9dcc81d5d
Lakhveer Kaur <lakhveerkaur@microsoft.com>
2022-07-21 15:52:20 -0700

    prettier fix

--- packages/teams-js/src/public/barCode.ts
+++ packages/teams-js/src/public/barCode.ts
@@ -7,14 +7,14 @@ import { runtime } from './runtime';
 
 /**
  * Namespace to interact with the barcode scanning-specific part of the SDK.
- * 
+ *
  * @beta
  */
 export namespace barCode {
   /**
    * Data structure to customize the barcode scanning experience in scanBarCode API.
    * All properties in BarCodeConfig are optional and have default values in the platform
-   * 
+   *
    * @beta
    */
   export interface BarCodeConfig {
@@ -31,7 +31,7 @@ export namespace barCode {
    * @param barCodeConfig - input configuration to customize the barcode scanning experience
    *
    * @returns a scanned code
-   * 
+   *
    * @beta
    */
   export function scanBarCode(barCodeConfig: BarCodeConfig): Promise<string> {
@@ -52,7 +52,7 @@ export namespace barCode {
    * Checks whether or not media has user permission
    *
    * @returns true if the user has granted the app permission to media information, false otherwise
-   * 
+   *
    * @beta
    */
   export function hasPermission(): Promise<boolean> {
@@ -71,7 +71,7 @@ export namespace barCode {
    * Requests user permission for media
    *
    * @returns true if the user has granted the app permission to the media, false otherwise
-   * 
+   *
    * @beta
    */
   export function requestPermission(): Promise<boolean> {
@@ -90,7 +90,7 @@ export namespace barCode {
    * Checks if barCode capability is supported by the host
    *
    * @returns boolean to represent whether barCode is supported
-   * 
+   *
    * @beta
    */
   export function isSupported(): boolean {
