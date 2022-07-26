ad6ab3fd4a4539c6458faf3ab8f595051b0b8a25
Jay Dahiya <jadahiya@microsoft.com>
2022-07-26 12:29:47 -0700

    Testing npm test token

--- packages/teams-js/scripts/add-npmrc-npmtoken.ps1
+++ packages/teams-js/scripts/add-npmrc-npmtoken.ps1
@@ -1,1 +1,4 @@
+Write-Host "Testing Token ${NPM_TOKEN}"
 Set-Content -Path $Env:SYSTEM_DEFAULTWORKINGDIRECTORY/_OfficeDev.microsoft-teams-library-js/NPMFeed/.npmrc -Value "//registry.npmjs.org/:_authToken=${NPM_TOKEN}"
+$registry = Get-Content -Path $Env:SYSTEM_DEFAULTWORKINGDIRECTORY/_OfficeDev.microsoft-teams-library-js/NPMFeed/.npmrc
+Write-Host "Testing npmrc: $registry"
