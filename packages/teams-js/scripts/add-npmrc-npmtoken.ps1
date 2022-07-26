ecdf5563d319b19ade278ddcb3356a2acc67230c
Jay Dahiya <jadahiya@microsoft.com>
2022-07-26 16:20:16 -0700

    Adding in Env tag

--- packages/teams-js/scripts/add-npmrc-npmtoken.ps1
+++ packages/teams-js/scripts/add-npmrc-npmtoken.ps1
@@ -1,4 +1,4 @@
-Write-Host "Testing Token $NPM_TOKEN"
-Set-Content -Path $Env:SYSTEM_DEFAULTWORKINGDIRECTORY/_OfficeDev.microsoft-teams-library-js/NPMFeed/.npmrc -Value "//registry.npmjs.org/:_authToken=$NPM_TOKEN"
+Write-Host "Testing Token $Env:NPM_TOKEN"
+Set-Content -Path $Env:SYSTEM_DEFAULTWORKINGDIRECTORY/_OfficeDev.microsoft-teams-library-js/NPMFeed/.npmrc -Value "//registry.npmjs.org/:_authToken=$Env:NPM_TOKEN"
 $registry = Get-Content -Path $Env:SYSTEM_DEFAULTWORKINGDIRECTORY/_OfficeDev.microsoft-teams-library-js/NPMFeed/.npmrc
 Write-Host "Testing npmrc: $registry"
