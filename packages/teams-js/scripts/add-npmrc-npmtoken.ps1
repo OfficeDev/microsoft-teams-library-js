1f1916d0f504255969e27a0f76067c2e44b7652b
jadahiya-MSFT <95651173+jadahiya-MSFT@users.noreply.github.com>
2022-07-27 15:09:44 -0700

    Added Env expansion to npmrc file (#1270)
    * Testing npm test token
    
    * Change files
    
    * Testing token
    
    * Adding in Env tag
    
    * Added in Env expansion to NPM_TOKEN variable
    
    * Removing backticks
    
    * Removing backticks

--- packages/teams-js/scripts/add-npmrc-npmtoken.ps1
+++ packages/teams-js/scripts/add-npmrc-npmtoken.ps1
@@ -1,1 +1,1 @@
-Set-Content -Path $Env:SYSTEM_DEFAULTWORKINGDIRECTORY/_OfficeDev.microsoft-teams-library-js/NPMFeed/.npmrc -Value "//registry.npmjs.org/:_authToken=${NPM_TOKEN}"
+Set-Content -Path $Env:SYSTEM_DEFAULTWORKINGDIRECTORY/_OfficeDev.microsoft-teams-library-js/NPMFeed/.npmrc -Value "//registry.npmjs.org/:_authToken=$Env:NPM_TOKEN"
