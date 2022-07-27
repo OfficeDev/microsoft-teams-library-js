9cb340524f8feea1e1fba271e7596f42e6b6ffd8
AnanyaBarthakur1 <107085407+AnanyaBarthakur1@users.noreply.github.com>
2022-07-26 19:42:25 -0700

    code clean up (#1269)
    * code clean up
    
    * compact return statement
    
    Co-authored-by: AE ( ͡ಠ ʖ̯ ͡ಠ) <36546967+AE-MS@users.noreply.github.com>

--- apps/sample-app/src/components/Meetings.tsx
+++ apps/sample-app/src/components/Meetings.tsx
@@ -4,6 +4,8 @@ import { DetailsList } from '@fluentui/react';
 import { Message } from '@microsoft/microsoft-graph-types';
 import React from 'react';
 
+import { shouldShowMeeting } from './utils';
+
 interface MeetingProps {
   messages: Message[];
 }
@@ -15,7 +17,7 @@ export const MeetingList: React.FC<MeetingProps> = (props: MeetingProps) => {
   // using a for loop instead of 'map' function to remove instances of 'OOF'
   for (let i = 0; i < messages.length; i++) {
     const message = messages[i];
-    if (message['isCancelled'] || message['showAs'] === 'free') {
+    if (!shouldShowMeeting(message)) {
       continue;
     }
     const item: CalendarListItem = {
