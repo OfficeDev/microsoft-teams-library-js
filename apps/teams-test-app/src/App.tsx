b3b61ca9d4911c899f25ee3a0ae6059d859ea328
Diana Licon <107079865+DianaLicon-3@users.noreply.github.com>
2022-08-19 18:11:21 -0700

    T licondiana/search capability (#1298)
    * Proposal for sdk update to add search capability
    
    * Responding to PR feedback
    
    * Updating based on PR feedback
    
    * orange test app
    
    * orange testing
    
    * Change files
    
    * cleaning up
    
    * typo
    
    * unregister has no use
    
    * revert to prev commit
    
    * adding more description
    
    * using setresult to assert on it
    
    * adding more detail to setresult
    
    * updating search documentation, adding module wrapper
    
    * backticks and timestamp
    
    * fixing comments
    
    * fixing more comments
    
    Co-authored-by: Trevor Harris <trharris@microsoft.com>
    Co-authored-by: Marcin Juraszek <marcinj@microsoft.com>

--- apps/teams-test-app/src/App.tsx
+++ apps/teams-test-app/src/App.tsx
@@ -35,6 +35,7 @@ import NotificationAPIs from './components/privateApis/NotificationAPIs';
 import PrivateAPIs from './components/privateApis/PrivateAPIs';
 import TeamsAPIs from './components/privateApis/TeamsAPIs';
 import RemoteCameraAPIs from './components/RemoteCameraAPIs';
+import SearchAPIs from './components/SearchAPIs';
 import SharingAPIs from './components/SharingAPIs';
 import StageViewAPIs from './components/StageViewAPIs';
 import TeamsCoreAPIs from './components/TeamsCoreAPIs';
@@ -137,6 +138,7 @@ const App = (): ReactElement => {
       <PeopleAPIs />
       <PrivateAPIs />
       <RemoteCameraAPIs />
+      <SearchAPIs />
       <SharingAPIs />
       <WebStorageAPIs />
       <StageViewAPIs />
