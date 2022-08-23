7903a36a2dc9c0c9391343533f31a19b6d72d8aa
Shreyas Shinde <shrshinde@microsoft.com>
2022-08-23 10:38:07 -0700

    Adding version check for pages.self capability

--- packages/teams-js/src/public/pages.ts
+++ packages/teams-js/src/public/pages.ts
@@ -113,7 +113,7 @@ export namespace pages {
    * @returns Promise that resolves with the {@link InstanceConfig} object.
    */
   export function getConfig(): Promise<InstanceConfig> {
-    return new Promise<InstanceConfig>((resolve) => {
+    return new Promise<InstanceConfig>(resolve => {
       ensureInitialized(FrameContexts.content, FrameContexts.settings, FrameContexts.remove, FrameContexts.sidePanel);
       if (!isSupported()) {
         throw errorNotSupportedOnPlatform;
@@ -132,7 +132,7 @@ export namespace pages {
    * @returns Promise that resolves when the navigation has completed.
    */
   export function navigateCrossDomain(url: string): Promise<void> {
-    return new Promise<void>((resolve) => {
+    return new Promise<void>(resolve => {
       ensureInitialized(
         FrameContexts.content,
         FrameContexts.sidePanel,
@@ -161,7 +161,7 @@ export namespace pages {
    * @returns a promise that will resolve if the navigation was successful
    */
   export function navigateToApp(params: NavigateToAppParams): Promise<void> {
-    return new Promise<void>((resolve) => {
+    return new Promise<void>(resolve => {
       ensureInitialized(
         FrameContexts.content,
         FrameContexts.sidePanel,
@@ -262,7 +262,7 @@ export namespace pages {
      * @returns Promise that resolves when the navigation has completed.
      */
     export function navigateToTab(tabInstance: TabInstance): Promise<void> {
-      return new Promise<void>((resolve) => {
+      return new Promise<void>(resolve => {
         ensureInitialized();
         if (!isSupported()) {
           throw errorNotSupportedOnPlatform;
@@ -278,7 +278,7 @@ export namespace pages {
      * @returns Promise that resolves with the {@link TabInformation}. Contains information for the user's tabs that are owned by this application {@link TabInstance}.
      */
     export function getTabInstances(tabInstanceParameters?: TabInstanceParameters): Promise<TabInformation> {
-      return new Promise<TabInformation>((resolve) => {
+      return new Promise<TabInformation>(resolve => {
         ensureInitialized();
         if (!isSupported()) {
           throw errorNotSupportedOnPlatform;
@@ -293,7 +293,7 @@ export namespace pages {
      * @returns Promise that resolves with the {@link TabInformation}. Contains information for the users' most recently used tabs {@link TabInstance}.
      */
     export function getMruTabInstances(tabInstanceParameters?: TabInstanceParameters): Promise<TabInformation> {
-      return new Promise<TabInformation>((resolve) => {
+      return new Promise<TabInformation>(resolve => {
         ensureInitialized();
         if (!isSupported()) {
           throw errorNotSupportedOnPlatform;
@@ -352,7 +352,7 @@ export namespace pages {
      * @returns Promise that resolves when the operation has completed.
      */
     export function setConfig(instanceConfig: InstanceConfig): Promise<void> {
-      return new Promise<void>((resolve) => {
+      return new Promise<void>(resolve => {
         ensureInitialized(FrameContexts.content, FrameContexts.settings, FrameContexts.sidePanel);
         if (!isSupported()) {
           throw errorNotSupportedOnPlatform;
@@ -549,7 +549,7 @@ export namespace pages {
      * @returns Promise that resolves when the navigation has completed.
      */
     export function navigateBack(): Promise<void> {
-      return new Promise<void>((resolve) => {
+      return new Promise<void>(resolve => {
         ensureInitialized();
         if (!isSupported()) {
           throw errorNotSupportedOnPlatform;
@@ -736,6 +736,15 @@ export namespace pages {
         }
         resolve(send('pages.self.navigateTo', params));
       });
+    }
+
+    /**
+     * Checks if pages.self capability is supported by the host
+     * @returns true if the pages.self capability is enabled in runtime.supports.pages.self and
+     * false if it is disabled
+     */
+    export function isSupported(): boolean {
+      return runtime.supports.pages ? (runtime.supports.pages.self ? true : false) : false;
     }
   }
 }
--- packages/teams-js/src/public/runtime.ts
+++ packages/teams-js/src/public/runtime.ts
@@ -31,10 +31,11 @@ export interface IRuntime {
     readonly notifications?: {};
     readonly pages?: {
       readonly appButton?: {};
-      readonly tabs?: {};
-      readonly config?: {};
       readonly backStack?: {};
+      readonly config?: {};
       readonly fullTrust?: {};
+      readonly self?: {};
+      readonly tabs?: {};
     };
     readonly people?: {};
     readonly permissions?: {};
@@ -83,6 +84,7 @@ export let runtime: IRuntime = {
       config: undefined,
       backStack: undefined,
       fullTrust: undefined,
+      self: undefined,
     },
     people: undefined,
     permissions: undefined,
@@ -192,6 +194,13 @@ export const versionConstants: Record<string, Array<ICapabilityReqs>> = {
       hostClientTypes: [HostClientType.android, HostClientType.desktop, HostClientType.ios],
     },
   ],
+
+  '2.2.0': [
+    {
+      capability: { pages: { self: {} } },
+      hostClientTypes: v1HostClientTypes,
+    },
+  ],
 };
 
 /**
@@ -208,9 +217,9 @@ export const versionConstants: Record<string, Array<ICapabilityReqs>> = {
 export function generateBackCompatRuntimeConfig(highestSupportedVersion: string): IRuntime {
   let newSupports = { ...teamsRuntimeConfig.supports };
 
-  Object.keys(versionConstants).forEach((versionNumber) => {
+  Object.keys(versionConstants).forEach(versionNumber => {
     if (compareSDKVersions(highestSupportedVersion, versionNumber) >= 0) {
-      versionConstants[versionNumber].forEach((capabilityReqs) => {
+      versionConstants[versionNumber].forEach(capabilityReqs => {
         if (capabilityReqs.hostClientTypes.includes(GlobalVars.hostClientType)) {
           newSupports = {
             ...newSupports,
