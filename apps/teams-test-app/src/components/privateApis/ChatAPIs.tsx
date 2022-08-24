a08f986fede0e41c1193cf9a115b6d944f85fa3b
Angela Eun <aeun@Angelas-MacBook-Pro.local>
2022-08-24 16:26:18 -0700

    fix wrongly returned values

--- apps/teams-test-app/src/components/privateApis/ChatAPIs.tsx
+++ apps/teams-test-app/src/components/privateApis/ChatAPIs.tsx
@@ -18,10 +18,10 @@ const CheckChatCapability = (): React.ReactElement =>
     onClick: async () => `Chat module ${chat.isSupported() ? 'is' : 'is not'} supported`,
   });
 
-const OpenChat = (): React.ReactElement =>
+const DeprecatedOpenChat = (): React.ReactElement =>
   ApiWithTextInput<OpenSingleChatRequest>({
     name: 'openChat',
-    title: 'Open Chat',
+    title: '[Deprecated] Open Chat',
     onClick: {
       validateInput: (input) => {
         if (!input.user) {
@@ -35,10 +35,33 @@ const OpenChat = (): React.ReactElement =>
     },
   });
 
-const OpenGroupChat = (): React.ReactElement =>
+const OpenChat = (): React.ReactElement =>
+  ApiWithTextInput<OpenSingleChatRequest>({
+    name: 'openChat2',
+    title: 'Open Chat',
+    onClick: {
+      validateInput: (input) => {
+        if (!input.user || !input.message) {
+          throw new Error('Both user and message are required on the input');
+        }
+      },
+      submit: async (input) => {
+        return await chat
+          .openChat(input)
+          .then(() => {
+            return 'openChat was called';
+          })
+          .catch(() => {
+            return 'openChat' + noHostSdkMsg;
+          });
+      },
+    },
+  });
+
+const DeprecatedOpenGroupChat = (): React.ReactElement =>
   ApiWithTextInput<OpenGroupChatRequest>({
     name: 'openGroupChat',
-    title: 'Open Group Chat',
+    title: '[Deprecated] Open Group Chat',
     onClick: {
       validateInput: (input) => {
         if (!input.users) {
@@ -52,7 +75,30 @@ const OpenGroupChat = (): React.ReactElement =>
     },
   });
 
-const OpenConversation = (): React.ReactElement =>
+const OpenGroupChat = (): React.ReactElement =>
+  ApiWithTextInput<OpenGroupChatRequest>({
+    name: 'openGroupChat2',
+    title: 'Open Group Chat',
+    onClick: {
+      validateInput: (input) => {
+        if (!input.users) {
+          throw new Error('users is required on the input');
+        }
+      },
+      submit: async (input) => {
+        return await chat
+          .openGroupChat(input)
+          .then(() => {
+            return 'openGroupChat was called';
+          })
+          .catch(() => {
+            return 'chat.openGroupChat()' + noHostSdkMsg;
+          });
+      },
+    },
+  });
+
+const DeprecatedOpenConversation = (): React.ReactElement =>
   ApiWithTextInput<OpenConversationRequest>({
     name: 'openConversation2',
     title: 'Open Conversation',
@@ -94,6 +140,54 @@ const OpenConversation = (): React.ReactElement =>
     },
   });
 
+const OpenConversation = (): React.ReactElement =>
+  ApiWithTextInput<OpenConversationRequest>({
+    name: 'openConversation3',
+    title: 'Open Conversation',
+    onClick: {
+      validateInput: (input) => {
+        if (!input.entityId || !input.title || !input.subEntityId) {
+          throw new Error('entityId, title and subEntityId are required on the input');
+        }
+      },
+      submit: async (input, setResult) => {
+        input.onStartConversation = (conversationResponse) => {
+          setResult(
+            'Start Conversation Subentity Id ' +
+              conversationResponse.subEntityId +
+              ' Conversation Id: ' +
+              conversationResponse.conversationId +
+              ' Entity Id: ' +
+              conversationResponse.entityId +
+              ' Channel Id: ' +
+              conversationResponse.channelId,
+          );
+        };
+        input.onCloseConversation = (conversationResponse) => {
+          setResult(
+            'Close Conversation Subentity Id ' +
+              conversationResponse.subEntityId +
+              ' Conversation Id: ' +
+              conversationResponse.conversationId +
+              ' Entity Id: ' +
+              conversationResponse.entityId +
+              ' Channel Id: ' +
+              conversationResponse.channelId,
+          );
+        };
+
+        return await conversations
+          .openConversation(input)
+          .then(() => {
+            return 'conversations.openConversation() called';
+          })
+          .catch(() => {
+            return 'conversations.openConversation()' + noHostSdkMsg;
+          });
+      },
+    },
+  });
+
 const CloseConversation = (): React.ReactElement =>
   ApiWithoutInput({
     name: 'closeConversation',
@@ -116,8 +210,11 @@ const GetChatMembers = (): React.ReactElement =>
 
 const ConversationsAPIs = (): React.ReactElement => (
   <ModuleWrapper title="Chat">
+    <DeprecatedOpenChat />
     <OpenChat />
+    <DeprecatedOpenGroupChat />
     <OpenGroupChat />
+    <DeprecatedOpenConversation />
     <OpenConversation />
     <CloseConversation />
     <GetChatMembers />
