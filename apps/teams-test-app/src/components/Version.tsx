207606129434476f5ca40b1b592a90c92e635e06
AE ( ͡ಠ ʖ̯ ͡ಠ) <36546967+AE-MS@users.noreply.github.com>
2022-09-19 16:12:33 -0700

    Remove commented code

--- apps/teams-test-app/src/components/Version.tsx
+++ apps/teams-test-app/src/components/Version.tsx
@@ -1,43 +1,10 @@
 import { version } from '@microsoft/teams-js';
 import React from 'react';
 
-// const GetTeamsChannels = (): React.ReactElement =>
-//   ApiWithTextInput<string>({
-//     name: 'getTeamChannels2',
-//     title: 'Get Team Channels',
-//     onClick: {
-//       validateInput: (input) => {
-//         if (!input || typeof input !== 'string') {
-//           throw new Error('input is required and it has to be a string.');
-//         }
-//       },
-//       submit: async (groupId) => {
-//         return new Promise((res, rej) => {
-//           const onComplete = (error: SdkError, channels: teams.ChannelInfo[]): void => {
-//             if (error) {
-//               rej('getTeamChannels() error: ' + JSON.stringify(error));
-//             } else {
-//               res(JSON.stringify(channels));
-//             }
-//           };
-
-//           teams.getTeamChannels(groupId, onComplete);
-//         });
-//       },
-//     },
-//   });
-
 const Version = (): React.ReactElement => (
   <div>
     Current library version: <span id="version">{version ?? 'unavailable'}</span>
   </div>
 );
-
-// const Version: React.FC = () => (
-//   <ModuleWrapper title="TeamsAPIs">
-//     <GetTeamsChannels />
-//     <RefreshSiteUrl />
-//   </ModuleWrapper>
-// );
 
 export default Version;
