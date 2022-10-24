d287a61ce3b4ca46fa74907e32921d3d9d839b00
AE ( ͡ಠ ʖ̯ ͡ಠ) <36546967+AE-MS@users.noreply.github.com>
2022-10-24 14:26:12 -0700

    Use correct type in calendar tests

--- packages/teams-js/test/public/calendar.spec.ts
+++ packages/teams-js/test/public/calendar.spec.ts
@@ -261,7 +261,7 @@ describe('calendar', () => {
       expect(executeDeepLinkMessage).not.toBeNull();
       expect(executeDeepLinkMessage.args).toHaveLength(1);
 
-      const calendarDeepLink: URL = new URL(executeDeepLinkMessage.args[0] as string | URL);
+      const calendarDeepLink: URL = new URL(executeDeepLinkMessage.args[0] as string);
       validateCalendarDeepLinkPrefix(calendarDeepLink);
 
       utils.respondToMessage(executeDeepLinkMessage, true);
