2626b0c0cd87a72a57b78c91710e2fe896281015
Noah Darveau <noahdarveau@microsoft.com>
2022-11-07 14:16:05 -0800

    basic SSR test app added

---
+++ apps/ssr-test-app/pages/index.tsx
@@ -0,0 +1,62 @@
+import * as microsoftTeams from '@microsoft/teams-js';
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
+import React, { ReactElement, useEffect, useState } from 'react';
+
+export interface SSRProps {
+  renderString: string;
+  context: microsoftTeams.app.Context;
+}
+
+export default function IndexPage(props: SSRProps): ReactElement {
+  const [teamsContext, setTeamsContext] = useState({});
+
+  useEffect(() => {
+    microsoftTeams.app.initialize().then(() =>
+      microsoftTeams.app.getContext().then((ctx) => {
+        if (ctx) {
+          setTeamsContext(ctx);
+        }
+      }),
+    );
+  }, []);
+
+  // useEffect(() => {
+  //   document.getElementById('id01').innerHTML = "CSR'd";
+  // }, []);
+
+  return (
+    <div>
+      <Head>
+        <title>SSR Test App</title>
+      </Head>
+      <div> Hello World. </div>
+      <div>
+        <h1 id="id01">{props.renderString}</h1>
+        <pre>{JSON.stringify(props.context, null, 2)}</pre>
+      </div>
+    </div>
+  );
+}
+
+/**
+ *
+ * @param context
+ * @returns prop data
+ */
+export const getServerSideProps: GetServerSideProps = async () => {
+  let appContext: microsoftTeams.app.Context = null;
+
+  microsoftTeams.app
+    .initialize()
+    .then(() => microsoftTeams.app.getContext())
+    .then((ctx) => {
+      if (ctx) {
+        appContext = ctx;
+      }
+    });
+
+  return {
+    props: { renderString: "SSR'd", context: appContext },
+  };
+};
