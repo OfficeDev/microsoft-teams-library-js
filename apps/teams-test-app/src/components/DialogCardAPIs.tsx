e25782cf27977e65ee200e75c1af7d099a93f1f9
Shreyas Shinde <shrshinde@microsoft.com>
2022-11-22 14:57:56 -0800

    Backcompact

--- apps/teams-test-app/src/components/DialogCardAPIs.tsx
+++ apps/teams-test-app/src/components/DialogCardAPIs.tsx
@@ -1,11 +1,20 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import { AdaptiveCardDialogInfo, dialog } from '@microsoft/teams-js';
+import { AdaptiveCardDialogInfo, dialog, DialogInfo, IAppWindow, tasks } from '@microsoft/teams-js';
 import React, { ReactElement } from 'react';
 
 import { ApiWithoutInput, ApiWithTextInput } from './utils';
+import { isTestBackCompat } from './utils/isTestBackCompat';
 import { ModuleWrapper } from './utils/ModuleWrapper';
 
 const DialogCardAPIs = (): ReactElement => {
+  const childWindowRef = React.useRef<IAppWindow | null>(null);
+  const openDialogHelper = (childWindow: IAppWindow, setResult: (result: string) => void): void => {
+    childWindow.addEventListener('message', (message: string) => {
+      // Message from parent
+      setResult(message);
+    });
+    childWindowRef.current = childWindow;
+  };
   const CheckDialogAdaptiveCardCapability = (): ReactElement =>
     ApiWithoutInput({
       name: 'checkCapabilityDialogAdaptiveCard',
@@ -30,6 +39,14 @@ const DialogCardAPIs = (): ReactElement => {
           }
         },
         submit: async (adaptiveCardDialogInfo, setResult) => {
+          if (isTestBackCompat()) {
+            const taskInfo = adaptiveCardDialogInfo as DialogInfo;
+            const onComplete = (err: string, result: string | object): void => {
+              setResult('Error: ' + err + '\nResult: ' + result);
+            };
+            openDialogHelper(tasks.startTask(taskInfo, onComplete), setResult);
+            return '';
+          }
           const onComplete = (resultObj: dialog.ISdkResponse): void => {
             setResult('Error: ' + resultObj.err + '\nResult: ' + resultObj.result);
           };
