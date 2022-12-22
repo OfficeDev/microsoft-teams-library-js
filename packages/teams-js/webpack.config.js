a4d70ba61e60f97e014016da664aeb4341a7709f
noahdarveau-MSFT <noahdarveau@microsoft.com>
2022-12-21 16:34:09 -0800

    added explanatory comment

--- packages/teams-js/webpack.config.js
+++ packages/teams-js/webpack.config.js
@@ -27,6 +27,7 @@ module.exports = {
       type: 'umd',
       umdNamedDefine: true,
     },
+    //Prevents 'self' object conflict between nodejs and nextjs
     globalObject: 'this',
   },
   devtool: 'source-map',
