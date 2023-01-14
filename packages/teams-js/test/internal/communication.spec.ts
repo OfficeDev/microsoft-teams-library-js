37426fa922dcd4eb24cd9fe5e135ea5c853f019b
Trevor Harris <trharris@microsoft.com>
2023-01-13 16:53:49 -0800

    Added more unit tests for communication.ts
    Tests for uninitializeCommunication, sendMessageToParentAsync, and sendAndUnwrap

--- packages/teams-js/test/internal/communication.spec.ts
+++ packages/teams-js/test/internal/communication.spec.ts
@@ -304,6 +304,370 @@ describe('Testing communication', () => {
       });
     });
   });
+  describe('uninitializeCommunication', () => {
+    let utils: Utils = new Utils();
+    beforeEach(() => {
+      utils = new Utils();
+    });
+    afterEach(() => {
+      communication.Communication.currentWindow = utils.mockWindow;
+      communication.uninitializeCommunication();
+      communication.Communication.currentWindow = undefined;
+    });
+    it('should set Communication.parentWindow to null', () => {
+      app._initialize(utils.mockWindow);
+      communication.Communication.parentWindow = utils.mockWindow.parent;
+      expect(communication.Communication.parentWindow).not.toBeNull();
+      communication.uninitializeCommunication();
+      expect(communication.Communication.parentWindow).toBeNull();
+    });
+
+    it('should set Communication.parentOrigin to null', () => {
+      app._initialize(utils.mockWindow);
+      communication.Communication.parentOrigin = utils.mockWindow.parentOrigin;
+      expect(communication.Communication.parentOrigin).not.toBeNull();
+      communication.uninitializeCommunication();
+      expect(communication.Communication.parentOrigin).toBeNull();
+    });
+
+    it('should set Communication.childWindow to null', () => {
+      app._initialize(utils.mockWindow);
+      communication.Communication.childWindow = utils.mockWindow;
+      expect(communication.Communication.childWindow).not.toBeNull();
+      communication.uninitializeCommunication();
+      expect(communication.Communication.childWindow).toBeNull();
+    });
+
+    it('should set Communication.childOrigin to null', () => {
+      app._initialize(utils.mockWindow);
+      communication.Communication.childOrigin = utils.mockWindow.origin;
+      expect(communication.Communication.childOrigin).not.toBeNull();
+      communication.uninitializeCommunication();
+      expect(communication.Communication.childOrigin).toBeNull();
+    });
+
+    it('should empty the queue of messages for the current parent', () => {
+      expect.assertions(1);
+      communication.Communication.childWindow = utils.mockWindow;
+      communication.Communication.currentWindow = utils.mockWindow;
+      communication.Communication.parentWindow = utils.mockWindow;
+      // This function inserts a message into the parentMessageQueue
+      communication.sendMessageEventToChild('testMessage');
+      communication.uninitializeCommunication();
+
+      communication.Communication.parentWindow = utils.mockWindow;
+      communication.Communication.currentWindow = utils.mockWindow;
+      communication.Communication.currentWindow.setInterval = (fn) => {
+        fn();
+      };
+      communication.waitForMessageQueue(communication.Communication.parentWindow, () => {
+        // this callback only ever fires if the message queue associated with the passed in window is empty
+        expect(true).toBeTruthy();
+      });
+    });
+
+    it('should empty the queue of messages for the current child', () => {
+      expect.assertions(1);
+      communication.Communication.childWindow = utils.mockWindow;
+      communication.Communication.currentWindow = utils.mockWindow;
+      communication.Communication.parentWindow = utils.mockWindow.parent;
+      // This function inserts a message into the parentMessageQueue
+      communication.sendMessageEventToChild('testMessage');
+      communication.uninitializeCommunication();
+
+      communication.Communication.childWindow = utils.mockWindow;
+      communication.Communication.currentWindow = utils.mockWindow;
+      communication.Communication.currentWindow.setInterval = (fn) => {
+        fn();
+      };
+      communication.waitForMessageQueue(communication.Communication.childWindow, () => {
+        // this callback only ever fires if the message queue associated with the passed in window is empty
+        expect(true).toBeTruthy();
+      });
+    });
+
+    it('should reset messageIds to start at 0 again', () => {
+      expect.assertions(2);
+      app._initialize(utils.mockWindow);
+      GlobalVars.isFramelessWindow = false;
+      communication.Communication.parentWindow = utils.mockWindow.parent;
+      communication.Communication.parentOrigin = utils.validOrigin;
+      communication.sendMessageToParent('testAction');
+      communication.sendMessageToParent('testAction2');
+      const message = utils.findMessageByFunc('testAction2');
+
+      if (message) {
+        expect(message.id).toBe(1);
+      }
+
+      communication.uninitializeCommunication();
+      GlobalVars.isFramelessWindow = false;
+      communication.Communication.parentWindow = utils.mockWindow.parent;
+      communication.Communication.parentOrigin = utils.validOrigin;
+      communication.sendMessageToParent('testAction3');
+
+      const messageAfterUninitialize = utils.findMessageByFunc('testAction3');
+      if (messageAfterUninitialize) {
+        expect(messageAfterUninitialize.id).toBe(0);
+      }
+    });
+
+    it('unresolved message callbacks should not be triggered after communication has been uninitialized', () => {
+      app._initialize(utils.mockWindow);
+      GlobalVars.isFramelessWindow = false;
+      communication.Communication.parentWindow = utils.mockWindow.parent;
+      communication.Communication.parentOrigin = utils.validOrigin;
+      communication.Communication.currentWindow = utils.mockWindow;
+      let callbackWasCalled = false;
+
+      communication.sendMessageToParent('testAction', () => {
+        callbackWasCalled = true;
+      });
+      communication.uninitializeCommunication();
+      const secondMessage = utils.findMessageByFunc('testAction');
+      communication.processMessage({
+        originalEvent: { originalEvent: {} as DOMMessageEvent, func: '' },
+        func: '',
+        data: { id: secondMessage?.id, args: [] },
+        source: communication.Communication.parentWindow,
+        origin: utils.mockWindow.location.origin,
+      });
+
+      expect(callbackWasCalled).toBeFalsy();
+    });
+
+    it('unresolved message promises should not be triggered after communication has been uninitialized', async () => {
+      expect.assertions(1);
+      app._initialize(utils.mockWindow);
+      GlobalVars.isFramelessWindow = false;
+      communication.Communication.parentWindow = utils.mockWindow.parent;
+      communication.Communication.parentOrigin = utils.validOrigin;
+      communication.Communication.currentWindow = utils.mockWindow;
+
+      const messageParent = communication.sendMessageToParentAsync('testAction');
+
+      communication.uninitializeCommunication();
+      const secondMessage = utils.findMessageByFunc('testAction');
+      communication.processMessage({
+        originalEvent: { originalEvent: {} as DOMMessageEvent, func: '' },
+        func: '',
+        data: { id: secondMessage?.id, args: [] },
+        source: communication.Communication.parentWindow,
+        origin: utils.mockWindow.location.origin,
+      });
+
+      messageParent.then(() => expect(true).toBeTruthy());
+      expect(true).toBeTruthy();
+    });
+
+    it('the current window should not have a message listener on it after communication has been uninitialized', async () => {
+      app._initialize(utils.mockWindow);
+      utils.mockWindow.addEventListener('message', () => {
+        // This listener should not be called during the unit test
+        expect(true).toBeFalsy();
+      });
+
+      // eslint-disable-next-line strict-null-checks/all
+      expect(utils.processMessage).not.toBeNull();
+
+      communication.uninitializeCommunication();
+
+      // eslint-disable-next-line strict-null-checks/all
+      expect(utils.processMessage).toBeNull();
+    });
+  });
+  describe('sendMessageToParentAsync', () => {
+    let utils: Utils = new Utils();
+    const actionName = 'test';
+    beforeEach(() => {
+      utils = new Utils();
+      communication.uninitializeCommunication();
+      app._initialize(utils.mockWindow);
+    });
+    afterAll(() => {
+      communication.Communication.currentWindow = utils.mockWindow;
+      communication.uninitializeCommunication();
+    });
+    it('should send framelessPostMessage to window when running in a frameless window and Communication.currentWindow is set and has a nativeInterface', () => {
+      GlobalVars.isFramelessWindow = true;
+
+      communication.sendMessageToParentAsync(actionName);
+
+      expect(utils.messages.length).toBe(1);
+      expect(utils.messages[0].id).toBe(0);
+      expect(utils.messages[0].func).toBe(actionName);
+    });
+    it('should receive response to framelessPostMessage when running in a frameless window and Communication.currentWindow is set and has a nativeInterface', async () => {
+      utils.mockWindow.parent = undefined;
+      communication.initializeCommunication(undefined);
+
+      const messagePromise = communication.sendMessageToParentAsync(actionName);
+
+      const sentMessage = utils.findMessageByFunc(actionName);
+      if (sentMessage === null) {
+        throw new Error('No sent message was found');
+      }
+      utils.respondToNativeMessage(sentMessage, false, []);
+
+      return expect(messagePromise).resolves;
+    });
+    it('should never send message if there is no Communication.currentWindow when message is sent', () => {
+      GlobalVars.isFramelessWindow = true;
+      communication.Communication.currentWindow = undefined;
+
+      communication.sendMessageToParentAsync(actionName);
+
+      expect(utils.messages.length).toBe(0);
+    });
+    it('should never receive response to framelessPostMessage if there is no Communication.currentWindow when message is sent', async () => {
+      utils.mockWindow.parent = undefined;
+      communication.initializeCommunication(undefined);
+      communication.Communication.currentWindow = undefined;
+
+      const messagePromise = communication.sendMessageToParentAsync(actionName);
+
+      utils.respondToNativeMessage({ id: 0, func: actionName }, false, []);
+
+      messagePromise.then(() => expect(true).toBeFalsy());
+    });
+    it('should never send message if there is no nativeInterface on the currentWindow when message is sent', () => {
+      GlobalVars.isFramelessWindow = true;
+      communication.Communication.currentWindow.nativeInterface = undefined;
+
+      communication.sendMessageToParentAsync(actionName);
+
+      expect(utils.messages.length).toBe(0);
+    });
+    it('should never receive response to framelessPostMessage if there is no nativeInterface on the currentWindow when message is sent', async () => {
+      utils.mockWindow.parent = undefined;
+      communication.initializeCommunication(undefined);
+      communication.Communication.currentWindow.nativeInterface = undefined;
+
+      const messagePromise = communication.sendMessageToParentAsync(actionName);
+
+      utils.respondToNativeMessage({ id: 0, func: actionName }, false, []);
+
+      messagePromise.then(() => expect(true).toBeFalsy());
+    });
+    it('args passed in should be sent with the framelessPostMessage', () => {
+      GlobalVars.isFramelessWindow = true;
+
+      const arg1 = 'testArg1';
+      communication.sendMessageToParentAsync(actionName, [arg1]);
+
+      expect(utils.messages.length).toBe(1);
+      if (utils.messages[0].args === undefined) {
+        throw new Error('args expected on message');
+      }
+      expect(utils.messages[0].args.length).toBe(1);
+      expect(utils.messages[0].args[0]).toBe(arg1);
+    });
+    it('should send a message to window when running in a framed window and Communication.parentWindow and Communication.parentOrigin are set', () => {
+      GlobalVars.isFramelessWindow = false;
+      communication.Communication.parentWindow = utils.mockWindow.parent;
+      communication.Communication.parentOrigin = utils.validOrigin;
+
+      communication.sendMessageToParentAsync(actionName);
+
+      expect(utils.messages.length).toBe(1);
+      expect(utils.messages[0].id).toBe(0);
+      expect(utils.messages[0].func).toBe(actionName);
+    });
+    it('should receive response to postMessage when running in a framed window and Communication.currentWindow has a parent with an origin', async () => {
+      communication.initializeCommunication(undefined);
+      const initializeMessage = utils.findInitializeMessageOrThrow();
+      utils.respondToMessage(initializeMessage);
+
+      const messagePromise = communication.sendMessageToParentAsync(actionName);
+
+      const sentMessage = utils.findMessageByFunc(actionName);
+      if (sentMessage === null) {
+        throw new Error('No sent message was found');
+      }
+      utils.respondToMessage(sentMessage, false, []);
+
+      return expect(messagePromise).resolves;
+    });
+    it('args passed in should be sent with the postMessage', () => {
+      communication.initializeCommunication(undefined);
+      const initializeMessage = utils.findInitializeMessageOrThrow();
+      utils.respondToMessage(initializeMessage);
+
+      const arg1 = 'testArg1';
+      communication.sendMessageToParentAsync(actionName, [arg1]);
+
+      const sentMessage = utils.findMessageByFunc(actionName);
+      if (sentMessage === null) {
+        throw new Error('No sent message was found');
+      }
+
+      if (sentMessage.args === undefined) {
+        throw new Error('args expected on message');
+      }
+      expect(sentMessage.args.length).toBe(1);
+      expect(sentMessage.args[0]).toBe(arg1);
+    });
+    it('should not send postMessage until after initialization response received', () => {
+      communication.initializeCommunication(undefined);
+      const initializeMessage = utils.findInitializeMessageOrThrow();
+
+      communication.sendMessageToParentAsync(actionName);
+
+      let sentMessage = utils.findMessageByFunc(actionName);
+      if (sentMessage !== null) {
+        throw new Error('Should not find a sent message until after the initialization response was received');
+      }
+
+      utils.respondToMessage(initializeMessage);
+
+      sentMessage = utils.findMessageByFunc(actionName);
+      if (sentMessage === null) {
+        throw new Error('Did not find any message even after initialization response was received');
+      }
+    });
+  });
+  describe('sendAndUnwrap', () => {
+    let utils: Utils = new Utils();
+    const actionName = 'test';
+    const actionName2 = 'test2';
+    beforeEach(() => {
+      utils = new Utils();
+      communication.uninitializeCommunication();
+      app._initialize(utils.mockWindow);
+    });
+    afterAll(() => {
+      communication.Communication.currentWindow = utils.mockWindow;
+      communication.uninitializeCommunication();
+    });
+    it('should unwrap response returned in an array and return it not in an array', async () => {
+      expect.assertions(2);
+      communication.initializeCommunication(undefined);
+      const initializeMessage = utils.findInitializeMessageOrThrow();
+      utils.respondToMessage(initializeMessage);
+
+      const messagePromise = communication.sendAndUnwrap(actionName);
+
+      const sentMessage = utils.findMessageByFunc(actionName);
+      if (sentMessage === null) {
+        throw new Error('No sent message was found');
+      }
+      utils.respondToMessage(sentMessage, actionName);
+
+      const response = await messagePromise;
+      expect(response).toBe(actionName);
+
+      const messagePromise2 = communication.sendMessageToParentAsync(actionName2);
+
+      const sentMessage2 = utils.findMessageByFunc(actionName2);
+      if (sentMessage2 === null) {
+        throw new Error('No sent message was found');
+      }
+      utils.respondToMessage(sentMessage2, actionName2);
+
+      const response2 = await messagePromise2;
+      expect(response2).toStrictEqual([actionName2]);
+    });
+  });
   describe('processMessage', () => {
     it('fail if message has a missing data property', () => {
       const event = { badData: '' } as unknown as DOMMessageEvent;
