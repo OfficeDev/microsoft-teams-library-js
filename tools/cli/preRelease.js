048f7ea0b131893118a110fbfe093b5f599180e6
Shreyas Shinde <shrshinde@microsoft.com>
2023-01-24 11:52:44 -0800

    Update workspace

--- tools/cli/preRelease.js
+++ tools/cli/preRelease.js
@@ -26,8 +26,8 @@ const buildAndGetIntegrityHash = async () => {
   const absolutePathToManifestJson = path.resolve(__dirname, relativePathToManifestJson);
 
   console.log('Building @microsoft/teams-js');
-  await execShellCommand('pnpm workspace @microsoft/teams-js install');
-  await execShellCommand('pnpm workspace @microsoft/teams-js build');
+  await execShellCommand('pnpm --filter @microsoft/teams-js install');
+  await execShellCommand('pnpm --filter @microsoft/teams-js build');
 
   if (!fs.existsSync(absolutePathToManifestJson)) {
     throw `ERROR: MicrosoftTeams-manifest.json at ${absolutePathToManifestJson} was not found.`;
