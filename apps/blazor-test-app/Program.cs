3543dce6533d0061886426bd2366d7e8032f958a
noahdarveau-MSFT <109628470+noahdarveau-MSFT@users.noreply.github.com>
2023-02-21 09:03:55 -0800

    noahdarveau/blazor test app (#1578)
    * added Blazor test app to monorepo

---
+++ apps/blazor-test-app/Program.cs
@@ -0,0 +1,43 @@
+using Blazor_Test_App;
+using Blazor_Test_App.Interop.TeamsSDK;
+using Microsoft.IdentityModel.Logging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+var config = builder.Configuration.Get<ConfigOptions>();
+builder.Services.AddTeamsFx(config.TeamsFx.Authentication);
+builder.Services.AddScoped<MicrosoftTeams>();
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient("WebClient", client => client.Timeout = TimeSpan.FromSeconds(600));
+builder.Services.AddHttpContextAccessor();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapBlazorHub();
+    endpoints.MapFallbackToPage("/_Host");
+    endpoints.MapControllers();
+});
+
+app.Run();
+
