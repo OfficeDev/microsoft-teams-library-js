178f5842ac830391528e1c86c22111102199e88b
AE ( ͡ಠ ʖ̯ ͡ಠ) <36546967+AE-MS@users.noreply.github.com>
2023-02-27 13:03:51 -0800

    Add changefile

--- packages/teams-js/src/public/tasks.ts
+++ packages/teams-js/src/public/tasks.ts
@@ -23,9 +23,9 @@ export namespace tasks {
    * - For url-based dialogs, please use {@link dialog.url.open dialog.url.open(urlDialogInfo: UrlDialogInfo, submitHandler?: DialogSubmitHandler, messageFromChildHandler?: PostMessageChannel): void} .
    * - For url-based dialogs with bot interaction, please use {@link dialog.url.bot.open dialog.url.bot.open(botUrlDialogInfo: BotUrlDialogInfo, submitHandler?: DialogSubmitHandler, messageFromChildHandler?: PostMessageChannel): void}
    * - For Adaptive Card-based dialogs:
-   *  - In Teams, please continue to use this function until the new functions in {@link dialog.card} have been fully implemented. You can tell at runtime when they are implemented in Teams by checking
-   *    the return value of the {@link dialog.card.isSupported} function. This documentation line will also be removed once they have been fully implemented in Teams.
-   *  - In all other hosts, please use {@link dialog.card.open dialog.card.open(cardDialogInfo: CardDialogInfo, submitHandler?: DialogSubmitHandler, messageFromChildHandler?: PostMessageChannel): void}
+   *   - In Teams, please continue to use this function until the new functions in {@link dialog.adaptiveCard} have been fully implemented. You can tell at runtime when they are implemented in Teams by checking
+   *     the return value of the {@link dialog.adaptiveCard.isSupported} function. This documentation line will also be removed once they have been fully implemented in Teams.
+   *   - In all other hosts, please use {@link dialog.adaptiveCard.open dialog.adaptiveCard.open(cardDialogInfo: CardDialogInfo, submitHandler?: DialogSubmitHandler, messageFromChildHandler?: PostMessageChannel): void}
    *
    * Allows an app to open the task module.
    *
