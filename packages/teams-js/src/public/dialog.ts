9c43af8fe7c096be53ef64e932a52044fb7d8441
Lakhveer Kaur <lakhveerkaur@microsoft.com>
2023-03-17 15:06:18 -0700

    Deleting extra frameContext from ensureInitialization

--- packages/teams-js/src/public/dialog.ts
+++ packages/teams-js/src/public/dialog.ts
@@ -148,10 +148,7 @@ export namespace dialog {
      * @beta
      */
     export function submit(result?: string | object, appIds?: string | string[]): void {
-      // FrameContext content should not be here because dialog.submit can be called only from inside of a dialog (FrameContext task)
-      // but it's here because Teams mobile incorrectly returns FrameContext.content when calling app.getFrameContext().
-      // FrameContexts.content will be removed once the bug is fixed.
-      ensureInitialized(runtime, FrameContexts.content, FrameContexts.task);
+      ensureInitialized(runtime, FrameContexts.task);
       if (!isSupported()) {
         throw errorNotSupportedOnPlatform;
       }
