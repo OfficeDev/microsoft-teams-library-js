9c295236ec0b54c740a5e7bcdbf90dd5d30fdf18
Trevor Harris <trharris@microsoft.com>
2023-05-05 12:51:51 -0700

    Fixing CodeQL issues (#1728)
    * trying to get unit tests to run
    
    * Does this fix the CodeQL error
    
    * Added unit tests for gitCommands
    
    * Removing file that was just used for local testing
    
    * Turned on linting and fixed or ignored existing linting errors
    
    ---------
    
    Co-authored-by: AE ( ͡ಠ ʖ̯ ͡ಠ) <36546967+AE-MS@users.noreply.github.com>

--- tools/bundle-size-tools/src/utilities/getLastCommitHashFromPR.ts
+++ tools/bundle-size-tools/src/utilities/getLastCommitHashFromPR.ts
@@ -8,7 +8,7 @@ import { WebApi } from 'azure-devops-node-api';
 /**
  * Fetches the last commit hash for a PR.
  */
-export async function getLastCommitHashFromPR(adoConnection: WebApi, prId: number, repoGuid: string) {
+export async function getLastCommitHashFromPR(adoConnection: WebApi, prId: number, repoGuid: string): Promise<string> {
   const gitApi = await adoConnection.getGitApi();
   const prCommits = await gitApi.getPullRequestCommits(repoGuid, prId);
 
