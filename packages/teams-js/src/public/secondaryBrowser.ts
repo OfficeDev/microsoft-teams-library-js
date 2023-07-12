ca186bc03b11eae228b7dfacc68e16766a596916
Michael Obajemu <mobajemu@microsoft.com>
2023-07-12 12:28:51 -0700

    initial

--- packages/teams-js/src/public/secondaryBrowser.ts
+++ packages/teams-js/src/public/secondaryBrowser.ts
@@ -13,7 +13,8 @@ import { runtime } from './runtime';
  */
 export namespace secondaryBrowser {
   /**
-   * Open a URL in the secondary browser aka in-app browser
+   * Open a URL in the secondary browser, aka in-app browser. 
+   * The user will be redirected to an external browser on web and desktop
    *
    * @param url Url to open in the browser
    * @returns Promise that successfully resolves if the URL  opens in the secondaryBrowser
