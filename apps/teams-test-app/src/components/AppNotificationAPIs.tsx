1064593eaf24e4147501880b8b59d1a13706a6db
Bernice <t-bchiaha@microsoft.com>
2023-08-03 08:20:20 -0700

    Made some changes to the test app and included the e2e test data file

--- apps/teams-test-app/src/components/AppNotificationAPIs.tsx
+++ apps/teams-test-app/src/components/AppNotificationAPIs.tsx
@@ -1,7 +1,22 @@
 import { appNotification } from '@microsoft/teams-js';
 import React, { ReactElement } from 'react';
+
 import { ApiWithoutInput, ApiWithTextInput } from './utils';
 import { ModuleWrapper } from './utils/ModuleWrapper';
+
+function deserializeParam(
+  notificationDisplayParam: appNotification.NotificationDisplayParamForAppHost,
+): appNotification.NotificationDisplayParam {
+  return {
+    title: notificationDisplayParam.title,
+    content: notificationDisplayParam.content,
+    icon: notificationDisplayParam.notificationIconAsString
+      ? new URL(notificationDisplayParam.notificationIconAsString)
+      : undefined,
+    displayDurationInSeconds: notificationDisplayParam.displayDurationInSeconds,
+    notificationActionUrl: new URL(notificationDisplayParam.notificationActionUrlAsString),
+  };
+}
 
 const CheckAppNotificationCapability = (): React.ReactElement =>
   ApiWithoutInput({
@@ -11,7 +26,7 @@ const CheckAppNotificationCapability = (): React.ReactElement =>
   });
 
 const DisplayAppNotification = (): React.ReactElement =>
-  ApiWithTextInput<appNotification.NotificationDisplayParam>({
+  ApiWithTextInput<appNotification.NotificationDisplayParamForAppHost>({
     name: 'displayAppNotificationCapability',
     title: 'Display App Notification Capability',
     onClick: {
@@ -25,11 +40,13 @@ const DisplayAppNotification = (): React.ReactElement =>
         if (!input.displayDurationInSeconds) {
           throw new Error('displayDurationInSeconds is required');
         }
+        if (!input.notificationActionUrlAsString) {
+          throw new Error('notification URL is required');
+        }
       },
-      
+
       submit: async (input) => {
-        input.notificationActionUrl = new URL('https://www.example.com')
-        await appNotification.displayInAppNotification(input);
+        await appNotification.displayInAppNotification(deserializeParam(input));
         return 'Completed';
       },
     },
