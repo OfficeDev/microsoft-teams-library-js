410daf11b907a9bf88095a2edd0cac7526e34499
noahdarveau-MSFT <noahdarveau@microsoft.com>
2023-10-05 13:40:08 -0700

    upgraded blazor test app to use newer version of teamsfx

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
     curly: 'error',
     'simple-import-sort/imports': 'error',
     quotes: ['error', 'single', { avoidEscape: true }],
+    'no-restricted-syntax': ['warn', 'CallExpression[callee.name="window"]'],
   },
   env: {
     node: true,
