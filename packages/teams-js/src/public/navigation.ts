cc2d0a50d4b5d6c9aac79350748d7439a384e2ad
AE ( ͡ಠ ʖ̯ ͡ಠ) <36546967+AE-MS@users.noreply.github.com>
2023-10-25 15:54:38 -0700

    Remove console logs

--- packages/teams-js/src/public/navigation.ts
+++ packages/teams-js/src/public/navigation.ts
@@ -32,7 +32,6 @@ export function returnFocus(navigateForward?: boolean): void {
  * @param onComplete - The callback to invoke when the action is complete.
  */
 export function navigateToTab(tabInstance: TabInstance, onComplete?: onCompleteHandlerFunctionType): void {
-  console.log('HELLLLLLLLLLLO!!!!!!!!!!!!!!!!!');
   ensureInitialized(runtime);
   onComplete = onComplete ? onComplete : getGenericOnCompleteHandler();
   pages.tabs
