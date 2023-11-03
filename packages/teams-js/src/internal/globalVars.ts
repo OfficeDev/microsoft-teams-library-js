1f34b05fd76a1156013afe33ee980741d999238a
AE ( ͡ಠ ʖ̯ ͡ಠ) <36546967+AE-MS@users.noreply.github.com>
2023-11-02 21:24:11 -0700

    Fix more strictNullChecks violations

--- packages/teams-js/src/internal/globalVars.ts
+++ packages/teams-js/src/internal/globalVars.ts
@@ -3,10 +3,10 @@ export class GlobalVars {
   public static initializeCalled = false;
   public static initializeCompleted = false;
   public static additionalValidOrigins: string[] = [];
-  public static initializePromise: Promise<void>;
+  public static initializePromise: Promise<void> | null = null;
   public static isFramelessWindow = false;
-  public static frameContext: FrameContexts;
-  public static hostClientType: string;
+  public static frameContext: FrameContexts | undefined;
+  public static hostClientType: string | undefined;
   public static clientSupportedSDKVersion: string;
   public static printCapabilityEnabled = false;
 }
