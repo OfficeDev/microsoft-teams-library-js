a4c266899187d5ead90f7b46b93d588beebe457a
Kangxuan <kangxuanye@Kangxuans-MacBook-Pro.local>
2023-11-22 15:25:53 -0800

    comment out unused telemetry version number in sharing capability temporarily

--- packages/teams-js/src/public/sharing.ts
+++ packages/teams-js/src/public/sharing.ts
@@ -1,10 +1,12 @@
 import { sendAndHandleSdkError } from '../internal/communication';
 import { ensureInitialized } from '../internal/internalAPIs';
+import { ApiVersionNumber } from '../internal/telemetry';
 import { callCallbackWithSdkErrorFromPromiseAndReturnPromise, InputFunction } from '../internal/utils';
 import { errorNotSupportedOnPlatform, FrameContexts } from './constants';
 import { ErrorCode, SdkError } from './interfaces';
 import { runtime } from './runtime';
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
 const sharingTelemetryVersionNumber_v1: ApiVersionNumber = ApiVersionNumber.V_1;
 const sharingTelemetryVersionNumber_v2: ApiVersionNumber = ApiVersionNumber.V_2;
 
