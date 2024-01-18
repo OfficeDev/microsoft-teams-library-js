6dd91cc443da77f18515beed35416e4dc0e6945f
AE ( ͡ಠ ʖ̯ ͡ಠ) <36546967+AE-MS@users.noreply.github.com>
2024-01-18 12:39:12 -0800

    Revert changed file

--- packages/teams-js/test/utils.ts
+++ packages/teams-js/test/utils.ts
@@ -196,10 +196,7 @@ export class Utils {
     return null;
   };
 
-  public respondToMessage = async (
-    message: MessageRequest | NestedAppAuthRequest,
-    ...args: unknown[]
-  ): Promise<void> => {
+  public respondToMessage = async (message: MessageRequest | NestedAppAuthRequest, ...args: unknown[]): Promise<void> => {
     if (this.processMessage === null) {
       throw Error(
         `Cannot respond to message ${message.id} because processMessage function has not been set and is null`,
