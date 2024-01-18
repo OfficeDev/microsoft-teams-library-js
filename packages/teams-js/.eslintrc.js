d35b5aae848be2236a8bfabc2fea7ce08a21af8e
AE ( ͡ಠ ʖ̯ ͡ಠ) <36546967+AE-MS@users.noreply.github.com>
2024-01-17 16:55:19 -0800

    Order file and delete useless comment

--- packages/teams-js/.eslintrc.js
+++ packages/teams-js/.eslintrc.js
@@ -5,13 +5,12 @@ module.exports = {
   },
   plugins: ['strict-null-checks'],
   rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/no-namespace': 'off',
     '@typescript-eslint/no-unused-vars': [
       'error',
       { argsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_', varsIgnorePattern: '^_' },
     ],
-    '@typescript-eslint/interface-name-prefix': 'off',
     'no-inner-declarations': 'off',
     'strict-null-checks/all': 'warn',
   },
