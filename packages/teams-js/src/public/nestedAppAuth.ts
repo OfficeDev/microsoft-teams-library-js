459eb084d0f4502f4634af076fcc0e6b113b9f0e
baljesingh <baljesingh@microsoft.com>
2024-03-15 12:20:02 -0700

    Update nestedAppAuth.ts

--- packages/teams-js/src/public/nestedAppAuth.ts
+++ packages/teams-js/src/public/nestedAppAuth.ts
@@ -3,19 +3,19 @@ import { runtime } from './runtime';
 
 /**
  * @beta
- * Capability that allows embedding other applications inside an existing application
+ * Nested app auth capabilities
  */
 export namespace nestedAppAuthService {
   /**
    * @beta
-   * @returns true if embedded apps are supported in this host and false otherwise
+   * @returns true if NAA bridge is available and false otherwise
    */
   export function isBridgeAvailable(): boolean {
     return ensureInitialized(runtime) && runtime.supports.nestedAppAuth ? true : false;
   }
   /**
    * @beta
-   * @returns true if embedded apps are supported in this host and false otherwise
+   * @returns true if host is recommending NAA channel and false otherwise
    */
   export function isChannelRecommended(): boolean {
     return ensureInitialized(runtime) && runtime.isNAAChannelRecommended ? true : false;
