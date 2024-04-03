30b7c89c53b7b96510309eab54ad9bf1fb304b0c
lakhveerk <88564543+lakhveerk@users.noreply.github.com>
2024-04-03 15:26:31 -0700

    add appId validation in all externalApp* capabilities (#2250)
    * add appId validation
    
    * changefile
    
    * fixing regular expression for script tag
    
    * add return type to helper functions
    
    * fix regex
    
    * appId has to be opaque string
    
    * Adding unit tests
    
    * fixing unit test
    
    * fixing unit test

--- packages/teams-js/src/internal/utils.ts
+++ packages/teams-js/src/internal/utils.ts
@@ -400,3 +400,35 @@ export function ssrSafeWindow(): Window {
 export function inServerSideRenderingEnvironment(): boolean {
   return typeof window === 'undefined';
 }
+
+/**
+ * @param appID The app ID to validate
+ * @throws Error if appID is not a valid GUID
+ *
+ * @internal
+ * Limited to Microsoft-internal use
+ */
+export function validateAppId(appId: string): void {
+  if (hasScriptTags(appId) || isAppIdLengthExceeded(appId) || !isOpaque(appId)) {
+    throw new Error('App ID is not valid.');
+  }
+}
+
+function hasScriptTags(appId: string): boolean {
+  const scriptRegex = /<script[^>]*>[\s\S]*?<\/script[^>]*>/gi;
+  return scriptRegex.test(appId);
+}
+
+function isAppIdLengthExceeded(appId: string): boolean {
+  return appId.length >= 256;
+}
+
+function isOpaque(appId: string): boolean {
+  for (let i = 0; i < appId.length; i++) {
+    const charCode = appId.charCodeAt(i);
+    if (charCode < 32 || charCode > 126) {
+      return false;
+    }
+  }
+  return true;
+}
