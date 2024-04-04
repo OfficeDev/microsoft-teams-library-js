21ed814809060f470662f07a59914080a617802c
Ella Liu (from Dev Box) <liuella@microsoft.com>
2024-04-04 11:57:55 -0700

    Added third party test component in teams test app

--- apps/teams-test-app/src/App.tsx
+++ apps/teams-test-app/src/App.tsx
@@ -55,13 +55,12 @@ import SharingAPIs from './components/SharingAPIs';
 import StageViewAPIs from './components/StageViewAPIs';
 import TeamsCoreAPIs from './components/TeamsCoreAPIs';
 import ThirdPartyCloudStorageAPIs from './components/ThirdPartyCloudStorageAPIs';
+import CookieAccessComponent from './components/ThirdPatryCookies';
 import { isTestBackCompat } from './components/utils/isTestBackCompat';
 import Version from './components/Version';
 import VideoAPIs from './components/VideoEffectsApis';
 import VisualMediaAPIs from './components/VisualMediaAPIs';
 import WebStorageAPIs from './components/WebStorageAPIs';
-import CookieAccessComponent from './test_cookies';
-import './setCookie';
 
 const urlParams = new URLSearchParams(window.location.search);
 
@@ -128,17 +127,7 @@ export const generateRegistrationMsg = (changeCause: string): string => {
 const App = (): ReactElement => {
   return (
     <div>
-      {/* <div className="embedded-webpage">
-        <iframe
-          src="https://storage-access-api-demo-site-b.glitch.me/" // The URL of the webpage you want to embed
-          title="Embedded Webpage"
-          width="100%"
-          height="600px" // Adjust the size as needed
-          style={{ border: 'none' }} // Optional: Removes the border around the iframe
-        ></iframe>
-      </div> */}
       <div className="App-container">
-        <CookieAccessComponent />
         <AppAPIs />
         <AppInitializationAPIs />
         <AppInstallDialogAPIs />
@@ -194,6 +183,7 @@ const App = (): ReactElement => {
         <VideoAPIs />
         <VideoExAPIs />
         <VisualMediaAPIs />
+        <CookieAccessComponent />
       </div>
       <Version />
     </div>
