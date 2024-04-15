4c8684358bcce6fa99380eb42a63f4ce94b5616f
noahdarveau-MSFT <noahdarveau@microsoft.com>
2024-04-15 16:45:34 -0700

    added link to wiki in warning message

--- packages/teams-js/eslint-rules/eslint-plugin-recommend-const-enums/recommendConstEnums.js
+++ packages/teams-js/eslint-rules/eslint-plugin-recommend-const-enums/recommendConstEnums.js
@@ -19,7 +19,8 @@ module.exports = {
         if (enumType === 'regular') {
           context.report({
             node: node,
-            message: `Please consider if you can use a const enum for ${enumName} to minimize bundle size. If not, add "/* eslint-disable-next-line recommend-const-enums/recommend-const-enums */" to the line above to disable this warning.`,
+            message: `Please consider if you can use a const enum for ${enumName} to minimize bundle size. If not, add "/* eslint-disable-next-line recommend-const-enums/recommend-const-enums */" to the line above to disable this warning, 
+            as well as a comment explaining why you can't use a const enum. To learn more about why we typically want to use const enums, see https://github.com/OfficeDev/microsoft-teams-library-js/wiki/Notes-on-Enum-Usage.`,
             severity: 2,
           });
         }
