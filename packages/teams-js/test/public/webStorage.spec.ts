7d62f528632137aa6170f82d061a6914762f3dc2
AE ( ͡ಠ ʖ̯ ͡ಠ) <36546967+AE-MS@users.noreply.github.com>
2024-04-24 21:23:31 -0700

    Final test updates

--- packages/teams-js/test/public/webStorage.spec.ts
+++ packages/teams-js/test/public/webStorage.spec.ts
@@ -1,20 +1,46 @@
 import { errorLibraryNotInitialized } from '../../src/internal/constants';
 import { ApiName } from '../../src/internal/telemetry';
-import { FrameContexts } from '../../src/public/constants';
-import {
-  generateVersionBasedTeamsRuntimeConfig,
-  mapTeamsVersionToSupportedCapabilities,
-  versionAndPlatformAgnosticTeamsRuntimeConfig,
-} from '../../src/public/runtime';
+import { app } from '../../src/public';
+import { FrameContexts, errorNotSupportedOnPlatform } from '../../src/public/constants';
 import { webStorage } from '../../src/public/webStorage';
 import { Utils } from '../utils';
 
 describe('webStorage', () => {
-  const utils = new Utils();
+  let utils: Utils;
+
+  beforeEach(() => {
+    utils = new Utils();
+    utils.mockWindow.parent = undefined;
+    utils.messages = [];
+  });
+
+  afterEach(() => {
+    app._uninitialize();
+    jest.clearAllMocks();
+  });
+
   describe('webStorage.isSupported', () => {
     it('webStorage.isSupported should throw if called before initialization', () => {
+      expect.assertions(1);
+
       utils.uninitializeRuntimeConfig();
       expect(() => webStorage.isSupported()).toThrowError(new Error(errorLibraryNotInitialized));
+    });
+
+    it('webStorage.isSupported should return false if webStorage not supported in runtime', async () => {
+      expect.assertions(1);
+
+      await utils.initializeWithContext(FrameContexts.content);
+      utils.setRuntimeConfig({ apiVersion: 1, supports: {} });
+      expect(webStorage.isSupported()).not.toBeTruthy();
+    });
+
+    it('webStorage.isSupported should return true if webStorage supported in runtime', async () => {
+      expect.assertions(1);
+
+      await utils.initializeWithContext(FrameContexts.content);
+      utils.setRuntimeConfig({ apiVersion: 1, supports: { webStorage: {} } });
+      expect(webStorage.isSupported()).toBeTruthy();
     });
   });
 
@@ -22,9 +48,22 @@ describe('webStorage', () => {
     it('should not allow calls before initialization', async () => {
       expect.assertions(1);
 
+      utils.uninitializeRuntimeConfig();
+
       await webStorage
         .isWebStorageClearedOnUserLogOut()
         .catch((e) => expect(e).toMatchObject(new Error(errorLibraryNotInitialized)));
+    });
+
+    it('should throw errorNotSupportedOnPlatform if webStorage not supported in runtime config', async () => {
+      expect.assertions(1);
+
+      await utils.initializeWithContext(FrameContexts.content);
+      utils.setRuntimeConfig({ apiVersion: 1, supports: {} });
+
+      await webStorage
+        .isWebStorageClearedOnUserLogOut()
+        .catch((e) => expect(e).toMatchObject(errorNotSupportedOnPlatform));
     });
 
     async function testForReturnValue(returnValueToTest: boolean): Promise<void> {
