a7c645f7356af03092baab5fe2b1323b58958ee1
Jeff Klouda <jeklouda@microsoft.com>
2024-05-03 14:17:49 -0700

    use new scripts for onebranch pipelines

--- packages/teams-js/scripts/add-npmrc-npmtoken.ps1
+++ packages/teams-js/scripts/add-npmrc-npmtoken.ps1
@@ -1,1 +1,1 @@
-Set-Content -Path $Env:SYSTEM_DEFAULTWORKINGDIRECTORY/microsoft-teams-library-js-pipeline/NPMFeed/.npmrc -Value "//registry.npmjs.org/:_authToken=$Env:NPM_TOKEN"
+Set-Content -Path $Env:SYSTEM_DEFAULTWORKINGDIRECTORY/_OfficeDev.microsoft-teams-library-js/NPMFeed/.npmrc -Value "//registry.npmjs.org/:_authToken=$Env:NPM_TOKEN"
