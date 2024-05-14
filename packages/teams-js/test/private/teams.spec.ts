42c114e8a5264d7ac6a7fcada5eb6198dd89b379
jadahiya-MSFT <95651173+jadahiya-MSFT@users.noreply.github.com>
2024-05-14 14:43:21 -0700

    Adding UUIDs to MessageRequests and MessageResponses (#2240)
    * Adding UUIDs to MessageRequests and MessageResponses
    
    * Committing changefile
    
    * Added in MessageID and MessageUUID types, added in testing for UUID communication and id as numbers communication
    
    * Changing waitForResponse to have input of type MessageUUID
    
    * Updated references to use MessageID and MessageUUID
    
    * Fixed lint error
    
    * Created BaseUUID type
    
    * Added validation for uuid to handleParentMessage
    
    * Only validate UUIDs if there is one
    
    * Added in MessageUUID object
    
    * Testing new changes to UUID
    
    * Added in serialization of objects
    
    * Updated to address PR feedback
    
    * Added comment describing MessageID vs MessageUUID
    
    * Added in helper function
    
    * Removing comment since map.delete was verified to work correctly
    
    * Moved uuid object to interfaces.ts for more general use
    
    * Replaced Function in Map declarations
    
    * Updated to use serialized and deserialized Message Response objects
    
    * Added in unit tests for UUID class
    
    * Fixed lint error
    
    * Updating tests
    
    * Moved uuid object to new file uuidObject.ts, edited changefile verbage
    
    * Added in unit tests for testing callback map deletion and functionality
    
    * Removed unnused import
    
    * Added in logging for when a callbackID fails to be generated
    
    * Fixed a typo in communication spec
    
    * Updated serialization and deserialization and uuid toString function
    
    * Reverted toString() change due to compatibility issues
    
    * Removing unnecessary map clear due to already clearing in uninitialize and removeHandlers
    
    * Fixed an issue with removing message ids
    
    * Reverting uuid back to private
    
    ---------
    
    Co-authored-by: Trevor Harris <trharris@microsoft.com>
    Co-authored-by: AE ( ͡ಠ ʖ̯ ͡ಠ) <36546967+AE-MS@users.noreply.github.com>

--- packages/teams-js/test/private/teams.spec.ts
+++ packages/teams-js/test/private/teams.spec.ts
@@ -250,7 +250,6 @@ describe('Testing teams capabillity', () => {
     });
 
     describe('joinedTeams.isSupported function', () => {
-      const utils = new Utils();
       it('joinedTeams.isSupported should return false if the runtime says joinedTeams is not supported', async () => {
         await utils.initializeWithContext(FrameContexts.content);
         utils.setRuntimeConfig({ apiVersion: 1, supports: {} });
@@ -281,7 +280,6 @@ describe('Testing teams capabillity', () => {
     });
 
     describe('teams.fullTrust.isSupported function', () => {
-      const utils = new Utils();
       it('teams.fullTrust.isSupported should return false if the runtime says fullTrust is not supported', async () => {
         await utils.initializeWithContext(FrameContexts.content);
         utils.setRuntimeConfig({ apiVersion: 1, supports: {} });
