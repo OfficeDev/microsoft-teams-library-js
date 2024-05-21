ecf5818b98b4ad6b0789d70d509343697855ec43
Shreyas <shrshinde@microsoft.com>
2024-05-21 12:57:49 -0700

    Update

--- apps/teams-test-app/src/App.tsx
+++ apps/teams-test-app/src/App.tsx
@@ -68,8 +68,11 @@ import WebStorageAPIs from './components/WebStorageAPIs';
 
 const urlParams = new URLSearchParams(window.location.search);
 
+// The search url parameter 'origins' is used to get the valid message origins which will be passed to
+// the initialize function and based on the hosts it will allow the origins or not.
+// The valid message origins are separated by comma. For example: https://relecloud.com/?origins=https://relecoud.com,https://*.relecloud.com
 const getOriginsParam = urlParams.has('origins') && urlParams.get('origins') ? urlParams.get('origins') : '';
-const validMessageOrigins: string[] = getOriginsParam ? [getOriginsParam] : [];
+const validMessageOrigins: string[] | undefined = getOriginsParam ? getOriginsParam.split(',') : undefined;
 
 // This is added for custom initialization when app can be initialized based upon a trigger/click.
 if (!urlParams.has('customInit') || !urlParams.get('customInit')) {
