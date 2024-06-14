dc2659e02d4ed28c7a260d55d2d8ecc49be32358
Ava Vellozzi <t-avvellozzi>
2024-06-14 15:34:30 -0700

    Fixed build issues

--- apps/teamsjs-diagnostic-app/src/components/sample/Scenario1.tsx
+++ apps/teamsjs-diagnostic-app/src/components/sample/Scenario1.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import config from "./lib/config";
 import "./Scenario1.css";
 import { captureConsoleLogs } from "./LoggerUtility";
