81fb4d89e4710fde5da8686511cd54b2827354c6
Ava Vellozzi <t-avvellozzi>
2024-07-09 09:44:50 -0700

    Modified jest.config and setupTests

--- apps/teamsjs-diagnostic-app/jest.config.ts
+++ apps/teamsjs-diagnostic-app/jest.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
+  verbose: true,
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
@@ -10,6 +11,7 @@ const config: Config.InitialOptions = {
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
+  setupFilesAfterEnv: [require.resolve('./src/components/sample/setupTests.ts')],
   reporters: [
     'default',
     [
--- apps/teamsjs-diagnostic-app/src/components/sample/setupTests.ts
+++ apps/teamsjs-diagnostic-app/src/components/sample/setupTests.ts
@@ -1,29 +1,33 @@
 import '@testing-library/jest-dom';
 
 beforeAll(() => {
-  console.log('Mocking window.location');
-  const location = {
-    assign: jest.fn(),
-    href: 'http://localhost/',
-    reload: jest.fn(),
-    replace: jest.fn(),
-    origin: 'http://localhost',
-    pathname: '',
-    search: '',
-    hash: '',
-    host: 'localhost',
-    hostname: 'localhost',
-    protocol: 'http:',
-    port: '',
-  };
-  
-  // Deleting location property before defining it
-  delete (window as any).location;
-  
-  Object.defineProperty(window, 'location', {
-    value: location,
-    writable: true,
-  });
+  try {
+    console.log('Mocking window.location');
+    const location = {
+      assign: jest.fn(),
+      href: 'http://localhost/',
+      reload: jest.fn(),
+      replace: jest.fn(),
+      origin: 'http://localhost',
+      pathname: '',
+      search: '',
+      hash: '',
+      host: 'localhost',
+      hostname: 'localhost',
+      protocol: 'http:',
+      port: '',
+    };
+
+    delete (window as any).location;
+
+    Object.defineProperty(window, 'location', {
+      value: location,
+      writable: true,
+    });
+    console.log('window.location mocked successfully');
+  } catch (error) {
+    console.error('Error setting up window.location mock:', error);
+  }
 });
 
 export {};
