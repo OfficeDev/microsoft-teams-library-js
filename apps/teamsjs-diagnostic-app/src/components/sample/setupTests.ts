893a23c2d618f89d4eb2b224b39acf603fa0dd84
Ava Vellozzi <t-avvellozzi>
2024-07-09 09:22:38 -0700

    Modified setupTests file

--- apps/teamsjs-diagnostic-app/src/components/sample/setupTests.ts
+++ apps/teamsjs-diagnostic-app/src/components/sample/setupTests.ts
@@ -1,25 +1,29 @@
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
 
 beforeAll(() => {
-    console.log('Mocking window.location');
-    const location = {
-      assign: jest.fn(),
-      href: 'http://localhost/',
-      reload: jest.fn(),
-      replace: jest.fn(),
-      origin: 'http://localhost',
-      pathname: '',
-      search: '',
-      hash: '',
-      host: 'localhost',
-      hostname: 'localhost',
-      protocol: 'http:',
-      port: '',
-    };
-    Object.defineProperty(window, 'location', {
-      value: location,
-      writable: true,
-    });
-  });
+  console.log('Mocking window.location');
+  const location = {
+    assign: jest.fn(),
+    href: 'http://localhost/',
+    reload: jest.fn(),
+    replace: jest.fn(),
+    origin: 'http://localhost',
+    pathname: '',
+    search: '',
+    hash: '',
+    host: 'localhost',
+    hostname: 'localhost',
+    protocol: 'http:',
+    port: '',
+  };
   
-  export {};
+  // Deleting location property before defining it
+  delete (window as any).location;
+  
+  Object.defineProperty(window, 'location', {
+    value: location,
+    writable: true,
+  });
+});
+
+export {};
