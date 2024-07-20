12e1d1e1abab77cbbfb9c02770fc788e6b228ba6
Niharika D <niharikad@microsoft.com>
2024-07-20 16:46:06 -0700

    adding tags to the api

--- packages/teams-js/src/private/copilot.ts
+++ packages/teams-js/src/private/copilot.ts
@@ -3,17 +3,29 @@ import { runtime } from '../public/runtime';
 
 /**
  * @beta
- * User information required by specific apps
+ * @hidden
+ * Namespace to delegate M365 chat app specific APIs
+ * @internal
+ * Limited to Microsoft-internal use
  */
 export namespace copilot {
+  /**
+   * @beta
+   * @hidden
+   * User information required by specific apps
+   * @internal
+   * Limited to Microsoft-internal use
+   */
   export namespace license {
     /**
+     * @hidden
+     * @internal
+     * Limited to Microsoft-internal use
+     * @beta
      * This function is called by M365Chat app.
      * @returns true if a user had M365Chat license and false otherwise
      *
      * @throws Error if {@linkcode app.initialize} has not successfully completed
-     *
-     * @beta
      */
     export function isSupported(): boolean {
       return (ensureInitialized(runtime) && runtime.hostVersionsInfo?.m365ChatLicenseInfo?.hasM365ChatLicense) ?? false;
