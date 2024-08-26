77d997f2650677e63e1ff909e4659de390f2aaec
noahdarveau-MSFT <noahdarveau@microsoft.com>
2024-08-26 13:21:21 -0700

    rollup build esm and umd

--- packages/teams-js/rollup.config.mjs
+++ packages/teams-js/rollup.config.mjs
@@ -15,63 +15,102 @@ import version from './package.json' assert { type: 'json' };
 
 const baseConfig = createBasicConfig();
 
-export default {
-  input: './src/index.ts',
-  output: {
-    dir: 'dist',
-    name: 'MicrosoftTeams.min.js',
-    format: 'es',
-    preserveModules: true,
-    entryFileNames: '[name].js',
-    sourcemap: false,
-    //plugins: [terser()],
-    globals: {
-      buffer: 'Buffer',
-      tty: 'tty',
-      util: 'util',
-      os: 'os',
-    },
-  },
-  preserveEntrySignatures: 'strict',
-  plugins: [
-    resolve({
-      extension: ['.js', '.ts'],
-      preferBuiltins: true,
-      customResolveOptions: {
-        moduleDirectories: ['node_modules'],
+export default [
+  {
+    input: './src/index.ts',
+    output: {
+      dir: 'dist/',
+      name: 'MicrosoftTeams.min.js',
+      format: 'es',
+      preserveModules: true,
+      entryFileNames: '[name].js',
+      sourcemap: false,
+      //plugins: [terser()],
+      globals: {
+        buffer: 'Buffer',
+        tty: 'tty',
+        util: 'util',
+        os: 'os',
       },
-    }),
-    replace({
-      preventAssignment: true,
-      'process.env.NODE_ENV': JSON.stringify('production'),
-      PACKAGE_VERSION: JSON.stringify(version.version),
-    }),
-    typescript(),
-    json(),
-    commonjs(),
-    sri(),
-    nodePolyfills(),
-    RollupFilemanager({
-      events: {
-        onEnd: {
-          copy: [
-            {
-              source: './dist/packages/teams-js/**/*.js',
-              destination: '../../apps/blazor-test-app/wwwroot/js/',
-              isFlat: false,
-            },
-          ],
+    },
+    preserveEntrySignatures: 'strict',
+    plugins: [
+      resolve({
+        extension: ['.js', '.ts'],
+        preferBuiltins: true,
+        customResolveOptions: {
+          moduleDirectories: ['node_modules'],
+        },
+      }),
+      replace({
+        preventAssignment: true,
+        'process.env.NODE_ENV': JSON.stringify('production'),
+        PACKAGE_VERSION: JSON.stringify(version.version),
+      }),
+      typescript(),
+      json(),
+      commonjs(),
+      sri(),
+      nodePolyfills(),
+      RollupFilemanager({
+        events: {
+          onEnd: {
+            copy: [
+              {
+                source: './dist/packages/teams-js/**/*.js',
+                destination: '../../apps/blazor-test-app/wwwroot/js/',
+                isFlat: false,
+              },
+            ],
+          },
+        },
+      }),
+      {
+        apply: (compiler) => {
+          compiler.hooks.done.tap('wsi-test', () => {
+            const manifest = JSON.parse(readFileSync(join(__dirname, 'dist/MicrosoftTeams-manifest.json'), 'utf-8'));
+            // If for some reason hash was not generated for the assets, this test will fail in build.
+            expect(manifest['MicrosoftTeams.min.js'].integrity).toMatch(/sha384-.*/);
+          });
         },
       },
-    }),
-    {
-      apply: (compiler) => {
-        compiler.hooks.done.tap('wsi-test', () => {
-          const manifest = JSON.parse(readFileSync(join(__dirname, 'dist/MicrosoftTeams-manifest.json'), 'utf-8'));
-          // If for some reason hash was not generated for the assets, this test will fail in build.
-          expect(manifest['MicrosoftTeams.min.js'].integrity).toMatch(/sha384-.*/);
-        });
+    ],
+  },
+  {
+    input: './src/index.ts',
+    output: {
+      dir: 'dist/',
+      name: 'MicrosoftTeams',
+      format: 'umd',
+      preserveModules: false,
+      entryFileNames: 'MicrosoftTeams.umd.min.js',
+      sourcemap: false,
+      plugins: [terser()],
+      globals: {
+        buffer: 'Buffer',
+        tty: 'tty',
+        util: 'util',
+        os: 'os',
       },
     },
-  ],
-};
+    plugins: [
+      resolve({
+        extension: ['.js', '.ts'],
+        preferBuiltins: true,
+        customResolveOptions: {
+          moduleDirectories: ['node_modules'],
+        },
+      }),
+      replace({
+        preventAssignment: true,
+        'process.env.NODE_ENV': JSON.stringify('production'),
+        PACKAGE_VERSION: JSON.stringify(version.version),
+      }),
+      typescript(),
+      json(),
+      commonjs(),
+      sri(),
+      nodePolyfills(),
+    ],
+  },
+];
