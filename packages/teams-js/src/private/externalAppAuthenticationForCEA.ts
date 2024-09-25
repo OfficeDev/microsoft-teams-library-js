270d7b7aeebaf959dbe20175d1773c96acecc225
Lakhveer Kaur <lakhveerkaur@microsoft.com>
2024-09-25 13:32:09 -0700

    adding documentation for new CEA APIs

--- packages/teams-js/src/private/externalAppAuthenticationForCEA.ts
+++ packages/teams-js/src/private/externalAppAuthenticationForCEA.ts
@@ -2,6 +2,7 @@ import { sendMessageToParentAsync } from '../internal/communication';
 import { ensureInitialized } from '../internal/internalAPIs';
 import { ApiName, ApiVersionNumber, getApiVersionTag } from '../internal/telemetry';
 import { validateId } from '../internal/utils';
+import { AppId } from '../public';
 import { errorNotSupportedOnPlatform, FrameContexts } from '../public/constants';
 import { runtime } from '../public/runtime';
 import {
@@ -17,9 +18,28 @@ import {
 
 const externalAppAuthenticationTelemetryVersionNumber: ApiVersionNumber = ApiVersionNumber.V_3;
 
+/**
+ * @hidden
+ * Namespace to delegate authentication requests to the host for custom engine agents
+ * @internal
+ * Limited to Microsoft-internal use
+ * @beta
+ */
 export namespace externalAppAuthenticationForCEA {
+  /**
+   * @beta
+   * @hidden
+   * Signals to the host to perform SSO authentication for the application specified by the app ID, and then send the authResult to the application backend.
+   * @internal
+   * Limited to Microsoft-internal use
+   * @param appId Id of the application backend for which the host should attempt SSO authentication.
+   * @param conversationId ConversationId To tell the bot what conversation the calls are coming from
+   * @param authTokenRequest Parameters for SSO authentication
+   * @throws InvokeError if the host encounters an error while authenticating
+   * @returns A promise that resolves when authentication succeeds and rejects with InvokeError on failure
+   */
   export function authenticateWithSSO(
-    appId: string,
+    appId: AppId,
     conversationId: string,
     authTokenRequest: AuthTokenRequestParameters,
   ): Promise<void> {
@@ -28,7 +48,7 @@ export namespace externalAppAuthenticationForCEA {
     if (!isSupported()) {
       throw errorNotSupportedOnPlatform;
     }
-    validateId(appId, new Error('App id is not valid.'));
+
     validateId(conversationId, new Error('conversation id is not valid.'));
 
     return sendMessageToParentAsync(
@@ -45,8 +65,20 @@ export namespace externalAppAuthenticationForCEA {
     });
   }
 
+  /**
+   * @beta
+   * @hidden
+   * Signals to the host to perform authentication using the given authentication parameters and then send the auth result to the application backend.
+   * @internal
+   * Limited to Microsoft-internal use
+   * @param appId ID of the application backend to which the request and authentication response should be sent. This must be a UUID
+   * @param conversationId ConversationId To tell the bot what conversation the calls are coming from
+   * @param authenticateParameters Parameters for the authentication pop-up
+   * @throws InvokeError if the host encounters an error while authenticating
+   * @returns A promise that resolves from the application backend and rejects with InvokeError if the host encounters an error while authenticating
+   */
   export function authenticateWithOAuth(
-    appId: string,
+    appId: AppId,
     conversationId: string,
     authenticateParameters: AuthenticatePopUpParameters,
   ): Promise<void> {
@@ -55,7 +87,7 @@ export namespace externalAppAuthenticationForCEA {
     if (!isSupported()) {
       throw errorNotSupportedOnPlatform;
     }
-    validateId(appId, new Error('App id is not valid.'));
+
     validateId(conversationId, new Error('conversation id is not valid.'));
 
     // Ask the parent window to open an authentication window with the parameters provided by the caller.
@@ -80,8 +112,21 @@ export namespace externalAppAuthenticationForCEA {
     });
   }
 
+  /**
+   * @beta
+   * @hidden
+   * Signals to the host to perform authentication using the given authentication parameters and then resend the request to the application backend with the authentication result.
+   * @internal
+   * Limited to Microsoft-internal use
+   * @param appId ID of the application backend to which the request and authentication response should be sent. This must be a UUID
+   * @param conversationId ConversationId To tell the bot what conversation the calls are coming from
+   * @param authenticateParameters Parameters for the authentication pop-up
+   * @param originalRequestInfo Information about the original request that should be resent
+   * @throws InvokeError if the host encounters an error while authenticating or resending the request
+   * @returns A promise that resolves to the IActionExecuteResponse from the application backend and rejects with InvokeError if the host encounters an error while authenticating or resending the request
+   */
   export function authenticateAndResendRequest(
-    appId: string,
+    appId: AppId,
     conversationId: string,
     authenticateParameters: AuthenticatePopUpParameters,
     originalRequestInfo: IActionExecuteInvokeRequest,
@@ -91,7 +136,7 @@ export namespace externalAppAuthenticationForCEA {
     if (!isSupported()) {
       throw errorNotSupportedOnPlatform;
     }
-    validateId(appId, new Error('App id is not valid.'));
+
     validateId(conversationId, new Error('conversation id is not valid.'));
 
     validateOriginalRequestInfo(originalRequestInfo);
@@ -121,8 +166,22 @@ export namespace externalAppAuthenticationForCEA {
       }
     });
   }
+
+  /**
+   * @beta
+   * @hidden
+   * Signals to the host to perform SSO authentication for the application specified by the app ID and then resend the request to the application backend with the authentication result and originalRequestInfo
+   * @internal
+   * Limited to Microsoft-internal use
+   * @param appId ID of the application backend for which the host should attempt SSO authentication and resend the request and authentication response. This must be a UUID.
+   * @param conversationId ConversationId To tell the bot what conversation the calls are coming from
+   * @param authTokenRequest Parameters for SSO authentication
+   * @param originalRequestInfo Information about the original request that should be resent
+   * @throws InvokeError if the host encounters an error while authenticating or resending the request
+   * @returns A promise that resolves to the IActionExecuteResponse from the application backend and rejects with InvokeError if the host encounters an error while authenticating or resending the request
+   */
   export function authenticateWithSSOAndResendRequest(
-    appId: string,
+    appId: AppId,
     conversationId: string,
     authTokenRequest: AuthTokenRequestParameters,
     originalRequestInfo: IActionExecuteInvokeRequest,
@@ -132,7 +191,7 @@ export namespace externalAppAuthenticationForCEA {
     if (!isSupported()) {
       throw errorNotSupportedOnPlatform;
     }
-    validateId(appId, new Error('App id is not valid.'));
+
     validateId(conversationId, new Error('conversation id is not valid.'));
 
     validateOriginalRequestInfo(originalRequestInfo);
@@ -153,9 +212,28 @@ export namespace externalAppAuthenticationForCEA {
       }
     });
   }
+
+  /**
+   * @beta
+   * @hidden
+   * Checks if the externalAppAuthenticationForCEA capability is supported by the host
+   * @returns boolean to represent whether externalAppAuthenticationForCEA capability is supported
+   *
+   * @throws Error if {@linkcode app.initialize} has not successfully completed
+   *
+   * @internal
+   * Limited to Microsoft-internal use
+   */
   export function isSupported(): boolean {
     return ensureInitialized(runtime) && runtime.supports.externalAppAuthenticationForCEA ? true : false;
   }
+
+  /**
+   * @hidden
+   * @internal
+   * Limited to Microsoft-internal use
+   * @beta
+   */
 
   function validateOriginalRequestInfo(actionExecuteRequest: IActionExecuteInvokeRequest): void {
     if (actionExecuteRequest.type !== ActionExecuteInvokeRequestType) {
