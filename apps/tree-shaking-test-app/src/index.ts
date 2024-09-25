892169125d3a2ad1ebc4267229ccfe7cc02c1dbf
noahdarveau-MSFT <noahdarveau@microsoft.com>
2024-09-25 10:56:47 -0700

    cleaned up test app index file

--- apps/tree-shaking-test-app/src/index.ts
+++ apps/tree-shaking-test-app/src/index.ts
@@ -1,21 +1,5 @@
 import { barCode } from 'testlibraryfortreeshaking';
-//import { PrintTestMessage } from 'testlibraryfortreeshaking/app';
 import { geoLocation } from '@microsoft/teams-js';
-//import { app } from '@microsoft/teams-js';
-
-//import { barCode } from '@microsoft/teams-js';
-
-//import { clipboard } from '@microsoft/teams-js';
-//clipboard.read();
-
-//import { app } from '@microsoft/teams-js';
-//app.getContext();
-
 barCode.hasPermission();
-//PrintTestMessage();
-//barCode.scanBarCode({});
-//barCode.hasPermission();
-
-//app.initialize();
 geoLocation.requestPermission();
 geoLocation.map.isSupported();
