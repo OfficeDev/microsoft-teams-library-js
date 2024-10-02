d966fab48abf949d582658ccb11e845fc07639dc
Trevor Harris <trharris@microsoft.com>
2024-10-02 16:18:34 -0700

    Update packages/teams-js/src/public/emailAddress.ts

--- packages/teams-js/src/public/emailAddress.ts
+++ packages/teams-js/src/public/emailAddress.ts
@@ -2,9 +2,6 @@ import { validateEmailAddress } from '../internal/emailAddressValidation';
 
 /**
  * Represents a validated email.
- *
- * @hidden
- * Hide from docs.
  */
 export class EmailAddress {
   public constructor(private readonly val: string) {
