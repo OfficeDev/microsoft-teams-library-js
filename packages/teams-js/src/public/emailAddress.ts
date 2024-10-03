91ee90f34aa3ac6dae2a4b7642a5a8904c79a4f8
johnsle <johnsle@microsoft.com_msteamsmdb>
2024-10-02 17:07:36 -0700

    Address comments and build errors

--- packages/teams-js/src/public/emailAddress.ts
+++ packages/teams-js/src/public/emailAddress.ts
@@ -4,10 +4,17 @@ import { validateEmailAddress } from '../internal/emailAddressValidation';
  * Represents a validated email.
  */
 export class EmailAddress {
-  public constructor(private readonly val: string) {
+  /** Represents the input email address string */
+  private readonly val: string;
+
+  public constructor(val: string) {
+    this.val = val;
     validateEmailAddress(val);
   }
 
+  /**
+   * Retrieve the validated email address as a string.
+   */
   public toString(): string {
     return this.val;
   }
