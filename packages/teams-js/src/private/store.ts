0c8a439668ab93fdb3f36043412bec9c5edb9dcf
zhith <zhith@microsoft.com_msteamsmdb>
2024-10-21 13:18:43 +0800

    typo

--- packages/teams-js/src/private/store.ts
+++ packages/teams-js/src/private/store.ts
@@ -22,7 +22,7 @@ export namespace store {
 
   /**
    * Displays different forms of Store or App Install Dialogs based on the dialogType.
-   * Promise is returned once Store App intalization is completed.
+   * Promise is returned once Store App initialization is completed.
    */
   export function openStoreExperience(openStoreParams: OpenStoreParams): Promise<void> {
     throw new Error(`not implemented ${JSON.stringify(openStoreParams)}`);
