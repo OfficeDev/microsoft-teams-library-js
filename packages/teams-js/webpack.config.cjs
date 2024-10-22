342cd04619b1f34dee8cc4f73641086a9b84a7a1
noahdarveau-MSFT <109628470+noahdarveau-MSFT@users.noreply.github.com>
2024-10-22 14:40:01 -0700

    removed webpack dts plugin

--- packages/teams-js/webpack.config.cjs
+++ packages/teams-js/webpack.config.cjs
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 const TerserPlugin = require('terser-webpack-plugin');
-const DtsBundleWebpack = require('dts-bundle-webpack');
 const { SubresourceIntegrityPlugin } = require('webpack-subresource-integrity');
 const { readFileSync } = require('fs');
 const { join } = require('path');
