c3ae1b189f5d9c223cbe82d9d0b2f989f48934f5
noahdarveau-MSFT <109628470+noahdarveau-MSFT@users.noreply.github.com>
2024-10-22 16:37:02 -0700

    batch of treeshaking updates

--- packages/teams-js/src/public/calendar.ts
+++ packages/teams-js/src/public/calendar.ts
@@ -13,93 +13,91 @@ const calendarTelemetryVersionNumber: ApiVersionNumber = ApiVersionNumber.V_2;
 /**
  * Interact with the user's calendar, including opening calendar items and composing meetings.
  */
-export namespace calendar {
-  /**
-   * Opens a calendar item.
-   *
-   * @param openCalendarItemParams - object containing unique ID of the calendar item to be opened.
-   */
-  export function openCalendarItem(openCalendarItemParams: OpenCalendarItemParams): Promise<void> {
-    return new Promise<void>((resolve) => {
-      ensureInitialized(runtime, FrameContexts.content);
-      if (!isSupported()) {
-        throw new Error('Not supported');
-      }
+/**
+ * Opens a calendar item.
+ *
+ * @param openCalendarItemParams - object containing unique ID of the calendar item to be opened.
+ */
+export function openCalendarItem(openCalendarItemParams: OpenCalendarItemParams): Promise<void> {
+  return new Promise<void>((resolve) => {
+    ensureInitialized(runtime, FrameContexts.content);
+    if (!isSupported()) {
+      throw new Error('Not supported');
+    }
 
-      if (!openCalendarItemParams.itemId || !openCalendarItemParams.itemId.trim()) {
-        throw new Error('Must supply an itemId to openCalendarItem');
-      }
+    if (!openCalendarItemParams.itemId || !openCalendarItemParams.itemId.trim()) {
+      throw new Error('Must supply an itemId to openCalendarItem');
+    }
 
+    resolve(
+      sendAndHandleStatusAndReason(
+        getApiVersionTag(calendarTelemetryVersionNumber, ApiName.Calendar_OpenCalendarItem),
+        'calendar.openCalendarItem',
+        openCalendarItemParams,
+      ),
+    );
+  });
+}
+
+/**
+ * Compose a new meeting in the user's calendar.
+ *
+ * @param composeMeetingParams - object containing various properties to set up the meeting details.
+ */
+export function composeMeeting(composeMeetingParams: ComposeMeetingParams): Promise<void> {
+  return new Promise<void>((resolve) => {
+    ensureInitialized(runtime, FrameContexts.content);
+    if (!isSupported()) {
+      throw new Error('Not supported');
+    }
+    const apiVersionTag = getApiVersionTag(calendarTelemetryVersionNumber, ApiName.Calendar_ComposeMeeting);
+    if (runtime.isLegacyTeams) {
       resolve(
         sendAndHandleStatusAndReason(
-          getApiVersionTag(calendarTelemetryVersionNumber, ApiName.Calendar_OpenCalendarItem),
-          'calendar.openCalendarItem',
-          openCalendarItemParams,
+          apiVersionTag,
+          'executeDeepLink',
+          createTeamsDeepLinkForCalendar(
+            composeMeetingParams.attendees,
+            composeMeetingParams.startTime,
+            composeMeetingParams.endTime,
+            composeMeetingParams.subject,
+            composeMeetingParams.content,
+          ),
         ),
       );
-    });
-  }
+    } else {
+      resolve(sendAndHandleStatusAndReason(apiVersionTag, 'calendar.composeMeeting', composeMeetingParams));
+    }
+  });
+}
 
-  /**
-   * Compose a new meeting in the user's calendar.
-   *
-   * @param composeMeetingParams - object containing various properties to set up the meeting details.
-   */
-  export function composeMeeting(composeMeetingParams: ComposeMeetingParams): Promise<void> {
-    return new Promise<void>((resolve) => {
-      ensureInitialized(runtime, FrameContexts.content);
-      if (!isSupported()) {
-        throw new Error('Not supported');
-      }
-      const apiVersionTag = getApiVersionTag(calendarTelemetryVersionNumber, ApiName.Calendar_ComposeMeeting);
-      if (runtime.isLegacyTeams) {
-        resolve(
-          sendAndHandleStatusAndReason(
-            apiVersionTag,
-            'executeDeepLink',
-            createTeamsDeepLinkForCalendar(
-              composeMeetingParams.attendees,
-              composeMeetingParams.startTime,
-              composeMeetingParams.endTime,
-              composeMeetingParams.subject,
-              composeMeetingParams.content,
-            ),
-          ),
-        );
-      } else {
-        resolve(sendAndHandleStatusAndReason(apiVersionTag, 'calendar.composeMeeting', composeMeetingParams));
-      }
-    });
-  }
+/**
+ * Checks if the calendar capability is supported by the host
+ * @returns boolean to represent whether the calendar capability is supported
+ *
+ * @throws Error if {@linkcode app.initialize} has not successfully completed
+ */
+export function isSupported(): boolean {
+  return ensureInitialized(runtime) && runtime.supports.calendar ? true : false;
+}
 
-  /**
-   * Checks if the calendar capability is supported by the host
-   * @returns boolean to represent whether the calendar capability is supported
-   *
-   * @throws Error if {@linkcode app.initialize} has not successfully completed
-   */
-  export function isSupported(): boolean {
-    return ensureInitialized(runtime) && runtime.supports.calendar ? true : false;
-  }
+/** Open calendar item parameters. */
+export interface OpenCalendarItemParams {
+  /** An unique base64-encoded string id that represents the event's unique identifier of the calendar item to be opened. */
+  itemId: string;
+}
 
-  /** Open calendar item parameters. */
-  export interface OpenCalendarItemParams {
-    /** An unique base64-encoded string id that represents the event's unique identifier of the calendar item to be opened. */
-    itemId: string;
-  }
+/** Compose meeting parameters */
 
-  /** Compose meeting parameters */
-
-  export interface ComposeMeetingParams {
-    /** An array of email addresses, user name, or user id of the attendees to invite to the meeting. */
-    attendees?: string[];
-    /** The start time of the meeting in MM/DD/YYYY HH:MM:SS format. */
-    startTime?: string;
-    /** The end time of the meeting in MM/DD/YYYY HH:MM:SS format. */
-    endTime?: string;
-    /** The subject line of the meeting. */
-    subject?: string;
-    /** The body content of the meeting. */
-    content?: string;
-  }
+export interface ComposeMeetingParams {
+  /** An array of email addresses, user name, or user id of the attendees to invite to the meeting. */
+  attendees?: string[];
+  /** The start time of the meeting in MM/DD/YYYY HH:MM:SS format. */
+  startTime?: string;
+  /** The end time of the meeting in MM/DD/YYYY HH:MM:SS format. */
+  endTime?: string;
+  /** The subject line of the meeting. */
+  subject?: string;
+  /** The body content of the meeting. */
+  content?: string;
 }
--- packages/teams-js/src/public/call.ts
+++ packages/teams-js/src/public/call.ts
@@ -14,88 +14,86 @@ const callTelemetryVersionNumber: ApiVersionNumber = ApiVersionNumber.V_2;
 /**
  * Used to interact with call functionality, including starting calls with other users.
  */
-export namespace call {
-  /** Modalities that can be associated with a call. */
-  export enum CallModalities {
-    /** Indicates that the modality is unknown or undefined. */
-    Unknown = 'unknown',
-    /** Indicates that the call includes audio. */
-    Audio = 'audio',
-    /** Indicates that the call includes video. */
-    Video = 'video',
-    /** Indicates that the call includes video-based screen sharing. */
-    VideoBasedScreenSharing = 'videoBasedScreenSharing',
-    /** Indicates that the call includes data sharing or messaging. */
-    Data = 'data',
-  }
+/** Modalities that can be associated with a call. */
+export enum CallModalities {
+  /** Indicates that the modality is unknown or undefined. */
+  Unknown = 'unknown',
+  /** Indicates that the call includes audio. */
+  Audio = 'audio',
+  /** Indicates that the call includes video. */
+  Video = 'video',
+  /** Indicates that the call includes video-based screen sharing. */
+  VideoBasedScreenSharing = 'videoBasedScreenSharing',
+  /** Indicates that the call includes data sharing or messaging. */
+  Data = 'data',
+}
 
-  /** Represents parameters for {@link startCall | StartCall}. */
-  export interface StartCallParams {
-    /**
-     * Comma-separated list of user IDs representing the participants of the call.
-     *
-     * @remarks
-     * Currently the User ID field supports the Microsoft Entra UserPrincipalName,
-     * typically an email address, or in case of a PSTN call, it supports a pstn
-     * mri 4:\<phonenumber>.
-     */
-    targets: string[];
-    /**
-     * List of modalities for the call. Defaults to [“audio”].
-     */
-    requestedModalities?: CallModalities[];
-    /**
-     * An optional parameter that informs about the source of the deep link
-     */
-    source?: string;
-  }
-
+/** Represents parameters for {@link startCall | StartCall}. */
+export interface StartCallParams {
   /**
-   * Starts a call with other users
+   * Comma-separated list of user IDs representing the participants of the call.
    *
-   * @param startCallParams - Parameters for the call
-   *
-   * @throws Error if call capability is not supported
-   * @throws Error if host notifies of a failed start call attempt in a legacy Teams environment
-   * @returns always true if the host notifies of a successful call inititation
+   * @remarks
+   * Currently the User ID field supports the Microsoft Entra UserPrincipalName,
+   * typically an email address, or in case of a PSTN call, it supports a pstn
+   * mri 4:\<phonenumber>.
    */
-  export function startCall(startCallParams: StartCallParams): Promise<boolean> {
-    const apiVersionTag = getApiVersionTag(callTelemetryVersionNumber, ApiName.Call_StartCall);
-    return new Promise((resolve) => {
-      ensureInitialized(runtime, FrameContexts.content, FrameContexts.task);
-      if (!isSupported()) {
-        throw errorNotSupportedOnPlatform;
-      }
-      if (runtime.isLegacyTeams) {
-        resolve(
-          sendAndUnwrap(
-            apiVersionTag,
-            'executeDeepLink',
-            createTeamsDeepLinkForCall(
-              startCallParams.targets,
-              startCallParams.requestedModalities?.includes(CallModalities.Video),
-              startCallParams.source,
-            ),
-          ).then((result: boolean) => {
-            if (!result) {
-              throw new Error(errorCallNotStarted);
-            }
-            return result;
-          }),
-        );
-      } else {
-        return sendMessageToParent(apiVersionTag, 'call.startCall', [startCallParams], resolve);
-      }
-    });
-  }
-
+  targets: string[];
   /**
-   * Checks if the call capability is supported by the host
-   * @returns boolean to represent whether the call capability is supported
-   *
-   * @throws Error if {@linkcode app.initialize} has not successfully completed
+   * List of modalities for the call. Defaults to [“audio”].
    */
-  export function isSupported(): boolean {
-    return ensureInitialized(runtime) && runtime.supports.call ? true : false;
-  }
+  requestedModalities?: CallModalities[];
+  /**
+   * An optional parameter that informs about the source of the deep link
+   */
+  source?: string;
+}
+
+/**
+ * Starts a call with other users
+ *
+ * @param startCallParams - Parameters for the call
+ *
+ * @throws Error if call capability is not supported
+ * @throws Error if host notifies of a failed start call attempt in a legacy Teams environment
+ * @returns always true if the host notifies of a successful call inititation
+ */
+export function startCall(startCallParams: StartCallParams): Promise<boolean> {
+  const apiVersionTag = getApiVersionTag(callTelemetryVersionNumber, ApiName.Call_StartCall);
+  return new Promise((resolve) => {
+    ensureInitialized(runtime, FrameContexts.content, FrameContexts.task);
+    if (!isSupported()) {
+      throw errorNotSupportedOnPlatform;
+    }
+    if (runtime.isLegacyTeams) {
+      resolve(
+        sendAndUnwrap(
+          apiVersionTag,
+          'executeDeepLink',
+          createTeamsDeepLinkForCall(
+            startCallParams.targets,
+            startCallParams.requestedModalities?.includes(CallModalities.Video),
+            startCallParams.source,
+          ),
+        ).then((result: boolean) => {
+          if (!result) {
+            throw new Error(errorCallNotStarted);
+          }
+          return result;
+        }),
+      );
+    } else {
+      return sendMessageToParent(apiVersionTag, 'call.startCall', [startCallParams], resolve);
+    }
+  });
+}
+
+/**
+ * Checks if the call capability is supported by the host
+ * @returns boolean to represent whether the call capability is supported
+ *
+ * @throws Error if {@linkcode app.initialize} has not successfully completed
+ */
+export function isSupported(): boolean {
+  return ensureInitialized(runtime) && runtime.supports.call ? true : false;
 }
