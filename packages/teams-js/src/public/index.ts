1fbf1d2c385f4983fa56309038c2488758c7f184
noahdarveau-MSFT <109628470+noahdarveau-MSFT@users.noreply.github.com>
2024-10-24 15:21:30 -0700

    added explanatory comment to index file

--- packages/teams-js/src/public/index.ts
+++ packages/teams-js/src/public/index.ts
@@ -46,6 +46,8 @@ export { EmailAddress } from './emailAddress';
 export * as appInstallDialog from './appInstallDialog';
 export * as barCode from './barCode';
 export * as chat from './chat';
+//It is necessary to export the OpenGroupChatRequest and OpenSingleChatRequest from chat.ts individually as well
+//to keep the named exports so as to not break the existing consumers using the named exports.
 export { OpenGroupChatRequest, OpenSingleChatRequest } from './chat';
 export * as clipboard from './clipboard';
 export { dialog } from './dialog';
