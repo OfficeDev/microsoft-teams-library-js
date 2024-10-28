df75cea2a1e29a05f58c98d381563d687768fa11
noahdarveau-MSFT <109628470+noahdarveau-MSFT@users.noreply.github.com>
2024-10-28 15:51:19 -0700

    missed a namespace

--- packages/teams-js/src/public/meeting/appShareButton.ts
+++ packages/teams-js/src/public/meeting/appShareButton.ts
@@ -21,55 +21,53 @@ import { runtime } from '../runtime';
  */
 const meetingTelemetryVersionNumber: ApiVersionNumber = ApiVersionNumber.V_1;
 
-export namespace appShareButton {
+/**
+ * Property bag for the setVisibilityInfo
+ *
+ * @hidden
+ * Hide from docs.
+ *
+ * @internal
+ * Limited to Microsoft-internal use
+ *
+ * @beta
+ */
+export interface ShareInformation {
   /**
-   * Property bag for the setVisibilityInfo
-   *
-   * @hidden
-   * Hide from docs.
-   *
-   * @internal
-   * Limited to Microsoft-internal use
-   *
-   * @beta
+   * boolean flag to set show or hide app share button
    */
-  export interface ShareInformation {
-    /**
-     * boolean flag to set show or hide app share button
-     */
-    isVisible: boolean;
+  isVisible: boolean;
 
-    /**
-     * optional string contentUrl, which will override contentUrl coming from Manifest
-     */
-    contentUrl?: string;
-  }
   /**
-   * By default app share button will be hidden and this API will govern the visibility of it.
-   *
-   * This function can be used to hide/show app share button in meeting,
-   * along with contentUrl (overrides contentUrl populated in app manifest)
-   * @throws standard Invalid Url error
-   * @param shareInformation has two elements, one isVisible boolean flag and another
    * optional string contentUrl, which will override contentUrl coming from Manifest
-   *
-   * @hidden
-   * Hide from docs.
-   *
-   * @internal
-   * Limited to Microsoft-internal use
-   *
-   * @beta
    */
-  export function setOptions(shareInformation: ShareInformation): void {
-    ensureInitialized(runtime, FrameContexts.sidePanel);
-    if (shareInformation.contentUrl) {
-      new URL(shareInformation.contentUrl);
-    }
-    sendMessageToParent(
-      getApiVersionTag(meetingTelemetryVersionNumber, ApiName.Meeting_AppShareButton_SetOptions),
-      'meeting.appShareButton.setOptions',
-      [shareInformation],
-    );
+  contentUrl?: string;
+}
+/**
+ * By default app share button will be hidden and this API will govern the visibility of it.
+ *
+ * This function can be used to hide/show app share button in meeting,
+ * along with contentUrl (overrides contentUrl populated in app manifest)
+ * @throws standard Invalid Url error
+ * @param shareInformation has two elements, one isVisible boolean flag and another
+ * optional string contentUrl, which will override contentUrl coming from Manifest
+ *
+ * @hidden
+ * Hide from docs.
+ *
+ * @internal
+ * Limited to Microsoft-internal use
+ *
+ * @beta
+ */
+export function setOptions(shareInformation: ShareInformation): void {
+  ensureInitialized(runtime, FrameContexts.sidePanel);
+  if (shareInformation.contentUrl) {
+    new URL(shareInformation.contentUrl);
   }
+  sendMessageToParent(
+    getApiVersionTag(meetingTelemetryVersionNumber, ApiName.Meeting_AppShareButton_SetOptions),
+    'meeting.appShareButton.setOptions',
+    [shareInformation],
+  );
 }
