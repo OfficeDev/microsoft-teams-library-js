a3455df6b2b722f1a2a3f12accf593dc9a14419e
noahdarveau-MSFT <109628470+noahdarveau-MSFT@users.noreply.github.com>
2024-10-29 16:17:47 -0700

    Noahdarveau/treeshaking pages (#2588)
    * made the `pages` capability treeshakable

---
+++ packages/teams-js/src/public/pages/fullTrust.ts
@@ -0,0 +1,66 @@
+import { sendMessageToParent } from '../../internal/communication';
+import { ensureInitialized } from '../../internal/internalAPIs';
+import { pagesTelemetryVersionNumber } from '../../internal/pagesHelpers';
+import { ApiName, getApiVersionTag } from '../../internal/telemetry';
+import { errorNotSupportedOnPlatform, FrameContexts } from '../constants';
+import { runtime } from '../runtime';
+
+/**
+ * @hidden
+ * Hide from docs
+ * ------
+ * Provides APIs to interact with the full-trust part of the SDK. Limited to 1P applications
+ * @internal
+ * Limited to Microsoft-internal use
+ */
+/**
+ * @hidden
+ * Hide from docs
+ * ------
+ * Place the tab into full-screen mode.
+ *
+ */
+export function enterFullscreen(): void {
+  ensureInitialized(runtime, FrameContexts.content);
+  if (!isSupported()) {
+    throw errorNotSupportedOnPlatform;
+  }
+  sendMessageToParent(
+    getApiVersionTag(pagesTelemetryVersionNumber, ApiName.Pages_FullTrust_EnterFullscreen),
+    'enterFullscreen',
+    [],
+  );
+}
+
+/**
+ * @hidden
+ * Hide from docs
+ * ------
+ * Reverts the tab into normal-screen mode.
+ */
+export function exitFullscreen(): void {
+  ensureInitialized(runtime, FrameContexts.content);
+  if (!isSupported()) {
+    throw errorNotSupportedOnPlatform;
+  }
+  sendMessageToParent(
+    getApiVersionTag(pagesTelemetryVersionNumber, ApiName.Pages_FullTrust_ExitFullscreen),
+    'exitFullscreen',
+    [],
+  );
+}
+/**
+ * @hidden
+ *
+ * Checks if the pages.fullTrust capability is supported by the host
+ * @returns boolean to represent whether the pages.fullTrust capability is supported
+ *
+ * @throws Error if {@linkcode app.initialize} has not successfully completed
+ */
+export function isSupported(): boolean {
+  return ensureInitialized(runtime) && runtime.supports.pages
+    ? runtime.supports.pages.fullTrust
+      ? true
+      : false
+    : false;
+}
