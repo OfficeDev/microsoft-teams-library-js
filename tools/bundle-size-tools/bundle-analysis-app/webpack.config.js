67d3352178029d595607bc7e64acbb8e691aedbd
noahdarveau-MSFT <109628470+noahdarveau-MSFT@users.noreply.github.com>
2024-11-09 00:09:30 -0800

    updated bundle analysis app

--- tools/bundle-size-tools/bundle-analysis-app/webpack.config.js
+++ tools/bundle-size-tools/bundle-analysis-app/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   resolve: {
     modules: ['node_modules'],
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.cjs', '.mjs'],
     symlinks: true,
   },
   module: {
