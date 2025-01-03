b21fc8db86deda71a57533bd084e6dd7405ad67f
Jay Dahiya <jadahiya@microsoft.com>
2025-01-03 13:42:57 -0800

    Added comment describing why fetch is polyfilled

--- packages/teams-js/test/setupTest.ts
+++ packages/teams-js/test/setupTest.ts
@@ -1,5 +1,10 @@
 import { validOriginsFallback } from '../src/internal/constants';
 
+/**
+ * We currently run a fetch call to acquire CDN assets as soon as TeamsJS is loaded.
+ * Since fetch is supported in both browser and Node environments, but not supported in jest/jsdom,
+ * we polyfill fetch with a mock implementation that acquires the fallback domain list prior to running the tests.
+ */
 global.fetch = jest.fn(() =>
   Promise.resolve({
     status: 200,
