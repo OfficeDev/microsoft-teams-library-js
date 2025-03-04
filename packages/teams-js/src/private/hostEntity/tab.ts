8ec0e1946d9f7ba05a4ed74d163b787490f2002c
cmachart <cmachart@microsoft.com>
2025-03-03 16:17:36 -0800

    PR comment

--- packages/teams-js/src/private/hostEntity/tab.ts
+++ packages/teams-js/src/private/hostEntity/tab.ts
@@ -194,10 +194,10 @@ export function addAndConfigure(
     'hostEntity.tab.addAndConfigure',
     [
       new SerializableHostEntityId(hostEntityIds),
+      appTypes,
       (Array.isArray(appTypesOrMeetingParams) || appTypesOrMeetingParams == undefined
         ? appTypesOrMeetingParams
         : new SerializableMeetingParams(appTypesOrMeetingParams)) as ISerializable | SimpleType,
-      appTypes,
     ],
     new HostEntityTabInstanceResponseHandler(),
     getApiVersionTag(hostEntityTelemetryVersionNumber, ApiName.HostEntity_Tab_addAndConfigureApp),
