8f5f0a99373e6f2db11542b9cc244ded33b65e5d
Lakhveer Kaur <lakhveerkaur@microsoft.com>
2025-04-25 15:33:58 -0700

    nit chnages

--- packages/teams-js/src/private/copilot/copilot.ts
+++ packages/teams-js/src/private/copilot/copilot.ts
@@ -1,5 +1,4 @@
 import * as customTelemetry from './customTelemetry';
 import * as eligibility from './eligibility';
-import * as sidePanel from './sidePanel';
 
-export { customTelemetry, eligibility, sidePanel };
+export { customTelemetry, eligibility };
