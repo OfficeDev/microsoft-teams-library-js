f4528fb604e701f50d114ebe494efc5f2209850c
Lakhveer Kaur <lakhveerkaur@microsoft.com>
2025-04-25 15:57:58 -0700

    fixing UTs

--- packages/teams-js/test/public/tasks.spec.ts
+++ packages/teams-js/test/public/tasks.spec.ts
@@ -268,7 +268,7 @@ describe('tasks', () => {
   });
 
   describe('submitTask', () => {
-    const allowedContexts = [FrameContexts.task, FrameContexts.content];
+    const allowedContexts = [FrameContexts.content, FrameContexts.task];
     it('should not allow calls before initialization', () => {
       expect(() => tasks.submitTask()).toThrowError(new Error(errorLibraryNotInitialized));
     });
