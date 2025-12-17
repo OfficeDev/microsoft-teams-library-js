d0c7fa83c070cffc9ccf029aad79a2319e486f54
Angela Eun <aeun@microsoft.com>
2025-12-17 11:04:20 -0800

    Test without publishing

--- tools/releases/powershell-commands/update-npmrc-with-npm-token.ps1
+++ tools/releases/powershell-commands/update-npmrc-with-npm-token.ps1
@@ -1,1 +1,5 @@
 Set-Content -Path $(PIPELINE.WORKSPACE)/microsoft-teams-library-js-pipeline/NPMFeed/.npmrc -Value "//registry.npmjs.org/:_authToken=$(NPM-TOKEN)"
+
+# TODO: Remove this test step when merging in. 
+# Test that we wrote to .npmrc correctly and the token works
+npm whoami --registry=https://registry.npmjs.org/
