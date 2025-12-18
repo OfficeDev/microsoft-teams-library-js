2fe4899cdfc5ad3b8c04256b9fd85de99f1d091a
Angela Eun <aeun@microsoft.com>
2025-12-18 13:11:59 -0800

    Corrected filepath and progressed to fix second inline script

--- tools/releases/powershell-commands/view-published-pkg-urls.ps1
+++ tools/releases/powershell-commands/view-published-pkg-urls.ps1
@@ -1,4 +1,6 @@
-$version = Get-ChildItem
-# Write-Host "Releasing version $version"
-# Write-Host "CDN: https://res-sdf.cdn.office.net/teams-js/$version/js/MicrosoftTeams.min.js"
-# Write-Host "NPM: https://www.npmjs.com/package/@microsoft/teams-js/v/$version"
+param ($PIPELINE_WORKSPACE)
+
+$version = Get-ChildItem -Path $PIPELINE_WORKSPACE/microsoft-teams-library-js-pipeline/CDNFeed -Directory -Name -Exclude _*
+Write-Host "Releasing version $version"
+Write-Host "CDN: https://res-sdf.cdn.office.net/teams-js/$version/js/MicrosoftTeams.min.js"
+Write-Host "NPM: https://www.npmjs.com/package/@microsoft/teams-js/v/$version"
