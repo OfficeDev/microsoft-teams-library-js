0ce90925f388632956651d7d2b9f7cae9a0c7258
Angela Eun <aeun@microsoft.com>
2025-12-18 10:00:49 -0800

    Reformat args for powershell filepath

--- tools/releases/powershell-commands/update-npmrc-with-npm-token.ps1
+++ tools/releases/powershell-commands/update-npmrc-with-npm-token.ps1
@@ -1,1 +1,2 @@
-Set-Content 
+param ($PIPELINE_WORKSPACE, $NPM_TOKEN)
+Set-Content -Path $PIPELINE_WORKSPACE/microsoft-teams-library-js-pipeline/NPMFeed/.npmrc -Value "//registry.npmjs.org/:_authToken=$NPM_TOKEN"
