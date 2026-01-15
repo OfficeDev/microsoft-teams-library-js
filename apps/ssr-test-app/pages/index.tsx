28c9c8b08c0ade3a9e7ebe4545521217f50f3cdc
vhuseinova-msft <98852890+vhuseinova-msft@users.noreply.github.com>
2026-01-15 13:10:08 -0800

    Updated SSR test app (#2961)

--- apps/ssr-test-app/pages/index.tsx
+++ apps/ssr-test-app/pages/index.tsx
@@ -3,9 +3,13 @@ import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import React, { ReactElement, useEffect, useState } from 'react';
 
+import { ContextDisplay, PageInfo, PostBodyDisplay } from '../components/CommonComponents';
+import { parseBody } from '../utils/serverUtils';
+
 export interface SSRProps {
   renderString: string;
   time: string;
+  postBody?: string;
 }
 
 export default function IndexPage(props: SSRProps): ReactElement {
@@ -28,12 +32,9 @@ export default function IndexPage(props: SSRProps): ReactElement {
         <title>SSR Test App</title>
       </Head>
       <div>
-        <h1 id="id01">{props.renderString}</h1>
-        <h1 id="stime">The server render time is {props.time.substring(12, 24)}</h1>
-        <h1 id="ctime">The client render time is {clientTime.substring(12, 24)}</h1>
-        <pre>
-          <b>Context:</b> {JSON.stringify(teamsContext, null, 2)}
-        </pre>
+        <PageInfo renderString={props.renderString} serverTime={props.time} clientTime={clientTime} />
+        <PostBodyDisplay postBody={props.postBody} />
+        <ContextDisplay context={teamsContext} />
       </div>
     </div>
   );
@@ -42,11 +43,24 @@ export default function IndexPage(props: SSRProps): ReactElement {
 /**
  * @returns prop data
  */
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const time = JSON.stringify(new Date());
+
+  if (req.method === 'POST') {
+    const postBody = await parseBody(req);
+    return {
+      props: {
+        renderString: 'This string brought to you by the server (POST request)',
+        postBody,
+        time,
+      },
+    };
+  }
+
+  // Default GET handling
   return {
     props: {
-      renderString: 'This string brought to you by the server',
+      renderString: 'This string brought to you by the server (GET request)',
       time,
     },
   };
